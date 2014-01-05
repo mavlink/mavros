a0ed47af4a6bb5fecfdfc831f5b10acfc834eba8
Vladimir Ermakov <vooon341@gmail.com>
2014-01-05 08:08:14 +0400

    Fix typo and add copyright string
    NOTE: Please check typos before coping and pasting :)

--- src/mavconn/mavconn_udp.h
+++ src/mavconn/mavconn_udp.h
@@ -1,8 +1,10 @@
 /**
  * @file mavconn_udp.h
- * @author Vladimit Ermkov <voon341@gmail.com>
+ * @author Vladimir Ermakov <vooon341@gmail.com>
  */
 /*
+ * Copyright 2013 Vladimir Ermakov.
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
