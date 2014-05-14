c0c7c5c39e9bdbd79f839df98266d8d0d78017d8
Vladimir Ermakov <vooon341@gmail.com>
2014-05-14 23:59:48 +0400

    plugins: imu_pub: Add RAW_IMU, SCALED_IMU and SCALED_PRESSURE handlers
    Fix #13. Refactor message processing.
    
    Combination of used messages:
    On APM: ATTITUDE + RAW_IMU + SCALED_PRESSURE
    On PX4: ATTITUDE + HIGHRES_IMU
    On other: ATTITUDE + (RAW_IMU|SCALED_IMU + SCALED_PRESSURE)|HIGHRES_IMU
    
    Published topics:
    
     * ~imu/data         - ATTITUDE + accel data from *_IMU
     * ~imu/data_raw     - HIGHRES_IMU or SCALED_IMU or RAW_IMU in that order
     * ~imu/mag          - magnetometer (same source as data_raw)
     * ~imu/temperature  - HIGHRES_IMU or SCALED_PRESSURE
     * ~imu/atm_pressure - same as temperature

--- src/plugins/imu_pub.cpp
+++ src/plugins/imu_pub.cpp
@@ -31,29 +31,27 @@
 #include <sensor_msgs/Imu.h>
 #include <sensor_msgs/MagneticField.h>
 #include <sensor_msgs/Temperature.h>
+#include <sensor_msgs/FluidPressure.h>
+#include <geometry_msgs/Vector3.h>
 
 namespace mavplugin {
 
 class IMUPubPlugin : public MavRosPlugin {
 public:
-	IMUPubPlugin()
+	IMUPubPlugin() :
+		linear_accel_vec(),
+		has_hr_imu(false),
+		has_scaled_imu(false)
 	{
-		imu_raw = {
-			0,
-			0.0, 0.0, 0.0, // accel
-			0.0, 0.0, 0.0, // gyro
-			0.0, 0.0, 0.0, // magn
-			0.0, 0.0, 0.0, // pressure
-			0.0,           // temp
-			0
-		};
 	};
 
-	void initialize(UAS &uas,
+	void initialize(UAS &uas_,
 			ros::NodeHandle &nh,
 			diagnostic_updater::Updater &diag_updater)
 	{
 		double linear_stdev, angular_stdev, orientation_stdev;
+
+		uas = &uas_;
 
 		nh.param<std::string>("imu/frame_id", frame_id, "fcu");
 		nh.param("imu/linear_acceleration_stdev", linear_stdev, 0.0003); // check default by MPU6000 spec
@@ -83,7 +81,8 @@ public:
 
 		imu_pub = nh.advertise<sensor_msgs::Imu>("imu/data", 10);
 		magn_pub = nh.advertise<sensor_msgs::MagneticField>("imu/mag", 10);
-		temp_pub = nh.advertise<sensor_msgs::Imu>("imu/temperature", 10);
+		temp_pub = nh.advertise<sensor_msgs::Temperature>("imu/temperature", 10);
+		press_pub = nh.advertise<sensor_msgs::FluidPressure>("imu/atm_pressure", 10);
 		imu_raw_pub = nh.advertise<sensor_msgs::Imu>("imu/data_raw", 10);
 	}
 
@@ -94,118 +93,293 @@ public:
 	std::vector<uint8_t> get_supported_messages() {
 		return {
 			MAVLINK_MSG_ID_ATTITUDE,
-			MAVLINK_MSG_ID_HIGHRES_IMU
+			MAVLINK_MSG_ID_RAW_IMU,
+			MAVLINK_MSG_ID_SCALED_IMU,
+			MAVLINK_MSG_ID_HIGHRES_IMU,
+			MAVLINK_MSG_ID_SCALED_PRESSURE
 		};
 	}
 
 	void message_rx_cb(const mavlink_message_t *msg, uint8_t sysid, uint8_t compid) {
 		switch (msg->msgid) {
 		case MAVLINK_MSG_ID_ATTITUDE:
-			if (imu_pub.getNumSubscribers() > 0) {
-				mavlink_attitude_t att;
-				mavlink_msg_attitude_decode(msg, &att);
-
-				sensor_msgs::ImuPtr imu_msg(new sensor_msgs::Imu);
-
-				// TODO: check/verify that these are body-fixed
-				imu_msg->orientation = tf::createQuaternionMsgFromRollPitchYaw(
-						att.roll, -att.pitch, -att.yaw);
-
-				imu_msg->angular_velocity.x = att.rollspeed;
-				imu_msg->angular_velocity.y = -att.pitchspeed;
-				imu_msg->angular_velocity.z = -att.yawspeed;
-
-				imu_msg->linear_acceleration.x = imu_raw.xacc;
-				imu_msg->linear_acceleration.y = -imu_raw.yacc;
-				imu_msg->linear_acceleration.z = -imu_raw.zacc;
-
-				imu_msg->orientation_covariance = orientation_cov;
-				imu_msg->angular_velocity_covariance = angular_velocity_cov;
-				imu_msg->linear_acceleration_covariance = linear_acceleration_cov;
-
-				imu_msg->header.frame_id = frame_id;
-				imu_msg->header.seq = imu_raw.time_usec / 1000;
-				imu_msg->header.stamp = ros::Time::now();
-
-				imu_pub.publish(imu_msg);
-			}
+			handle_attitude(msg);
 			break;
-
 		case MAVLINK_MSG_ID_HIGHRES_IMU:
-			{
-				mavlink_msg_highres_imu_decode(msg, &imu_raw);
-
-				std_msgs::Header header;
-				header.stamp = ros::Time::now();
-				header.seq = imu_raw.time_usec / 1000;
-				header.frame_id = frame_id;
-
-				if (imu_raw_pub.getNumSubscribers() > 0 &&
-						imu_raw.fields_updated & 0x003f) {
-					sensor_msgs::ImuPtr imu_msg(new sensor_msgs::Imu);
-
-					imu_msg->angular_velocity.x = imu_raw.xgyro;
-					imu_msg->angular_velocity.y = -imu_raw.ygyro;
-					imu_msg->angular_velocity.z = -imu_raw.xgyro;
-
-					imu_msg->linear_acceleration.x = imu_raw.xacc;
-					imu_msg->linear_acceleration.y = -imu_raw.yacc;
-					imu_msg->linear_acceleration.z = -imu_raw.zacc;
-
-					std::fill(imu_msg->orientation_covariance.begin(),
-							imu_msg->orientation_covariance.end(), 0);
-					imu_msg->orientation_covariance[0] = -1;
-
-					imu_msg->angular_velocity_covariance = angular_velocity_cov;
-					imu_msg->linear_acceleration_covariance = linear_acceleration_cov;
-
-					imu_msg->header = header;
-					imu_raw_pub.publish(imu_msg);
-				}
-				if (magn_pub.getNumSubscribers() > 0 &&
-						imu_raw.fields_updated & 0x01c0) {
-					const double gauss_to_tesla = 1.0e-4;
-					sensor_msgs::MagneticFieldPtr magn_msg(new sensor_msgs::MagneticField);
-
-					magn_msg->magnetic_field.x = imu_raw.xmag * gauss_to_tesla;
-					magn_msg->magnetic_field.y = imu_raw.ymag * gauss_to_tesla;
-					magn_msg->magnetic_field.z = imu_raw.zmag * gauss_to_tesla;
-
-					// TODO: again covariance
-					std::fill(magn_msg->magnetic_field_covariance.begin(),
-							magn_msg->magnetic_field_covariance.end(), 0);
-
-					magn_msg->header = header;
-					magn_pub.publish(magn_msg);
-				}
-				if (imu_raw.fields_updated & 0x0e00) {
-					/* TODO: pressure & alt */
-				}
-				if (temp_pub.getNumSubscribers() > 0 &&
-						imu_raw.fields_updated & 0x1000) {
-					sensor_msgs::TemperaturePtr temp_msg(new sensor_msgs::Temperature);
-
-					temp_msg->temperature = imu_raw.temperature;
-					temp_msg->header = header;
-					temp_pub.publish(temp_msg);
-				}
-			}
+			handle_highres_imu(msg);
+			break;
+		case MAVLINK_MSG_ID_RAW_IMU:
+			handle_raw_imu(msg);
+			break;
+		case MAVLINK_MSG_ID_SCALED_IMU:
+			handle_scaled_imu(msg);
+			break;
+		case MAVLINK_MSG_ID_SCALED_PRESSURE:
+			handle_scaled_pressure(msg);
 			break;
 		};
 	}
 
 private:
 	std::string frame_id;
+	UAS *uas;
 
 	ros::Publisher imu_pub;
 	ros::Publisher imu_raw_pub;
 	ros::Publisher magn_pub;
 	ros::Publisher temp_pub;
+	ros::Publisher press_pub;
 
-	mavlink_highres_imu_t imu_raw;
+	bool has_hr_imu;
+	bool has_scaled_imu;
+	geometry_msgs::Vector3 linear_accel_vec;
 	boost::array<double, 9> linear_acceleration_cov;
 	boost::array<double, 9> angular_velocity_cov;
 	boost::array<double, 9> orientation_cov;
+
+	const double gauss_to_tesla = 1.0e-4;
+	const double millit_to_tesla = 1000.0;
+	const double millirs_to_radsec = 1000.0;
+	const double millig_to_ms2 = 9.80665 / 1000.0;
+	const double millibar_to_pascal = 1.0e5;
+
+
+	void handle_attitude(const mavlink_message_t *msg) {
+		if (imu_pub.getNumSubscribers() == 0)
+			return;
+
+		mavlink_attitude_t att;
+		mavlink_msg_attitude_decode(msg, &att);
+
+		sensor_msgs::ImuPtr imu_msg(new sensor_msgs::Imu);
+
+		// TODO: check/verify that these are body-fixed
+		imu_msg->orientation = tf::createQuaternionMsgFromRollPitchYaw(
+				att.roll, -att.pitch, -att.yaw);
+
+		imu_msg->angular_velocity.x = att.rollspeed;
+		imu_msg->angular_velocity.y = -att.pitchspeed;
+		imu_msg->angular_velocity.z = -att.yawspeed;
+
+		// vector from HIGHRES_IMU or RAW_IMU
+		imu_msg->linear_acceleration = linear_accel_vec;
+
+		imu_msg->orientation_covariance = orientation_cov;
+		imu_msg->angular_velocity_covariance = angular_velocity_cov;
+		imu_msg->linear_acceleration_covariance = linear_acceleration_cov;
+
+		imu_msg->header.frame_id = frame_id;
+		imu_msg->header.stamp = ros::Time::now();
+		imu_pub.publish(imu_msg);
+	}
+
+	void fill_imu_msg_vec(sensor_msgs::ImuPtr &imu_msg,
+			double xg, double yg, double zg,
+			double xa, double ya, double za)
+	{
+		imu_msg->angular_velocity.x = xg;
+		imu_msg->angular_velocity.y = yg;
+		imu_msg->angular_velocity.z = xg;
+
+		imu_msg->linear_acceleration.x = xa;
+		imu_msg->linear_acceleration.y = ya;
+		imu_msg->linear_acceleration.z = za;
+	}
+
+	void handle_highres_imu(const mavlink_message_t *msg) {
+		mavlink_highres_imu_t imu_hr;
+		mavlink_msg_highres_imu_decode(msg, &imu_hr);
+
+		ROS_INFO_COND_NAMED(!has_hr_imu, "imu", "High resolution IMU detected!");
+		has_hr_imu = true;
+
+		std_msgs::Header header;
+		header.stamp = ros::Time::now();
+		header.frame_id = frame_id;
+
+		/* imu/data_raw filled by HR IMU */
+		if (imu_raw_pub.getNumSubscribers() > 0 &&
+				imu_hr.fields_updated & 0x003f) {
+			sensor_msgs::ImuPtr imu_msg(new sensor_msgs::Imu);
+
+			fill_imu_msg_vec(imu_msg,
+					imu_hr.xgyro, -imu_hr.ygyro, -imu_hr.zgyro,
+					imu_hr.xacc, -imu_hr.yacc, -imu_hr.zacc);
+			linear_accel_vec = imu_msg->linear_acceleration;
+
+			std::fill(imu_msg->orientation_covariance.begin(),
+					imu_msg->orientation_covariance.end(), 0);
+			imu_msg->orientation_covariance[0] = -1;
+
+			imu_msg->angular_velocity_covariance = angular_velocity_cov;
+			imu_msg->linear_acceleration_covariance = linear_acceleration_cov;
+
+			imu_msg->header = header;
+			imu_raw_pub.publish(imu_msg);
+		}
+
+		if (magn_pub.getNumSubscribers() > 0 &&
+				imu_hr.fields_updated & 0x01c0) {
+			sensor_msgs::MagneticFieldPtr magn_msg(new sensor_msgs::MagneticField);
+
+			magn_msg->magnetic_field.x = imu_hr.xmag * gauss_to_tesla;
+			magn_msg->magnetic_field.y = imu_hr.ymag * gauss_to_tesla;
+			magn_msg->magnetic_field.z = imu_hr.zmag * gauss_to_tesla;
+
+			// TODO: again covariance
+			std::fill(magn_msg->magnetic_field_covariance.begin(),
+					magn_msg->magnetic_field_covariance.end(), 0);
+			magn_msg->magnetic_field_covariance[0] = -1;
+
+			magn_msg->header = header;
+			magn_pub.publish(magn_msg);
+		}
+
+		if (press_pub.getNumSubscribers() > 0 &&
+				imu_hr.fields_updated & 0x0e00) {
+			sensor_msgs::FluidPressurePtr atmp_msg(new sensor_msgs::FluidPressure);
+
+			atmp_msg->fluid_pressure = imu_hr.abs_pressure * millibar_to_pascal;
+			atmp_msg->header = header;
+			press_pub.publish(atmp_msg);
+		}
+
+		if (temp_pub.getNumSubscribers() > 0 &&
+				imu_hr.fields_updated & 0x1000) {
+			sensor_msgs::TemperaturePtr temp_msg(new sensor_msgs::Temperature);
+
+			temp_msg->temperature = imu_hr.temperature;
+			temp_msg->header = header;
+			temp_pub.publish(temp_msg);
+		}
+	}
+
+	void handle_raw_imu(const mavlink_message_t *msg) {
+		if (has_hr_imu || has_scaled_imu)
+			return;
+
+		sensor_msgs::ImuPtr imu_msg(new sensor_msgs::Imu);
+		mavlink_raw_imu_t imu_raw;
+		mavlink_msg_raw_imu_decode(msg, &imu_raw);
+
+		std_msgs::Header header;
+		header.stamp = ros::Time::now();
+		header.frame_id = frame_id;
+
+		/* NOTE: APM send SCALED_IMU data as RAW_IMU */
+		fill_imu_msg_vec(imu_msg,
+				imu_raw.xgyro * millirs_to_radsec,
+				-imu_raw.ygyro * millirs_to_radsec,
+				-imu_raw.zgyro * millirs_to_radsec,
+				imu_raw.xacc * millig_to_ms2,
+				-imu_raw.yacc * millig_to_ms2,
+				-imu_raw.zacc * millig_to_ms2);
+
+		if (!uas->is_ardupilotmega()) {
+			ROS_WARN_THROTTLE_NAMED(60, "imu", "RAW_IMU: linear acceleration known on APM only");
+			linear_accel_vec.x = 0;
+			linear_accel_vec.y = 0;
+			linear_accel_vec.z = 0;
+		} else
+			linear_accel_vec = imu_msg->linear_acceleration;
+
+		std::fill(imu_msg->orientation_covariance.begin(),
+				imu_msg->orientation_covariance.end(), 0);
+		imu_msg->orientation_covariance[0] = -1;
+
+		imu_msg->angular_velocity_covariance = angular_velocity_cov;
+		imu_msg->linear_acceleration_covariance = linear_acceleration_cov;
+
+		imu_msg->header = header;
+		imu_raw_pub.publish(imu_msg);
+
+		/* -*- magnetic vector -*- */
+		sensor_msgs::MagneticFieldPtr magn_msg(new sensor_msgs::MagneticField);
+
+		magn_msg->magnetic_field.x = imu_raw.xmag * millit_to_tesla;
+		magn_msg->magnetic_field.y = -imu_raw.ymag * millit_to_tesla;
+		magn_msg->magnetic_field.z = -imu_raw.zmag * millit_to_tesla;
+
+		// TODO: again covariance
+		std::fill(magn_msg->magnetic_field_covariance.begin(),
+				magn_msg->magnetic_field_covariance.end(), 0);
+		magn_msg->magnetic_field_covariance[0] = -1;
+
+		magn_msg->header = header;
+		magn_pub.publish(magn_msg);
+	}
+
+	void handle_scaled_imu(const mavlink_message_t *msg) {
+		if (has_hr_imu)
+			return;
+
+		ROS_INFO_COND_NAMED(!has_scaled_imu, "imu", "Scaled IMU message used.");
+		has_scaled_imu = true;
+
+		sensor_msgs::ImuPtr imu_msg(new sensor_msgs::Imu);
+		mavlink_scaled_imu_t imu_raw;
+		mavlink_msg_scaled_imu_decode(msg, &imu_raw);
+
+		std_msgs::Header header;
+		header.stamp = ros::Time::now();
+		header.frame_id = frame_id;
+
+		fill_imu_msg_vec(imu_msg,
+				imu_raw.xgyro * millirs_to_radsec,
+				-imu_raw.ygyro * millirs_to_radsec,
+				-imu_raw.zgyro * millirs_to_radsec,
+				imu_raw.xacc * millig_to_ms2,
+				-imu_raw.yacc * millig_to_ms2,
+				-imu_raw.zacc * millig_to_ms2);
+		linear_accel_vec = imu_msg->linear_acceleration;
+
+		std::fill(imu_msg->orientation_covariance.begin(),
+				imu_msg->orientation_covariance.end(), 0);
+		imu_msg->orientation_covariance[0] = -1;
+
+		imu_msg->angular_velocity_covariance = angular_velocity_cov;
+		imu_msg->linear_acceleration_covariance = linear_acceleration_cov;
+
+		imu_msg->header = header;
+		imu_raw_pub.publish(imu_msg);
+
+		/* -*- magnetic vector -*- */
+		sensor_msgs::MagneticFieldPtr magn_msg(new sensor_msgs::MagneticField);
+
+		magn_msg->magnetic_field.x = imu_raw.xmag * millit_to_tesla;
+		magn_msg->magnetic_field.y = -imu_raw.ymag * millit_to_tesla;
+		magn_msg->magnetic_field.z = -imu_raw.zmag * millit_to_tesla;
+
+		// TODO: again covariance
+		std::fill(magn_msg->magnetic_field_covariance.begin(),
+				magn_msg->magnetic_field_covariance.end(), 0);
+		magn_msg->magnetic_field_covariance[0] = -1;
+
+		magn_msg->header = header;
+		magn_pub.publish(magn_msg);
+	}
+
+	void handle_scaled_pressure(const mavlink_message_t *msg) {
+		if (has_hr_imu)
+			return;
+
+		mavlink_scaled_pressure_t press;
+		mavlink_msg_scaled_pressure_decode(msg, &press);
+
+		std_msgs::Header header;
+		header.stamp = ros::Time::now();
+		header.frame_id = frame_id;
+
+		sensor_msgs::TemperaturePtr temp_msg(new sensor_msgs::Temperature);
+		temp_msg->temperature = press.temperature / 100.0;
+		temp_msg->header = header;
+		temp_pub.publish(temp_msg);
+
+		sensor_msgs::FluidPressurePtr atmp_msg(new sensor_msgs::FluidPressure);
+		atmp_msg->fluid_pressure = press.press_abs * 100.0;
+		atmp_msg->header = header;
+		press_pub.publish(atmp_msg);
+	}
 };
 
 }; // namespace mavplugin
