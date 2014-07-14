13a07aded03fc6f9c6496603100a81f3ac12b9f0
Vladimir Ermakov <vooon341@gmail.com>
2014-07-14 19:15:34 +0400

    plugin: local_position: Change plane conversion
    Bug: #49.

--- src/plugins/local_position.cpp
+++ src/plugins/local_position.cpp
@@ -94,10 +94,14 @@ private:
 				pos_ned.vx, pos_ned.vy, pos_ned.vz);
 
 		/* TODO: check convertion to ENU
-		 * orientation in ENU
+		 * I think XZY is not body-fixed, but orientation does.
+		 * Perhaps this adds additional errorprone to us.
+		 * Need more tests. Issue #49.
+		 *
+		 * orientation in ENU, body-fixed
 		 */
 		tf::Transform transform;
-		transform.setOrigin(tf::Vector3(pos_ned.x, -pos_ned.y, -pos_ned.z));
+		transform.setOrigin(tf::Vector3(pos_ned.y, pos_ned.x, -pos_ned.z));
 		transform.setRotation(uas->get_attitude_orientation());
 
 		if (send_tf)
