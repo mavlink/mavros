b9365183cf765710d2092e92d8597f8485c3f221
Vladimir Ermakov <vooon341@gmail.com>
2014-08-15 14:46:24 +0400

    plugin: global_position: Move heaedr; Style fix.

--- mavros/include/mavros/gps_conversions.h
+++ mavros/include/mavros/gps_conversions.h
@@ -24,7 +24,7 @@
 #include <cstdio>
 #include <cstdlib>
 
-namespace mav_plugin
+namespace UTM
 {
 
 const double RADIANS_PER_DEGREE = M_PI/180.0;
@@ -39,7 +39,7 @@ const double WGS84_EP = 0.0820944379;		// second eccentricity
 
 // UTM Parameters
 const double UTM_K0 = 0.9996;			// scale factor
-const double UTM_FE = 500000.0;		// false easting
+const double UTM_FE = 500000.0;			// false easting
 const double UTM_FN_N = 0.0;			// false northing on north hemisphere
 const double UTM_FN_S = 10000000.0;		// false northing on south hemisphere
 const double UTM_E2 = (WGS84_E*WGS84_E);	// e^2
@@ -56,46 +56,46 @@ const double UTM_EP2 = (UTM_E2/(1-UTM_E2));	// e'^2
  */
 static inline void UTM(double lat, double lon, double *x, double *y)
 {
-  // constants
-  const static double m0 = (1 - UTM_E2/4 - 3*UTM_E4/64 - 5*UTM_E6/256);
-  const static double m1 = -(3*UTM_E2/8 + 3*UTM_E4/32 + 45*UTM_E6/1024);
-  const static double m2 = (15*UTM_E4/256 + 45*UTM_E6/1024);
-  const static double m3 = -(35*UTM_E6/3072);
+	// constants
+	const static double m0 = (1 - UTM_E2/4 - 3*UTM_E4/64 - 5*UTM_E6/256);
+	const static double m1 = -(3*UTM_E2/8 + 3*UTM_E4/32 + 45*UTM_E6/1024);
+	const static double m2 = (15*UTM_E4/256 + 45*UTM_E6/1024);
+	const static double m3 = -(35*UTM_E6/3072);
 
-  // compute the central meridian
-  int cm = ((lon >= 0.0)
-	    ? ((int)lon - ((int)lon)%6 + 3)
-	    : ((int)lon - ((int)lon)%6 - 3));
+	// compute the central meridian
+	int cm = ((lon >= 0.0)
+			? ((int)lon - ((int)lon)%6 + 3)
+			: ((int)lon - ((int)lon)%6 - 3));
 
-  // convert degrees into radians
-  double rlat = lat * RADIANS_PER_DEGREE;
-  double rlon = lon * RADIANS_PER_DEGREE;
-  double rlon0 = cm * RADIANS_PER_DEGREE;
+	// convert degrees into radians
+	double rlat = lat * RADIANS_PER_DEGREE;
+	double rlon = lon * RADIANS_PER_DEGREE;
+	double rlon0 = cm * RADIANS_PER_DEGREE;
 
-  // compute trigonometric functions
-  double slat = sin(rlat);
-  double clat = cos(rlat);
-  double tlat = tan(rlat);
+	// compute trigonometric functions
+	double slat = sin(rlat);
+	double clat = cos(rlat);
+	double tlat = tan(rlat);
 
-  // decide the false northing at origin
-  double fn = (lat > 0) ? UTM_FN_N : UTM_FN_S;
+	// decide the false northing at origin
+	double fn = (lat > 0) ? UTM_FN_N : UTM_FN_S;
 
-  double T = tlat * tlat;
-  double C = UTM_EP2 * clat * clat;
-  double A = (rlon - rlon0) * clat;
-  double M = WGS84_A * (m0*rlat + m1*sin(2*rlat)
+	double T = tlat * tlat;
+	double C = UTM_EP2 * clat * clat;
+	double A = (rlon - rlon0) * clat;
+	double M = WGS84_A * (m0*rlat + m1*sin(2*rlat)
 			+ m2*sin(4*rlat) + m3*sin(6*rlat));
-  double V = WGS84_A / sqrt(1 - UTM_E2*slat*slat);
+	double V = WGS84_A / sqrt(1 - UTM_E2*slat*slat);
 
-  // compute the easting-northing coordinates
-  *x = UTM_FE + UTM_K0 * V * (A + (1-T+C)*pow(A,3)/6
-			      + (5-18*T+T*T+72*C-58*UTM_EP2)*pow(A,5)/120);
-  *y = fn + UTM_K0 * (M + V * tlat * (A*A/2
-				      + (5-T+9*C+4*C*C)*pow(A,4)/24
-				      + ((61-58*T+T*T+600*C-330*UTM_EP2)
-					 * pow(A,6)/720)));
+	// compute the easting-northing coordinates
+	*x = UTM_FE + UTM_K0 * V * (A + (1-T+C)*pow(A,3)/6
+			+ (5-18*T+T*T+72*C-58*UTM_EP2)*pow(A,5)/120);
+	*y = fn + UTM_K0 * (M + V * tlat * (A*A/2
+				+ (5-T+9*C+4*C*C)*pow(A,4)/24
+				+ ((61-58*T+T*T+600*C-330*UTM_EP2)
+					* pow(A,6)/720)));
 
-  return;
+	return;
 }
 
 
@@ -131,7 +131,7 @@ static inline char UTMLetterDesignator(double Lat)
 	else if((-56 > Lat) && (Lat >= -64)) LetterDesignator = 'E';
 	else if((-64 > Lat) && (Lat >= -72)) LetterDesignator = 'D';
 	else if((-72 > Lat) && (Lat >= -80)) LetterDesignator = 'C';
-        // 'Z' is an error flag, the Latitude is outside the UTM limits
+	// 'Z' is an error flag, the Latitude is outside the UTM limits
 	else LetterDesignator = 'Z';
 	return LetterDesignator;
 }
@@ -146,8 +146,8 @@ static inline char UTMLetterDesignator(double Lat)
  * Written by Chuck Gantz- chuck.gantz@globalstar.com
  */
 static inline void LLtoUTM(const double Lat, const double Long,
-                           double &UTMNorthing, double &UTMEasting,
-                           char* UTMZone)
+		double &UTMNorthing, double &UTMEasting,
+		char* UTMZone)
 {
 	double a = WGS84_A;
 	double eccSquared = UTM_E2;
@@ -157,7 +157,7 @@ static inline void LLtoUTM(const double Lat, const double Long,
 	double eccPrimeSquared;
 	double N, T, C, A, M;
 
-        //Make sure the longitude is between -180.00 .. 179.9
+	//Make sure the longitude is between -180.00 .. 179.9
 	double LongTemp = (Long+180)-int((Long+180)/360)*360-180;
 
 	double LatRad = Lat*RADIANS_PER_DEGREE;
@@ -170,15 +170,15 @@ static inline void LLtoUTM(const double Lat, const double Long,
 	if( Lat >= 56.0 && Lat < 64.0 && LongTemp >= 3.0 && LongTemp < 12.0 )
 		ZoneNumber = 32;
 
-        // Special zones for Svalbard
+	// Special zones for Svalbard
 	if( Lat >= 72.0 && Lat < 84.0 )
 	{
-	  if(      LongTemp >= 0.0  && LongTemp <  9.0 ) ZoneNumber = 31;
-	  else if( LongTemp >= 9.0  && LongTemp < 21.0 ) ZoneNumber = 33;
-	  else if( LongTemp >= 21.0 && LongTemp < 33.0 ) ZoneNumber = 35;
-	  else if( LongTemp >= 33.0 && LongTemp < 42.0 ) ZoneNumber = 37;
-	 }
-        // +3 puts origin in middle of zone
+		if(      LongTemp >= 0.0  && LongTemp <  9.0 ) ZoneNumber = 31;
+		else if( LongTemp >= 9.0  && LongTemp < 21.0 ) ZoneNumber = 33;
+		else if( LongTemp >= 21.0 && LongTemp < 33.0 ) ZoneNumber = 35;
+		else if( LongTemp >= 33.0 && LongTemp < 42.0 ) ZoneNumber = 37;
+	}
+	// +3 puts origin in middle of zone
 	LongOrigin = (ZoneNumber - 1)*6 - 180 + 3;
 	LongOriginRad = LongOrigin * RADIANS_PER_DEGREE;
 
@@ -193,28 +193,28 @@ static inline void LLtoUTM(const double Lat, const double Long,
 	A = cos(LatRad)*(LongRad-LongOriginRad);
 
 	M = a*((1	- eccSquared/4		- 3*eccSquared*eccSquared/64	- 5*eccSquared*eccSquared*eccSquared/256)*LatRad
-				- (3*eccSquared/8	+ 3*eccSquared*eccSquared/32	+ 45*eccSquared*eccSquared*eccSquared/1024)*sin(2*LatRad)
-									+ (15*eccSquared*eccSquared/256 + 45*eccSquared*eccSquared*eccSquared/1024)*sin(4*LatRad)
-									- (35*eccSquared*eccSquared*eccSquared/3072)*sin(6*LatRad));
+			- (3*eccSquared/8	+ 3*eccSquared*eccSquared/32	+ 45*eccSquared*eccSquared*eccSquared/1024)*sin(2*LatRad)
+			+ (15*eccSquared*eccSquared/256 + 45*eccSquared*eccSquared*eccSquared/1024)*sin(4*LatRad)
+			- (35*eccSquared*eccSquared*eccSquared/3072)*sin(6*LatRad));
 
 	UTMEasting = (double)(k0*N*(A+(1-T+C)*A*A*A/6
-					+ (5-18*T+T*T+72*C-58*eccPrimeSquared)*A*A*A*A*A/120)
-					+ 500000.0);
+				+ (5-18*T+T*T+72*C-58*eccPrimeSquared)*A*A*A*A*A/120)
+			+ 500000.0);
 
 	UTMNorthing = (double)(k0*(M+N*tan(LatRad)*(A*A/2+(5-T+9*C+4*C*C)*A*A*A*A/24
-				 + (61-58*T+T*T+600*C-330*eccPrimeSquared)*A*A*A*A*A*A/720)));
+					+ (61-58*T+T*T+600*C-330*eccPrimeSquared)*A*A*A*A*A*A/720)));
 	if(Lat < 0)
 		UTMNorthing += 10000000.0; //10000000 meter offset for southern hemisphere
 }
 
 static inline void LLtoUTM(const double Lat, const double Long,
-                           double &UTMNorthing, double &UTMEasting,
-                           std::string &UTMZone) {
-  char zone_buf[] = {0, 0, 0, 0};
+		double &UTMNorthing, double &UTMEasting,
+		std::string &UTMZone) {
+	char zone_buf[] = {0, 0, 0, 0};
 
-  LLtoUTM(Lat, Long, UTMNorthing, UTMEasting, zone_buf);
+	LLtoUTM(Lat, Long, UTMNorthing, UTMEasting, zone_buf);
 
-  UTMZone = zone_buf;
+	UTMZone = zone_buf;
 }
 
 
@@ -228,7 +228,7 @@ static inline void LLtoUTM(const double Lat, const double Long,
  * Written by Chuck Gantz- chuck.gantz@globalstar.com
  */
 static inline void UTMtoLL(const double UTMNorthing, const double UTMEasting,
-                           const char* UTMZone, double& Lat,  double& Long )
+		const char* UTMZone, double& Lat,  double& Long )
 {
 	double k0 = UTM_K0;
 	double a = WGS84_A;
@@ -263,8 +263,8 @@ static inline void UTMtoLL(const double UTMNorthing, const double UTMEasting,
 	mu = M/(a*(1-eccSquared/4-3*eccSquared*eccSquared/64-5*eccSquared*eccSquared*eccSquared/256));
 
 	phi1Rad = mu	+ (3*e1/2-27*e1*e1*e1/32)*sin(2*mu)
-				+ (21*e1*e1/16-55*e1*e1*e1*e1/32)*sin(4*mu)
-				+(151*e1*e1*e1/96)*sin(6*mu);
+		+ (21*e1*e1/16-55*e1*e1*e1*e1/32)*sin(4*mu)
+		+(151*e1*e1*e1/96)*sin(6*mu);
 	phi1 = phi1Rad*DEGREES_PER_RADIAN;
 
 	N1 = a/sqrt(1-eccSquared*sin(phi1Rad)*sin(phi1Rad));
@@ -274,20 +274,19 @@ static inline void UTMtoLL(const double UTMNorthing, const double UTMEasting,
 	D = x/(N1*k0);
 
 	Lat = phi1Rad - (N1*tan(phi1Rad)/R1)*(D*D/2-(5+3*T1+10*C1-4*C1*C1-9*eccPrimeSquared)*D*D*D*D/24
-					+(61+90*T1+298*C1+45*T1*T1-252*eccPrimeSquared-3*C1*C1)*D*D*D*D*D*D/720);
+			+(61+90*T1+298*C1+45*T1*T1-252*eccPrimeSquared-3*C1*C1)*D*D*D*D*D*D/720);
 	Lat = Lat * DEGREES_PER_RADIAN;
 
 	Long = (D-(1+2*T1+C1)*D*D*D/6+(5-2*C1+28*T1-3*C1*C1+8*eccPrimeSquared+24*T1*T1)
-					*D*D*D*D*D/120)/cos(phi1Rad);
+			*D*D*D*D*D/120)/cos(phi1Rad);
 	Long = LongOrigin + Long * DEGREES_PER_RADIAN;
-
 }
 
 static inline void UTMtoLL(const double UTMNorthing, const double UTMEasting,
-    std::string UTMZone, double& Lat, double& Long) {
-  UTMtoLL(UTMNorthing, UTMEasting, UTMZone.c_str(), Lat, Long);
+		std::string UTMZone, double& Lat, double& Long) {
+	UTMtoLL(UTMNorthing, UTMEasting, UTMZone.c_str(), Lat, Long);
 }
 
-} // end namespace UTM
+}; // end namespace UTM
 
 #endif // _UTM_H
--- mavros/src/plugins/global_position.cpp
+++ mavros/src/plugins/global_position.cpp
@@ -26,6 +26,7 @@
  */
 
 #include <mavros/mavros_plugin.h>
+#include <mavros/gps_conversions.h>
 #include <pluginlib/class_list_macros.h>
 
 #include <tf/transform_broadcaster.h>
@@ -33,17 +34,16 @@
 #include <sensor_msgs/NavSatFix.h>
 #include <geometry_msgs/Vector3Stamped.h>
 #include <std_msgs/Float64.h>
-
-#include "gps_conversions.h"
+#include <std_msgs/Header.h>
 
 namespace mavplugin {
 
 /**
  * @brief Global position plugin.
- * 
+ *
  * Publishes global position. Convertion from GPS to UTF allows
  * publishing local position to TF and PoseWithCovarianceStamped.
- * 
+ *
  */
 class GlobalPositionPlugin : public MavRosPlugin {
 public:
@@ -92,7 +92,7 @@ private:
 	ros::Publisher vel_pub;
 	ros::Publisher hdg_pub;
 	ros::Publisher rel_alt_pub;
-	
+
 	tf::TransformBroadcaster tf_broadcaster;
 
 	std::string frame_id;		//!< origin for TF
@@ -100,107 +100,108 @@ private:
 	bool send_tf;
 	double rot_cov;
 
+	/**
+	 * @todo Handler for GLOBAL_POSITION_INT_COV
+	 */
+
 	void handle_global_position_int(const mavlink_message_t *msg, uint8_t sysid, uint8_t compid) {
 		mavlink_global_position_int_t gp_pos;
 		mavlink_msg_global_position_int_decode(msg, &gp_pos);
-	
-		ROS_DEBUG_THROTTLE_NAMED(10, "global_position", "Local position NED: boot_ms:%06d "
+
+		ROS_DEBUG_THROTTLE_NAMED(10, "global", "Global position: boot_ms:%06d "
 				"lat/lon/alt:(%d %d %d) relative_alt: (%d) gps_vel:(%d %d %d) compass_heading: (%d)",
 				gp_pos.time_boot_ms,
 				gp_pos.lat, gp_pos.lon, gp_pos.alt, gp_pos.relative_alt,
 				gp_pos.vx, gp_pos.vy, gp_pos.vz, gp_pos.hdg);
-		/**
-		 * TODO: Upgrade to GLOBAL_POSITION_INT_COV so to get the covariance
-		 */
-		
-		geometry_msgs::PoseWithCovarianceStampedPtr pose_cov = boost::make_shared<geometry_msgs::PoseWithCovarianceStamped>();
-		
-		sensor_msgs::NavSatFixPtr gps_cord = boost::make_shared<sensor_msgs::NavSatFix>();
-		geometry_msgs::Vector3StampedPtr gps_vel =  boost::make_shared<geometry_msgs::Vector3Stamped>();
+
+		geometry_msgs::PoseWithCovarianceStampedPtr pose_cov =
+			boost::make_shared<geometry_msgs::PoseWithCovarianceStamped>();
+
+		std_msgs::Header header;
+		header.frame_id = frame_id;
+		header.stamp = ros::Time::now();
+
+		sensor_msgs::NavSatFixPtr gps_cord =
+			boost::make_shared<sensor_msgs::NavSatFix>();
+		geometry_msgs::Vector3StampedPtr gps_vel =
+			boost::make_shared<geometry_msgs::Vector3Stamped>();
 		std_msgs::Float64Ptr relative_alt = boost::make_shared<std_msgs::Float64>();
-		std_msgs::Float64Ptr compass_heading = compass_heading = boost::make_shared<std_msgs::Float64>();
-		
-		/**
-		 * TODO: Check if PX4 sends time stamp of GPS
-		 *       or use system time handler...
-		 */
-		
-		//gps_cord->header.stamp = gp_pos.time_boot_ms;
-		gps_cord->header.frame_id = frame_id;
+		std_msgs::Float64Ptr compass_heading = boost::make_shared<std_msgs::Float64>();
+
+		gps_cord->header = header;
 		gps_cord->latitude = gp_pos.lat;
 		gps_cord->longitude = gp_pos.lon;
 		gps_cord->altitude = gp_pos.alt;
-		
-		//gps_vel->header.stamp = gp_pos.time_boot_ms;
-		gps_vel->header.frame_id = frame_id;
+
+		gps_vel->header = header;
 		gps_vel->vector.x = gp_pos.vx;
 		gps_vel->vector.y = gp_pos.vy;
 		gps_vel->vector.z = gp_pos.vz;
-		
+
+		// XXX TODO
 		compass_heading->data;
 		relative_alt->data;
-		
-		/** Adapted from gps_umd ROS package [http://wiki.ros.org/gps_umd?distro=hydro]
-		 *  Author: Ken Tossell <ken AT tossell DOT net>
-		 */
-		
+
 		double northing, easting;
 		std::string zone;
-		
-		mav_plugin::LLtoUTM(gps_cord->latitude, gps_cord->longitude, northing, easting, zone);
-		
-		//pose_cov->header.stamp = time_boot_ms;
-		pose_cov->header.frame_id = frame_id;
+
+		/** Adapted from gps_umd ROS package @a http://wiki.ros.org/gps_umd
+		 *  Author: Ken Tossell <ken AT tossell DOT net>
+		 */
+		UTM::LLtoUTM(gps_cord->latitude, gps_cord->longitude, northing, easting, zone);
+
+		pose_cov->header = header;
 		pose_cov->pose.pose.position.x = easting;
 		pose_cov->pose.pose.position.y = northing;
 		pose_cov->pose.pose.position.z = gp_pos.alt;
-		
+
 		pose_cov->pose.pose.orientation.x = 0;
 		pose_cov->pose.pose.orientation.y = 0;
 		pose_cov->pose.pose.orientation.z = 0;
 		pose_cov->pose.pose.orientation.w = 1;
-		
+
 		// Use ENU covariance to build XYZRPY covariance
-		boost::array<double, 36> covariance = {{
-		gps_cord->position_covariance[0],
-		gps_cord->position_covariance[1],
-		gps_cord->position_covariance[2],
-		0, 0, 0,
-		gps_cord->position_covariance[3],
-		gps_cord->position_covariance[4],
-		gps_cord->position_covariance[5],
-		0, 0, 0,
-		gps_cord->position_covariance[6],
-		gps_cord->position_covariance[7],
-		gps_cord->position_covariance[8],
-		0, 0, 0,
-		0, 0, 0, rot_cov, 0, 0,
-		0, 0, 0, 0, rot_cov, 0,
-		0, 0, 0, 0, 0, rot_cov
-		}};	  
-		
+		boost::array<double, 36> covariance = {
+			gps_cord->position_covariance[0],
+			gps_cord->position_covariance[1],
+			gps_cord->position_covariance[2],
+			0, 0, 0,
+			gps_cord->position_covariance[3],
+			gps_cord->position_covariance[4],
+			gps_cord->position_covariance[5],
+			0, 0, 0,
+			gps_cord->position_covariance[6],
+			gps_cord->position_covariance[7],
+			gps_cord->position_covariance[8],
+			0, 0, 0,
+			0, 0, 0, rot_cov, 0, 0,
+			0, 0, 0, 0, rot_cov, 0,
+			0, 0, 0, 0, 0, rot_cov
+		};
+
 		fix_pub.publish(gps_cord);
 		pos_pub.publish(pose_cov);
 		vel_pub.publish(gps_vel);
-	
-		tf::Transform transform;
-		transform.setOrigin(tf::Vector3(pose_cov->pose.pose.position.y,
-						 pose_cov->pose.pose.position.x,
-						-pose_cov->pose.pose.position.z));
-		
-		transform.setRotation(tf::Quaternion(pose_cov->pose.pose.orientation.y,
-						      pose_cov->pose.pose.orientation.x,
-						     -pose_cov->pose.pose.orientation.z,
-						      pose_cov->pose.pose.orientation.w));				    
 
-		if (send_tf)
+		if (send_tf) {
+			tf::Transform transform;
+			// XXX: we realy need change frame?
+			transform.setOrigin(tf::Vector3(pose_cov->pose.pose.position.y,
+						pose_cov->pose.pose.position.x,
+						-pose_cov->pose.pose.position.z));
+
+			transform.setRotation(tf::Quaternion(pose_cov->pose.pose.orientation.y,
+						pose_cov->pose.pose.orientation.x,
+						-pose_cov->pose.pose.orientation.z,
+						pose_cov->pose.pose.orientation.w));
+
 			tf_broadcaster.sendTransform(
 					tf::StampedTransform(
 						transform,
 						pose_cov->header.stamp,
-						frame_id, child_frame_id));		
+						frame_id, child_frame_id));
+		}
 	}
-	
 };
 
 }; // namespace mavplugin
