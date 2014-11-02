03b75c84996a536c249fcfbdc2e427f1f22df1f4
Vladimir Ermakov <vooon341@gmail.com>
2014-11-02 20:36:40 +0300

    mavconn #161: Enable rosconsole bridge.

---
+++ mavros/src/lib/rosconsole_bridge.cpp
@@ -0,0 +1,26 @@
+/**
+ * @brief Enable console bridge
+ * @file rosconsole_bridge.cpp
+ * @author Vladimir Ermakov <vooon341@gmail.com>
+ */
+/*
+ * Copyright 2014 Vladimir Ermakov.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include <rosconsole_bridge/bridge.h>
+
+REGISTER_ROSCONSOLE_BRIDGE;
