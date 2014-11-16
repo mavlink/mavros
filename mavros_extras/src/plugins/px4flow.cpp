f41351a233f47793cf79c788682338700b03ca23
Kabir Mohammed <mhkabir98@gmail.com>
2014-11-16 22:09:58 +0530

    New interface commit

--- mavros_extras/src/plugins/px4flow.cpp
+++ mavros_extras/src/plugins/px4flow.cpp
@@ -29,6 +29,10 @@
 #include <pluginlib/class_list_macros.h>
 
 #include <mavros_extras/OpticalFlow.h>
+#include <mavros_extras/OpticalFlowRad.h>
+#include <geometry_msgs/TwistWithCovarianceStamped.h>
+#include <sensor_msgs/Temperature.h>
+#include <sensor_msgs/Range.h>
 
 namespace mavplugin {
 
@@ -49,11 +53,23 @@ public:
 			diagnostic_updater::Updater &diag_updater)
 	{
 		uas = &uas_;
+		
+		nh.param<std::string>("px4flow/frame_id", frame_id, "px4flow");
 
-		flow_pub = nh.advertise<mavros_extras::OpticalFlow>("optical_flow", 10);
-		flow_rad_pub = nh.advertise<mavros_extras::OpticalFlowRad>("optical_flow_rad", 10);
-		twist_pub = nh.advertise<geometry_msgs::Twist>("velocity", 10);
-			// distance ranger and temperature too ?
+		//Default rangefinder is Maxbotix HRLV-EZ4
+		nh.param("px4flow/ranger_type", ranger_type, 0);	// See sensor_msgs/Range docs 
+		nh.param("px4flow/ranger_fov", ranger_fov, 0.0); 	// TODO
+		nh.param("px4flow/ranger_min_range", ranger_min_range, 0.3);	
+		nh.param("px4flow/ranger_max_range", ranger_max_range, 5.0);
+		flow_pub = nh.advertise<mavros_extras::OpticalFlow>("/px4flow/optical_flow", 10);
+
+		flow_rad_pub = nh.advertise<mavros_extras::OpticalFlowRad>("px4flow/optical_flow_rad", 10);
+		twist_pub = nh.advertise<geometry_msgs::TwistWithCovarianceStamped>("px4flow/velocity", 10);
+		range_pub = nh.advertise<sensor_msgs::Range>("px4flow/ground_distance", 10);
+		temp_pub = nh.advertise<sensor_msgs::Temperature>("px4flow/temperature", 10);
+
+		//set rangefinder params here.
+			
 
 	}
 
@@ -63,33 +79,40 @@ public:
 
 	const message_map get_rx_handlers() {
 		return {
+			MESSAGE_HANDLER(MAVLINK_MSG_ID_OPTICAL_FLOW_RAD, &PX4FlowPlugin::handle_optical_flow_rad),
 			MESSAGE_HANDLER(MAVLINK_MSG_ID_OPTICAL_FLOW, &PX4FlowPlugin::handle_optical_flow)
-			MESSAGE_HANDLER(MAVLINK_MSG_ID_OPTICAL_FLOW_RAD, &PX4FlowPlugin::handle_optical_flow_rad)
 		};
 	}
 
 private:
 	UAS *uas;
 
+	std::string frame_id;
+	
+	int ranger_type;
+	double ranger_fov;
+	double ranger_min_range;
+	double ranger_max_range;
+
 	ros::Publisher flow_pub;
-	ros::Subscriber flow_sub;
+	ros::Publisher flow_rad_pub;
+	ros::Publisher twist_pub;
+	ros::Publisher range_pub;
+	ros::Publisher temp_pub;
 
 	void handle_optical_flow(const mavlink_message_t *msg, uint8_t sysid, uint8_t compid) {
-		if (flow_pub.getNumSubscribers() == 0)
-			return;
-
+	
 		mavlink_optical_flow_t flow;
 		mavlink_msg_optical_flow_decode(msg, &flow);
 
 		mavros_extras::OpticalFlowPtr flow_msg =
 			boost::make_shared<mavros_extras::OpticalFlow>();
 
-		// Note: for ENU->NED conversion XXX CHECK DIS
 		flow_msg->header.stamp = ros::Time::now();
-		flow_msg->flow_x = flow.flow_y;
-		flow_msg->flow_y = flow.flow_x;
-		flow_msg->flow_comp_m_x	 = flow.flow_comp_m_y;
-		flow_msg->flow_comp_m_y	 = flow.flow_comp_m_x;
+		flow_msg->flow_x = flow.flow_x;
+		flow_msg->flow_y = -flow.flow_y;
+		flow_msg->flow_comp_m_x	 = flow.flow_comp_m_x;
+		flow_msg->flow_comp_m_y	 = flow.flow_comp_m_y;
 		flow_msg->quality = flow.quality;
 		flow_msg->ground_distance = flow.ground_distance;
 
@@ -99,38 +122,79 @@ private:
 		 *                with data filled by spec on used sonar.
 		 */
 	}
-
+	
 	void handle_optical_flow_rad(const mavlink_message_t *msg, uint8_t sysid, uint8_t compid) {
-		if (flow_pub.getNumSubscribers() == 0)
-			return;
 
 		mavlink_optical_flow_rad_t flow_rad;
 		mavlink_msg_optical_flow_rad_decode(msg, &flow_rad);
+	
+		std_msgs::Header header;
+		header.stamp = ros::Time::now();
+		header.frame_id = frame_id;
 
+		// Raw message with axes mapped to ROS conventions and temp in degrees celsius
 		mavros_extras::OpticalFlowRadPtr flow_rad_msg =
 			boost::make_shared<mavros_extras::OpticalFlowRad>();
 
 		
-		flow_rad_msg->header.stamp = ros::Time::now(); // TODO use PX4flow stamp?
+		flow_rad_msg->header = header; 
 		
-		// Note: for ENU->NED conversion XXX CHECK DIS
-		flow_msg->integration_time_us = flow_rad.integration_time_us;
-		flow_msg->integrated_x = flow_rad.integrated_x;
-		flow_msg->integrated_y = flow_rad.integrated_y;
-		flow_msg->integrated_xgyro = flow_rad.integrated_xgyro;
-		flow_msg->integrated_ygyro = flow_rad.integrated_ygyro;
-		flow_msg->integrated_zgyro = flow_rad.integrated_zgyro;
-		flow_msg->temperature = flow_rad.temperature;
-		flow_msg->time_delta_distance_us = flow_rad.time_delta_distance_us
-		flow_msg->distance = flow_rad.distance;
+		flow_rad_msg->integration_time_us = flow_rad.integration_time_us;
+		flow_rad_msg->integrated_x = flow_rad.integrated_x;
+		flow_rad_msg->integrated_y = -flow_rad.integrated_y; // NED -> ENU
+		flow_rad_msg->integrated_xgyro = flow_rad.integrated_xgyro;
+		flow_rad_msg->integrated_ygyro = -flow_rad.integrated_ygyro; // NED -> ENU
+		flow_rad_msg->integrated_zgyro = -flow_rad.integrated_zgyro; // NED -> ENU
+		flow_rad_msg->temperature = flow_rad.temperature/100.0f; // in degrees celsius
+		flow_rad_msg->time_delta_distance_us = flow_rad.time_delta_distance_us;
+		flow_rad_msg->distance = flow_rad.distance; 
 
 		flow_rad_pub.publish(flow_rad_msg);
+				
+		if(flow_rad.quality > 0) // don't publish garbage
+		{
+		//Twist - PX4flow is rotated 90 degrees yaw
+		geometry_msgs::TwistWithCovarianceStampedPtr twist_msg =
+		boost::make_shared<geometry_msgs::TwistWithCovarianceStamped>();
 
-		/* Optional TODO: send ground_distance in sensor_msgs/Range and temperature too
+		twist_msg->header = header; 
+		
+		twist_msg->twist.twist.linear.x = ((flow_rad.integrated_x/flow_rad.integration_time_us)/flow_rad.distance)*1000000 ;
+		twist_msg->twist.twist.linear.y = -((flow_rad.integrated_y/flow_rad.integration_time_us)/flow_rad.distance)*1000000;// NED -> ENU
+		twist_msg->twist.twist.linear.z = 0; 
+		twist_msg->twist.twist.angular.x = flow_rad.integrated_xgyro/flow_rad.integration_time_us;
+		twist_msg->twist.twist.angular.y = -(flow_rad.integrated_ygyro/flow_rad.integration_time_us);// NED -> ENU
+		twist_msg->twist.twist.angular.z = -(flow_rad.integrated_zgyro/flow_rad.integration_time_us);// NED -> ENU
 
-		 *                with data filled by spec on used sonar.
+		//TODO : Covariances from quality?
 
-		 */
+		twist_pub.publish(twist_msg);
+
+		}
+
+		// Temperature
+		sensor_msgs::TemperaturePtr temp_msg = 
+			boost::make_shared<sensor_msgs::Temperature>();
+		
+		temp_msg->header = header;	
+
+		temp_msg->temperature = flow_rad.temperature/100.0f; 
+		
+		temp_pub.publish(temp_msg);
+
+		// Rangefinder
+		sensor_msgs::RangePtr range_msg = 
+			boost::make_shared<sensor_msgs::Range>();
+
+		range_msg->header = header;
+		
+		range_msg->radiation_type = ranger_type;
+		range_msg->field_of_view = ranger_fov;
+		range_msg->min_range = ranger_min_range;
+		range_msg->max_range = ranger_max_range;
+		range_msg->range = flow_rad.distance;
+
+		range_pub.publish(range_msg);
 	}
 
 };
