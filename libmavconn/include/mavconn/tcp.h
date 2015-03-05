2c6344f78fb222e773c14594fadcc0aea46b1f59
Vladimir Ermakov <vooon341@gmail.com>
2015-03-05 18:19:31 +0300

    license #242: update libmavconn headers

--- libmavconn/include/mavconn/tcp.h
+++ libmavconn/include/mavconn/tcp.h
@@ -10,18 +10,9 @@
  * libmavconn
  * Copyright 2014,2015 Vladimir Ermakov, All rights reserved.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3.0 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.
+ * This file is part of the mavros package and subject to the license terms
+ * in the top-level LICENSE file of the mavros repository.
+ * https://github.com/mavlink/mavros/tree/master/LICENSE.md
  */
 
 #pragma once
