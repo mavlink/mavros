ef1b71a6088d9bc025f49b628f70538bdc07f2c0
Vladimir Ermakov <vooon341@gmail.com>
2015-04-01 18:30:44 +0300

    python #262: add get_topic()

--- mavros/src/mavros/__init__.py
+++ mavros/src/mavros/__init__.py
@@ -1,2 +1,34 @@
-# -*- python -*-
+# -*- coding: utf-8 -*-
 # vim:set ts=4 sw=4 et:
+
+__all__ = (
+    'get_namespace',
+    'set_namespace',
+    'get_topic'
+)
+
+# global namespace storage
+_mavros_ns = "/mavros"
+
+
+def get_namespace():
+    """
+    Returns mavros node namespace
+    """
+    global _mavros_ns
+    return _mavros_ns
+
+
+def set_namespace(ns):
+    """
+    Sets namespace of mavros node
+    """
+    global _mavros_ns
+    _mavros_ns = ns
+
+
+def get_topic(*args):
+    """
+    Create topic name for mavros node
+    """
+    return '/'.join((get_namespace(), ) + args)
