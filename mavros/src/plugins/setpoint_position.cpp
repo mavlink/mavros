1455fcb4e78bc14b1019d57828ddf32993f90f24
Vladimir Ermakov <vooon341@gmail.com>
2015-04-15 13:56:58 +0300

    plugin: setpoint_position #273: add quirk for PX4.

--- mavros/src/plugins/setpoint_position.cpp
+++ mavros/src/plugins/setpoint_position.cpp
@@ -89,12 +89,22 @@ private:
 		/* Documentation start from bit 1 instead 0,
 		 * Ignore velocity and accel vectors, yaw rate
 		 */
-		uint16_t ignore_all_except_xyzy = (1 << 11) | (7 << 6) | (7 << 3);
+		uint16_t ignore_all_except_xyz_y = (1 << 11) | (7 << 6) | (7 << 3);
+
+		if (uas->is_px4()) {
+			/**
+			 * Current PX4 has bug: it cuts throttle if there no velocity sp
+			 * Issue #273.
+			 *
+			 * @todo Revesit this quirk later. Should be fixed in firmware.
+			 */
+			ignore_all_except_xyz_y = (1 << 11) | (7 << 6);
+		}
 
 		// ENU->NED. Issue #49.
 		set_position_target_local_ned(stamp.toNSec() / 1000000,
 				MAV_FRAME_LOCAL_NED,
-				ignore_all_except_xyzy,
+				ignore_all_except_xyz_y,
 				origin.y(), origin.x(), -origin.z(),
 				0.0, 0.0, 0.0,
 				0.0, 0.0, 0.0,
