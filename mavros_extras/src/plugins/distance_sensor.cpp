4bb3f1e4e30dc08f7e28d746b849a3328d3d6877
TSC21 <n.marques21@hotmail.com>
2015-05-12 14:23:30 +0100

    distance_sensor: minor fixes that include use Range.msg to Laser data

--- mavros_extras/src/plugins/distance_sensor.cpp
+++ mavros_extras/src/plugins/distance_sensor.cpp
@@ -17,10 +17,8 @@
 #include <mavros/mavros_plugin.h>
 #include <pluginlib/class_list_macros.h>
 
-#include <mavros_extras/LaserDistanceSensor.h>
-#include <mavros_extras/SonarDistanceSensor.h>
+#include <mavros_extras/DistanceSensor.h>
 #include <sensor_msgs/Range.h>
-#include <sensor_msgs/LaserScan.h>
 
 namespace mavplugin {
 /**
@@ -42,44 +40,43 @@ public:
 
 		dist_nh.param<std::string>("frame_id", frame_id, "distance_sensor");
 
-		dist_nh.param("input/rangefinder_type", rangefinder_type, 0); // default = laser (0)
+		dist_nh.param("input/rangefinder_type", rangefinder_type, 0);	// default = laser (0)
 
 		//Default sonar rangefinder is Maxbotix HRLV-EZ4
 		dist_nh.param("input/sonar_fov", sonar_fov, 0.0);	// TODO
-		dist_nh.param("input/sonar_min_range", sonar_min_range, 0.3);
-		dist_nh.param("input/sonar_max_range", sonar_max_range, 5.0);
+		dist_nh.param("input/sonar_min_range", sonar_min_range, 30.0); // in centimeters
+		dist_nh.param("input/sonar_max_range", sonar_max_range, 500.0); 
 
 		dist_nh.param("sonar_covariance", sonar_covariance, 0);
 		dist_nh.param("output/sonar_orientation", sonar_orientation, 0); // TODO: Check!
 		dist_nh.param("output/sonar_id", sonar_id, 1);
 
 		//Default laser rangefinder is PulsedLight LIDAR Lite (fixed orientation)
-		dist_nh.param("input/laser_min_range", laser_min_range, 0.01);
-		dist_nh.param("input/laser_max_range", laser_max_range, 40.0);
+		dist_nh.param("input/laser_fov", laser_fov, 0.0);	// TODO
+		dist_nh.param("input/laser_min_range", laser_min_range, 1.0); // in centimeters
+		dist_nh.param("input/laser_max_range", laser_max_range, 4000.0);
 
 		dist_nh.param("laser_covariance", laser_covariance, 0);
 		dist_nh.param("output/laser_orientation", sonar_orientation, 0); // TODO: Check!
 		dist_nh.param("output/laser_id", laser_id, 0);
-		
-		// TODO: 
+
+		// TODO:
 		//		- variable topic advertising depending on the rangefinder type;
 		//		this means incrementing number of the publishing topics and change their names according to
 		//		the the type of sensor and its ID. p.e.: topics "laser_distance_1", "laser_distance_2"...
 
-		if(rangefinder_type == 0)
-			dist_laser_in_pub = dist_nh.advertise<mavros_extras::LaserDistanceSensor>("laser_distance", 10);
+		if (rangefinder_type == 0)
+			dist_laser_in_pub = dist_nh.advertise<mavros_extras::DistanceSensor>("laser_distance", 10);
 		else if (rangefinder_type == 1)
-			dist_sonar_in_pub = dist_nh.advertise<mavros_extras::SonarDistanceSensor>("sonar_distance", 10);
-		else ROS_ERROR_NAMED("rangefinder", "Invalid rangefinder type! Valid values are 0 (laser) and 1 (ultrasound)!");
-		
-		dist_laser_sub = dist_nh.subscribe("laser_range", 10, &DistanceSensorPlugin::laser_dist_sensor_cb, this);
-		dist_sonar_sub = dist_nh.subscribe("sonar_range", 10, &DistanceSensorPlugin::sonar_dist_sensor_cb, this);
-		
+			dist_sonar_in_pub = dist_nh.advertise<mavros_extras::DistanceSensor>("sonar_distance", 10);
+		else ROS_ERROR_NAMED("rangefinder_type", "Invalid rangefinder type! Valid values are 0 (laser) and 1 (ultrasound)!");
+
+		dist_sensor_sub = dist_nh.subscribe("range_data", 10, &DistanceSensorPlugin::dist_sensor_cb, this);
 	}
 
 	const message_map get_rx_handlers() {
 		return {
-			       MESSAGE_HANDLER(MAVLINK_MSG_ID_DISTANCE_SENSOR, &DistanceSensorPlugin::handle_dist_sensor)
+			       MESSAGE_HANDLER(MAVLINK_MSG_ID_DISTANCE_SENSOR, &DistanceSensorPlugin::handle_distance_sensor)
 		};
 	}
 
@@ -91,6 +88,7 @@ private:
 	std::string frame_id;
 
 	double sonar_fov;
+	double laser_fov;
 	double sonar_min_range;
 	double laser_min_range;
 	double sonar_max_range;
@@ -109,16 +107,15 @@ private:
 	ros::Publisher dist_sonar_in_pub;
 
 	/* -*- subscribers -*- */
-	ros::Subscriber dist_laser_sub;
-	ros::Subscriber dist_sonar_sub;
+	ros::Subscriber dist_sensor_sub;
 
 	/* -*- low-level send -*- */
 	void distance_sensor(uint32_t time_boot_ms,
-				uint32_t min_distance,
-				uint32_t max_distance,
-				uint32_t current_distance,
-				uint8_t type, uint8_t id,
-				uint8_t orientation, uint8_t covariance) {
+			uint32_t min_distance,
+			uint32_t max_distance,
+			uint32_t current_distance,
+			uint8_t type, uint8_t id,
+			uint8_t orientation, uint8_t covariance) {
 		mavlink_message_t msg;
 		mavlink_msg_distance_sensor_pack_chan(UAS_PACK_CHAN(uas), &msg,
 				time_boot_ms,
@@ -137,39 +134,27 @@ private:
 	/**
 	 * Receive distance sensor data from FCU.
 	 */
-	void handle_dist_sensor(const mavlink_message_t *msg, uint8_t sysid, uint8_t compid) {
+	void handle_distance_sensor(const mavlink_message_t *msg, uint8_t sysid, uint8_t compid) {
 		mavlink_distance_sensor_t dist_sen;
 		mavlink_msg_distance_sensor_decode(msg, &dist_sen);
 
 		std_msgs::Header header;
-		header.stamp = ros::Time::now();
+		header.stamp = uas->synchronise_stamp(dist_sen.time_boot_ms);
 		header.frame_id = frame_id;
 
-		sensor_msgs::Range sonar;
-		sensor_msgs::LaserScan laser;
+		sensor_msgs::Range range;
 
-		if (dist_sen.type == 0) { // Laser type
-			auto dist_laser_msg = boost::make_shared<mavros_extras::LaserDistanceSensor>();
+		if (dist_sen.type == 0) {	// Laser type
+			auto dist_laser_msg = boost::make_shared<mavros_extras::DistanceSensor>();
 
 			dist_laser_msg->header = header;
-			dist_laser_msg->time_boot_ms = dist_sen.time_boot_ms;
 
-			dist_laser_msg->laser.header = header;
-			dist_laser_msg->laser.angle_min = 0.0;
-			dist_laser_msg->laser.angle_max = 0.0;
-			dist_laser_msg->laser.angle_increment = 0.0;
-			dist_laser_msg->laser.time_increment = 0.0;
-			dist_laser_msg->laser.scan_time = 0.0;
-			dist_laser_msg->laser.range_min = laser_min_range/1E2; 
-			dist_laser_msg->laser.range_max = laser_max_range/1E2;
-			dist_laser_msg->laser.ranges[0] = dist_sen.current_distance/1E2;
-			
-			for (int i = 1; i < dist_laser_msg->laser.ranges.size(); i++) {         
-  				dist_laser_msg->laser.ranges[i] = 0.0;         
-			}
-			for (int f = 0; f < dist_laser_msg->laser.intensities.size(); f++) {     
-				dist_laser_msg->laser.intensities[f] = 0.0;
-			}
+			dist_laser_msg->range.header = header;
+			dist_laser_msg->range.radiation_type = sensor_msgs::Range::INFRARED; // FIXME: request LASER type for Range.msg
+			dist_laser_msg->range.field_of_view = laser_fov;
+			dist_laser_msg->range.min_range = laser_min_range;
+			dist_laser_msg->range.max_range = laser_max_range;
+			dist_laser_msg->range.range = dist_sen.current_distance;
 
 			dist_laser_msg->type = dist_sen.type;
 			dist_laser_msg->id = dist_sen.id;
@@ -181,19 +166,18 @@ private:
 			dist_laser_in_pub.publish(dist_laser_msg);
 		}
 
-		else if (dist_sen.type == 1) { // Sonar type
-			auto dist_sonar_msg = boost::make_shared<mavros_extras::SonarDistanceSensor>();
-			
+		else if (dist_sen.type == 1) {	// Sonar type
+			auto dist_sonar_msg = boost::make_shared<mavros_extras::DistanceSensor>();
+
 			dist_sonar_msg->header = header;
-			dist_sonar_msg->time_boot_ms = dist_sen.time_boot_ms;
-			
-			dist_sonar_msg->sonar.header = header;
-			dist_sonar_msg->sonar.radiation_type = sensor_msgs::Range::ULTRASOUND;
-			dist_sonar_msg->sonar.field_of_view = sonar_fov/1E2;
-			dist_sonar_msg->sonar.min_range = sonar_min_range/1E2;
-			dist_sonar_msg->sonar.max_range = sonar_max_range/1E2;
-			dist_sonar_msg->sonar.range = dist_sen.current_distance/1E2;
-			
+
+			dist_sonar_msg->range.header = header;
+			dist_sonar_msg->range.radiation_type = sensor_msgs::Range::ULTRASOUND;
+			dist_sonar_msg->range.field_of_view = sonar_fov;
+			dist_sonar_msg->range.min_range = sonar_min_range;
+			dist_sonar_msg->range.max_range = sonar_max_range;
+			dist_sonar_msg->range.range = dist_sen.current_distance;
+
 			dist_sonar_msg->type = dist_sen.type;
 			dist_sonar_msg->id = dist_sen.id;
 			dist_sonar_msg->orientation = dist_sen.orientation;
@@ -209,52 +193,51 @@ private:
 
 	// XXX TODO: Determine FOV for Sonar
 
+	// XXX TODO: Use the sensors orientation to publish a transform between the sensor frame and FCU frame
+
 	/**
 	 * Send laser rangefinder distance sensor data to FCU.
 	 */
 	void send_laser_dist_sensor_data(const ros::Time &stamp, uint16_t min_distance,
-								uint16_t max_distance, uint16_t current_distance) {
+			uint16_t max_distance, uint16_t current_distance) {
 		distance_sensor(stamp.toNSec() / 1000000,
-						min_distance,
-						max_distance,
-						current_distance,
-						0, laser_id,
-						laser_orientation,
-						sonar_covariance);	
+				min_distance,
+				max_distance,
+				current_distance,
+				0, laser_id,
+				laser_orientation,
+				sonar_covariance);
 	}
 
 	/**
 	 * Send laser rangefinder distance sensor data to FCU.
 	 */
 	void send_sonar_dist_sensor_data(const ros::Time &stamp, uint16_t min_distance,
-								uint16_t max_distance, uint16_t current_distance) {
+			uint16_t max_distance, uint16_t current_distance) {
 		distance_sensor(stamp.toNSec() / 1000000,
-						min_distance,
-						max_distance,
-						current_distance,
-						1, sonar_id,
-						sonar_orientation,
-						laser_covariance);	
+				min_distance,
+				max_distance,
+				current_distance,
+				1, sonar_id,
+				sonar_orientation,
+				laser_covariance);
 	}
 
 	/* -*- callbacks -*- */
-	void sonar_dist_sensor_cb(const sensor_msgs::Range::ConstPtr &req) {
-		send_sonar_dist_sensor_data(req->header.stamp,
-								req->min_range*1E-2,
-								req->max_range*1E-2,
-								req->range*1E-2);
+	void dist_sensor_cb(const sensor_msgs::Range::ConstPtr &req) {
+		if (req->radiation_type == 0) {
+			send_sonar_dist_sensor_data(req->header.stamp,
+					req->min_range * 1E-2,
+					req->max_range * 1E-2,
+					req->range * 1E-2);
+		}
+		else if (req->radiation_type == 1) {
+			send_laser_dist_sensor_data(req->header.stamp,
+					req->min_range * 1E-2,
+					req->max_range * 1E-2,
+					req->range * 1E-2);
+		}
 	}
-
-	/**
-	 * This is only applied to a fixed laser rangefinder, without angle variation.
-	 */
-	void laser_dist_sensor_cb(const sensor_msgs::LaserScan::ConstPtr &req) {
-		send_laser_dist_sensor_data(req->header.stamp,
-								req->range_min*1E-2,
-								req->range_max*1E-2,
-								req->ranges[0]*1E-2);
-	}
-
 };
 };	// namespace mavplugin
 
