6095ca5023aabf5188d41237fe061da0be1bb360
Mohammed Kabir <mhkabir98@gmail.com>
2015-06-16 17:59:56 +0530

    command handling in mavcmd for camera trigger

--- mavros/src/plugins/command.cpp
+++ mavros/src/plugins/command.cpp
@@ -322,9 +322,10 @@ private:
    
         bool trigger_control_cb(mavros::CommandTriggerControl::Request &req,
 			mavros::CommandTriggerControl::Response &res) {
-		return send_command_long_and_wait(MAV_CMD_DO_TRIGGER_CONTROL, (req.trigger_enable)? 1.0 : 0.0,
+		return send_command_long_and_wait(MAV_CMD_DO_TRIGGER_CONTROL, 1,
+				(req.trigger_enable)? 1.0 : 0.0,
 				req.integration_time,
-				0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0,
 				res.success, res.result);
 	}
 };
