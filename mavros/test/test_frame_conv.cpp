393d290ae222e69b13d601f3fb2c51a37f3e9ad5
Vladimir Ermakov <vooon341@gmail.com>
2015-06-29 18:27:11 +0300

    test: test for UAS::transform_frame_xyz()

--- mavros/test/test_frame_conv.cpp
+++ mavros/test/test_frame_conv.cpp
@@ -7,6 +7,51 @@
 #include <ros/ros.h>
 #include <mavros/mavros_uas.h>
 
+using mavros::UAS;
+
+
+/* -*- test general Vector3 transform function -*- */
+
+TEST(VECTOR, transform_frame_xyz_111)
+{
+	tf::Vector3 input(1, 1, 1);
+	tf::Vector3 expected_out(1, -1, -1);
+
+	auto out = UAS::transform_frame_xyz(input.x(), input.y(), input.z());
+	EXPECT_EQ(expected_out, out);
+}
+
+TEST(VECTOR, transform_frame_xyz_pi00)
+{
+	tf::Vector3 input(M_PI, 0, 0);
+	tf::Vector3 expected_out(M_PI, 0, 0);
+
+	auto out = UAS::transform_frame_xyz(input.x(), input.y(), input.z());
+	EXPECT_EQ(expected_out, out);
+}
+
+TEST(VECTOR, transform_frame_xyz_0pi0)
+{
+	tf::Vector3 input(0, M_PI, 0);
+	tf::Vector3 expected_out(0, -M_PI, 0);
+
+	auto out = UAS::transform_frame_xyz(input.x(), input.y(), input.z());
+	EXPECT_EQ(expected_out, out);
+}
+
+TEST(VECTOR, transform_frame_xyz_00pi)
+{
+	tf::Vector3 input(0, 0, M_PI);
+	tf::Vector3 expected_out(0, 0, -M_PI);
+
+	auto out = UAS::transform_frame_xyz(input.x(), input.y(), input.z());
+	EXPECT_EQ(expected_out, out);
+}
+
+
+/* -*- test attitude RPY transform -*- */
+
+
 
 int main(int argc, char **argv)
 {
