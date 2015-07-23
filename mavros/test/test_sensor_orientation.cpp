b66bcb0a803616d8716ee2e447a6dcbdd73a7606
Vladimir Ermakov <vooon341@gmail.com>
2015-07-23 18:58:01 +0300

    test: add test cases for new sensor orientation functions

--- mavros/test/test_sensor_orientation.cpp
+++ mavros/test/test_sensor_orientation.cpp
@@ -43,6 +43,45 @@ TEST(UAS, sensor_orientation_matching__roll_180_yaw_90)
 	EXPECT_QUATERNION(expected, out);
 }
 
+TEST(UAS, str_sensor_orientation__none)
+{
+	EXPECT_EQ("NONE", UAS::str_sensor_orientation(MAV_SENSOR_ROTATION_NONE));
+}
+
+TEST(UAS, str_sensor_orientation__roll_180)
+{
+	EXPECT_EQ("ROLL_180", UAS::str_sensor_orientation(MAV_SENSOR_ROTATION_ROLL_180));
+}
+
+TEST(UAS, str_sensor_orientation__roll_180_yaw_90)
+{
+	EXPECT_EQ("ROLL_180_YAW_90", UAS::str_sensor_orientation(MAV_SENSOR_ROTATION_ROLL_180_YAW_90));
+}
+
+TEST(UAS, orientation_from_str__none)
+{
+	EXPECT_EQ(MAV_SENSOR_ROTATION_NONE, UAS::orientation_from_str("NONE"));
+}
+
+TEST(UAS, orientation_from_str__unknown)
+{
+	EXPECT_LT(UAS::orientation_from_str("completely wrong identificator"), 0);
+}
+
+TEST(UAS, orientation_from_str__roll_180)
+{
+	EXPECT_EQ(MAV_SENSOR_ROTATION_ROLL_180, UAS::orientation_from_str("ROLL_180"));
+}
+
+TEST(UAS, orientation_from_str__roll_180_yaw_90)
+{
+	EXPECT_EQ(MAV_SENSOR_ROTATION_ROLL_180_YAW_90, UAS::orientation_from_str("ROLL_180_YAW_90"));
+}
+
+TEST(UAS, orientation_from_str__last_element_rpy_315)
+{
+	EXPECT_EQ(MAV_SENSOR_ROTATION_ROLL_315_PITCH_315_YAW_315, UAS::orientation_from_str("ROLL_315_PITCH_315_YAW_315"));
+}
 
 int main(int argc, char **argv)
 {
