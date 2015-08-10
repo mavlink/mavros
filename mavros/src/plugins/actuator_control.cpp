258e2996e5daec2900f348f1ec8b9c2dd7047433
Vladimir Ermakov <vooon341@gmail.com>
2015-08-10 17:04:31 +0300

    plugin: actuator_control: Fix message include.

--- mavros/src/plugins/actuator_control.cpp
+++ mavros/src/plugins/actuator_control.cpp
@@ -17,7 +17,7 @@
 #include <mavros/mavros_plugin.h>
 #include <pluginlib/class_list_macros.h>
 
-#include <mavros/ActuatorControl.h>
+#include <mavros_msgs/ActuatorControl.h>
 
 namespace mavplugin {
 /**
@@ -67,7 +67,7 @@ private:
 
 	/* -*- callbacks -*- */
 
-	void actuator_control_cb(const mavros::ActuatorControl::ConstPtr &req) {
+	void actuator_control_cb(const mavros_msgs::ActuatorControl::ConstPtr &req) {
 		//! about groups, mixing and channels: @p https://pixhawk.org/dev/mixing
 		set_actuator_control_target(ros::Time::now().toNSec() / 1000,
 				req->group_mix,
