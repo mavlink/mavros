a29367e636b28466d64370c3c22c49f5d1081197
Vladimir Ermakov <vooon341@gmail.com>
2015-09-09 19:58:39 +0300

    scripts: starting event_launcher

---
+++ mavros/src/mavros/event_launcher.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# vim:set ts=4 sw=4 et:
+#
+# Copyright 2015 Vladimir Ermakov.
+#
+# This file is part of the mavros package and subject to the license terms
+# in the top-level LICENSE file of the mavros repository.
+# https://github.com/mavlink/mavros/tree/master/LICENSE.md
+
+from __future__ import print_function
+
+import rospy
+
+
+def main():
+    rospy.init_node("event_launcher")
+
+    rospy.loginfo("Starting event launcher...")
