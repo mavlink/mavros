07b9f79d1f2cb48e641ee6258a0f626f2a51c497
Vladimir Ermakov <vooon341@gmail.com>
2015-09-09 23:42:31 +0300

    script #385: shell handler done. next - rosparam handling

--- mavros/src/mavros/event_launcher.py
+++ mavros/src/mavros/event_launcher.py
@@ -49,6 +49,9 @@ class EventHandler(object):
             rospy.logerr("Misconfiguration of '{}', there no action '{}'".format(
                 str(self), action))
 
+    def spin_once(self):
+        raise NotImplementedError
+
 
 class ShellHandler(EventHandler):
     """
@@ -68,21 +71,74 @@ class ShellHandler(EventHandler):
             rospy.loginfo("Process %s still active, terminating before run", str(self))
             self.action_stop()
 
-        cmd = [self.command] + self.args
+        rospy.loginfo("Starting %s", self)
+
+        args = [self.command] + self.args
+        rospy.logdebug("Starting subprocess: %s", ' '.join(args))
+
+        child_stdout = subprocess.PIPE
+        child_stderr = subprocess.PIPE
+
+        self.process = subprocess.Popen(args, stdout=child_stdout, stderr=child_stderr, close_fds=False)
+
+        poll_result = self.process.poll()
+        if poll_result is None or poll_result == 0:
+            rospy.loginfo("Process '%s' started, pid: %s", self, self.process.pid)
+        else:
+            rospy.logerr("Failed to start '%s'", self)
+            self.process = None
 
     def action_stop(self):
-        pass
+        if self.process:
+            self._log_messages()
+            rospy.loginfo("Stopping %s", self)
+            self.process.terminate()
+            self.process = None
+
+    def _log_messages(self):
+        for msg in self.process.stdout.readlines():
+            rospy.loginfo("%s: %s", self, msg.strip())
+
+        for msg in self.process.stderr.readlines():
+            rospy.logerr("%s: %s", self, msg.strip())
+
+    def spin_once(self):
+        if self.process:
+            self._log_messages()
+
+            poll_result = self.process.poll()
+            if poll_result is not None:
+                rospy.loginfo("Process %s (pid %s) terminated, exit code: %s", self, self.process.pid, poll_result)
+                self.process = None
 
 
 class RosrunHandler(EventHandler):
-    pass
+    def __call__(self, event):
+        raise NotImplementedError
 
 
 class RoslaunchHandler(EventHandler):
-    pass
+    def __call__(self, event):
+        raise NotImplementedError
+
 
 
 def main():
     rospy.init_node("event_launcher")
 
     rospy.loginfo("Starting event launcher...")
+
+    handlers = []
+
+    # TODO: load config from rosparam
+
+    if not handlers:
+        rospy.logwarn("No event handlers defined, terminating.")
+        return
+
+    rate = rospy.Rate(1.0)
+    while not rospy.is_shutdown():
+        for h in handlers:
+            h.spin_once()
+
+        rate.sleep()
