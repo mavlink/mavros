4da392a9386d20cc0c3c5860d42b00524fa2e8f9
Vladimir Ermakov <vooon341@gmail.com>
2015-09-09 21:33:18 +0300

    script #385: starting work on simple shell launcher

--- mavros/src/mavros/event_launcher.py
+++ mavros/src/mavros/event_launcher.py
@@ -10,6 +10,76 @@
 from __future__ import print_function
 
 import rospy
+import subprocess
+from roslaunch.scriptapi import ROSLaunch, Node
+
+from std_srvs.srv import Trigger, TriggerRequest, TriggerResponse
+from mavros_msgs.msg import State
+
+
+class EventHandler(object):
+    """
+    Base class for event handlers
+    """
+
+    __slots__ = [
+        'name', 'process', 'args', 'events', 'actions'
+    ]
+
+    def __init__(self, name, args=[], events=[], actions=[]):
+        self.name = name
+        self.process = None
+        self.args = args
+        self.events = events
+        self.actions = actions
+
+    def __str__(self):
+        return self.name
+
+    def __call__(self, event):
+        if event not in self.events:
+            return  # not our event
+
+        idx = self.events.index(event)
+        action = self.actions[idx]
+
+        if hasattr(self, 'action_' + action):
+            getattr(self, 'action_' + action)()
+        else:
+            rospy.logerr("Misconfiguration of '{}', there no action '{}'".format(
+                str(self), action))
+
+
+class ShellHandler(EventHandler):
+    """
+    Simple program runner
+    """
+
+    __slots__ = [
+        'command'
+    ]
+
+    def __init__(self, name, command, args=[], events=[], actions=[]):
+        super(ShellHandler, self).__init__(name, args, events, actions)
+        self.command = command
+
+    def action_run(self):
+        if self.process:
+            rospy.loginfo("Process %s still active, terminating before run", str(self))
+            self.action_stop()
+
+        cmd = [self.command] + self.args
+
+    def action_stop(self):
+        pass
+
+
+class RosrunHandler(EventHandler):
+    pass
+
+
+class RoslaunchHandler(EventHandler):
+    pass
 
 
 def main():
