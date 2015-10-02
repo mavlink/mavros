e16daed2193097f451e75532fe0b46c41a19756d
Vladimir Ermakov <vooon341@gmail.com>
2015-10-02 20:38:24 +0300

    python: convert input to bytearray

--- mavros/src/mavros/mavlink.py
+++ mavros/src/mavros/mavlink.py
@@ -53,12 +53,12 @@ def convert_to_payload64(payload_bytes):
     """
     Convert payload bytes to Mavlink.payload64
     """
-
+    payload_bytes = bytearray(payload_bytes)
     payload_len = len(payload_bytes)
     payload_octets = payload_len / 8
     if payload_len % 8 > 0:
         payload_octets += 1
-        payload_bytes += [0] * (8 - payload_len % 8)
+        payload_bytes += b'\0' * (8 - payload_len % 8)
 
-    return struct.unpack('<%dQ' % payload_octets, bytearray(payload_bytes))
+    return struct.unpack('<%dQ' % payload_octets, payload_bytes)
 
