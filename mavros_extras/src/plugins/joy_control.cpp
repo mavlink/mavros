4a8e0fd078d2b405e416d4cff5520ad658a8a7f8
francois <administrador@CU-FCHATAIGNER>
2016-03-01 18:11:26 +0100

    updated after review by vooon

--- mavros_extras/src/plugins/joy_control.cpp
+++ mavros_extras/src/plugins/joy_control.cpp
@@ -7,6 +7,8 @@
  * @{
  */
 /*
+ * Copyright 2016 Francois Chataigner.
+ *
  * This file is part of the mavros package and subject to the license terms
  * in the top-level LICENSE file of the mavros repository.
  * https://github.com/mavlink/mavros/tree/master/LICENSE.md
@@ -22,50 +24,39 @@
 #include <mavros_msgs/State.h>
 #include <mavros_msgs/SetMode.h>
 
-#include <thread>
-#include <mutex>
 using namespace std;
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-template<typename Service>
-bool call_service( const string& service_name, const typename Service::Request& req )
-{
-	if ( !ros::service::exists( service_name, true ) )
-		return false;
-
-	Service cmd;
-	cmd.request = req;
-
-	bool success = ros::service::call( service_name, cmd ) && cmd.response.success;
-	ROS_DEBUG_STREAM( "calling service=" << service_name << " success=" << success );
-	return success;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-
 namespace mavplugin {
+struct Control
+{
+	int axis;
+	double scale;
+	double offset;
+	double deadzone;
+
+	double map_axis( const sensor_msgs::Joy::ConstPtr& joy ) const
+	{
+		if ( axis < 0 || axis >= joy->axes.size() ) throw std::out_of_range("failed to map axis");
+		double val = joy->axes[axis] + offset;
+		return ( val > deadzone || val < -deadzone ? val * scale : 0.0 );
+	}
+};
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
 /**
  * @brief Joystick Control plugin
  */
 class JoyControlPlugin : public MavRosPlugin
 {
-	struct Control
-	{
-		int axis;
-		double scale;
-		double offset;
-		double deadzone;
-
-		double map_axis( const sensor_msgs::Joy::ConstPtr& joy ) const
-		{
-			if ( axis < 0 || axis >= joy->axes.size() ) throw std::out_of_range("failed to map axis");
-			double val = joy->axes[axis] + offset;
-			return ( val > deadzone || val < -deadzone ? val * scale : 0.0 );
-		}
-	};
+	ros::NodeHandle mavros_nh_, plugin_nh_;
+	UAS *uas_;
+	mavros_msgs::State state_;
+	mavlink_message_t ctrl_msg_;
 
 	Control x_, y_, z_, r_;
 
@@ -73,59 +64,62 @@ class JoyControlPlugin : public MavRosPlugin
 	vector<int> mode_mapping_;
 	vector<string> mode_names_;
 
-	string arming_service_;
-	string mode_service_;
+	ros::Timer send_msg_timer_;
+	ros::ServiceClient arming_srv_, set_mode_srv_;
+	ros::Subscriber joystick_sub_, state_sub_;
 
 public:
-	JoyControlPlugin() :
-		nh_("~joy_control"),
-		uas_(nullptr) {}
+	JoyControlPlugin()
+		: mavros_nh_("~")
+		, plugin_nh_("~joy_control")
+		, uas_(nullptr) {}
 
 	void initialize(UAS &uas )
 	{
 		uas_ = &uas;
 
-		int rc_rate_hz;
+		int msg_rate;
 
-		nh_.param("rc_rate_hz",     rc_rate_hz,      10 );
-
-		nh_.param("arming_service", arming_service_, string("/mavros/cmd/arming") );
-		nh_.param("mode_service",   mode_service_,   string("/mavros/set_mode") );
+		plugin_nh_.param<int>("msg_rate", msg_rate, 10 );
+		plugin_nh_.param<int>("arm_button", arm_button_, 2 );
 
 		// Mode switches on remotes can be reported a single flag, or as a binary
 		// combination of several flags: eg. mode1=[0,0]=0, mode2=[1,0]=1, mode3=[0,1]=2
 		// mode_mapping={3,4} means a binary combination of the 4th and 5th values
 		// in the sensor_msgs::Joy buttons array
 
-		nh_.param("mode_mapping",   mode_mapping_,  {3,4} );
-		nh_.param("mode_names",     mode_names_,    {"MANUAL","ALTCTL","OFFBOARD"} );
+		plugin_nh_.param< vector<int> >("mode_mapping", mode_mapping_, {3,4} );
+		plugin_nh_.param< vector<string> >("mode_names", mode_names_, {"MANUAL","ALTCTL","OFFBOARD"} );
 
-		nh_.param("arm_button",     arm_button_,    2   );
+		plugin_nh_.param<int>      ("x_axis",       x_.axis,        1   );
+		plugin_nh_.param<double>   ("x_scale",      x_.scale,       1.0 );
+		plugin_nh_.param<double>   ("x_offset",     x_.offset,      0.0 );
+		plugin_nh_.param<double>   ("x_deadzone",   x_.deadzone,    0.2 );
 
-		nh_.param("x_axis",         x_.axis,        1   );
-		nh_.param("x_scale",        x_.scale,       1.0 );
-		nh_.param("x_offset",       x_.offset,      0.0 );
-		nh_.param("x_deadzone",     x_.deadzone,    0.2 );
+		plugin_nh_.param<int>      ("y_axis",       y_.axis,        0   );
+		plugin_nh_.param<double>   ("y_scale",      y_.scale,       1.0 );
+		plugin_nh_.param<double>   ("y_offset",     y_.offset,      0.0 );
+		plugin_nh_.param<double>   ("y_deadzone",   y_.deadzone,    0.2 );
 
-		nh_.param("y_axis",         y_.axis,        0   );
-		nh_.param("y_scale",        y_.scale,       1.0 );
-		nh_.param("y_offset",       y_.offset,      0.0 );
-		nh_.param("y_deadzone",     y_.deadzone,    0.2 );
+		plugin_nh_.param<int>      ("z_axis",       z_.axis,        2   );
+		plugin_nh_.param<double>   ("z_scale",      z_.scale,       1.0 );
+		plugin_nh_.param<double>   ("z_offset",     z_.offset,      0.0 );
+		plugin_nh_.param<double>   ("z_deadzone",   z_.deadzone,    0.0 );
 
-		nh_.param("z_axis",         z_.axis,        2   );
-		nh_.param("z_scale",        z_.scale,       1.0 );
-		nh_.param("z_offset",       z_.offset,      0.0 );
-		nh_.param("z_deadzone",     z_.deadzone,    0.0 );
+		plugin_nh_.param<int>      ("r_axis",       r_.axis,        4   );
+		plugin_nh_.param<double>   ("r_scale",      r_.scale,       1.0 );
+		plugin_nh_.param<double>   ("r_offset",     r_.offset,      0.0 );
+		plugin_nh_.param<double>   ("r_deadzone",   r_.deadzone,    0.2 );
 
-		nh_.param("r_axis",         r_.axis,        4   );
-		nh_.param("r_scale",        r_.scale,       1.0 );
-		nh_.param("r_offset",       r_.offset,      0.0 );
-		nh_.param("r_deadzone",     r_.deadzone,    0.2 );
+		arming_srv_ = mavros_nh_.serviceClient<mavros_msgs::CommandBool>("cmd/arming");
+		set_mode_srv_ = mavros_nh_.serviceClient<mavros_msgs::SetMode>("set_mode");
 
-		joystick_sub_ = nh_.subscribe("/joy", 10, &JoyControlPlugin::handle_joystick, this );
-		state_sub_ = nh_.subscribe("/mavros/state", 10, &JoyControlPlugin::handle_state, this );
+		arming_srv_.waitForExistence();
+		//set_mode_srv_.waitForExistence();
 
-		thread_.reset( new std::thread( &JoyControlPlugin::thread_call, this, rc_rate_hz ) );
+		joystick_sub_ = mavros_nh_.subscribe("joy_topic", 10, &JoyControlPlugin::handle_joystick, this );
+		state_sub_ = mavros_nh_.subscribe("state", 10, &JoyControlPlugin::handle_state, this );
+		send_msg_timer_ = mavros_nh_.createTimer( ros::Rate(msg_rate), &JoyControlPlugin::send_message, this );
 	}
 
 	const message_map get_rx_handlers() {
@@ -133,34 +127,6 @@ public:
 	}
 
 private:
-	ros::NodeHandle nh_;
-	UAS *uas_;
-	ros::Subscriber joystick_sub_, state_sub_;
-	mavros_msgs::State state_;
-
-	std::mutex msg_mutex_;
-	shared_ptr<std::thread> thread_;
-	mavlink_message_t ctrl_msg_;
-
-	////////////////////////////////////////////////////////////////////////////
-	////////////////////////////////////////////////////////////////////////////
-
-	void thread_call( float rate_hz )
-	{
-		ros::Rate rate(rate_hz);
-
-		// We need to keep sending messages at a fixed
-		// rate or PX4 will trigger RC timeouts
-
-		while (1)
-		{
-			msg_mutex_.lock();
-			UAS_FCU(uas_)->send_message(&ctrl_msg_);
-			msg_mutex_.unlock();
-
-			rate.sleep();
-		}
-	}
 
 	////////////////////////////////////////////////////////////////////////////
 	////////////////////////////////////////////////////////////////////////////
@@ -194,15 +160,20 @@ private:
 			float z = z_.map_axis( joy );
 			float r = r_.map_axis( joy );
 
-			std::lock_guard<std::mutex> lock(msg_mutex_);
-
 			mavlink_msg_manual_control_pack_chan( UAS_PACK_CHAN(uas_), &ctrl_msg_,
 					(uas_)->get_tgt_system(), x, y, z, r, array_to_bitset(joy->buttons) );
 		}
 		catch ( const std::out_of_range& e )
 		{
-			ROS_WARN( "failed to process joystick input: %s", e.what() );
+			ROS_ERROR( "failed to process joystick input: %s", e.what() );
 		}
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+
+	void send_message( const ros::TimerEvent& ) {
+		UAS_FCU(uas_)->send_message(&ctrl_msg_);
 	}
 
 	////////////////////////////////////////////////////////////////////////////
@@ -217,15 +188,15 @@ private:
 
 		if ( arm_button_on && !state_.armed )
 		{
-			mavros_msgs::CommandBool::Request req;
-			req.value = true;
-			call_service<mavros_msgs::CommandBool>( arming_service_, req );
+			mavros_msgs::CommandBool arming;
+			arming.request.value = true;
+			arming_srv_.call(arming);
 		}
 		else if ( arm_button_on && state_.armed )
 		{
-			mavros_msgs::CommandBool::Request req;
-			req.value = false;
-			call_service<mavros_msgs::CommandBool>( arming_service_, req );
+			mavros_msgs::CommandBool arming;
+			arming.request.value = false;
+			arming_srv_.call(arming);
 		}
 	}
 
@@ -242,13 +213,13 @@ private:
 		for ( int i = 0; i < mode_mapping_.size(); ++i )
 			mode_idx += pow(2,i) * joy->buttons.at( mode_mapping_[i] );
 
-		static mavros_msgs::SetMode::Request req;
-		req.custom_mode = mode_names_.at(mode_idx);
+		static mavros_msgs::SetMode set_mode;
+		set_mode.request.custom_mode = mode_names_.at(mode_idx);
 
-		if ( req.custom_mode != state_.mode )
+		if ( set_mode.request.custom_mode != state_.mode )
 		{
-			ROS_INFO_STREAM("setting mode=" << req.custom_mode);
-			call_service<mavros_msgs::SetMode>( mode_service_, req );
+			ROS_INFO_STREAM("setting custom mode=" << set_mode.request.custom_mode);
+			set_mode_srv_.call( set_mode );
 		}
 	}
 
