f591167af274e02298954e19ec23ee16301cfed4
Pavel <pvechersky@gmail.com>
2016-06-22 18:56:14 +0200

    Ran uncrustify on hil_controls plugin

--- mavros/src/plugins/hil_controls.cpp
+++ mavros/src/plugins/hil_controls.cpp
@@ -25,52 +25,52 @@ namespace mavplugin {
  */
 class HilControlsPlugin : public MavRosPlugin {
 public:
-    HilControlsPlugin() :
-        hil_controls_nh("~hil_controls"),
-        uas(nullptr)
-    { };
+	HilControlsPlugin() :
+		hil_controls_nh("~hil_controls"),
+		uas(nullptr)
+	{ };
 
-    void initialize(UAS &uas_)
-    {
-        uas = &uas_;
+	void initialize(UAS &uas_)
+	{
+		uas = &uas_;
 
-        hil_controls_pub = hil_controls_nh.advertise<mavros_msgs::HilControls>("hil_controls", 10);
-    };
+		hil_controls_pub = hil_controls_nh.advertise<mavros_msgs::HilControls>("hil_controls", 10);
+	};
 
-    const message_map get_rx_handlers() {
-        return {
-                   MESSAGE_HANDLER(MAVLINK_MSG_ID_HIL_CONTROLS, &HilControlsPlugin::handle_hil_controls),
-        };
-    }
+	const message_map get_rx_handlers() {
+		return {
+			       MESSAGE_HANDLER(MAVLINK_MSG_ID_HIL_CONTROLS, &HilControlsPlugin::handle_hil_controls),
+		};
+	}
 
 private:
-    ros::NodeHandle hil_controls_nh;
-    UAS *uas;
+	ros::NodeHandle hil_controls_nh;
+	UAS *uas;
 
-    ros::Publisher hil_controls_pub;
+	ros::Publisher hil_controls_pub;
 
-    /* -*- rx handlers -*- */
+	/* -*- rx handlers -*- */
 
-    void handle_hil_controls(const mavlink_message_t *msg, uint8_t sysid, uint8_t compid) {
-        mavlink_hil_controls_t hil_controls;
-        mavlink_msg_hil_controls_decode(msg, &hil_controls);
+	void handle_hil_controls(const mavlink_message_t *msg, uint8_t sysid, uint8_t compid) {
+		mavlink_hil_controls_t hil_controls;
+		mavlink_msg_hil_controls_decode(msg, &hil_controls);
 
-        auto hil_controls_msg = boost::make_shared<mavros_msgs::HilControls>();
+		auto hil_controls_msg = boost::make_shared<mavros_msgs::HilControls>();
 
-        hil_controls_msg->header.stamp = uas->synchronise_stamp(hil_controls.time_usec);
-        hil_controls_msg->roll_ailerons = hil_controls.roll_ailerons;
-        hil_controls_msg->pitch_elevator = hil_controls.roll_ailerons;
-        hil_controls_msg->yaw_rudder = hil_controls.yaw_rudder;
-        hil_controls_msg->throttle = hil_controls.throttle;
-        hil_controls_msg->aux1 = hil_controls.aux1;
-        hil_controls_msg->aux2 = hil_controls.aux2;
-        hil_controls_msg->aux3 = hil_controls.aux3;
-        hil_controls_msg->aux4 = hil_controls.aux4;
-        hil_controls_msg->mode = hil_controls.mode;
-        hil_controls_msg->nav_mode = hil_controls.nav_mode;
+		hil_controls_msg->header.stamp = uas->synchronise_stamp(hil_controls.time_usec);
+		hil_controls_msg->roll_ailerons = hil_controls.roll_ailerons;
+		hil_controls_msg->pitch_elevator = hil_controls.roll_ailerons;
+		hil_controls_msg->yaw_rudder = hil_controls.yaw_rudder;
+		hil_controls_msg->throttle = hil_controls.throttle;
+		hil_controls_msg->aux1 = hil_controls.aux1;
+		hil_controls_msg->aux2 = hil_controls.aux2;
+		hil_controls_msg->aux3 = hil_controls.aux3;
+		hil_controls_msg->aux4 = hil_controls.aux4;
+		hil_controls_msg->mode = hil_controls.mode;
+		hil_controls_msg->nav_mode = hil_controls.nav_mode;
 
-        hil_controls_pub.publish(hil_controls_msg);
-    }
+		hil_controls_pub.publish(hil_controls_msg);
+	}
 };
 };	// namespace mavplugin
 
