a445f38a002888743a240508c1e834463f5d4bd3
Mohamed Abdelkader Zahana <mohamedashraf123@gmail.com>
2016-12-17 17:34:10 +0300

    increase number of fake satellites

--- mavros_extras/src/plugins/mocap_fake_gps.cpp
+++ mavros_extras/src/plugins/mocap_fake_gps.cpp
@@ -145,7 +145,7 @@ private:
 		pos.eph = 2;
 		pos.epv = 2;
 		pos.fix_type = 3;
-		pos.satellites_visible = 5;
+		pos.satellites_visible = 10;
 		UAS_FCU(m_uas)->send_message_ignore_drop(pos);
 	}
 };
