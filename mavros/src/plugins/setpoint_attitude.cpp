1f01ab89932a6ea6713c15c8552c1fbf97f90f31
TSC21 <n.marques21@hotmail.com>
2017-05-02 14:17:34 +0300

    plugins: setpoint_attitude: add sync between throttle and attitude topics to be sent together

--- mavros/src/plugins/setpoint_attitude.cpp
+++ mavros/src/plugins/setpoint_attitude.cpp
@@ -19,9 +19,13 @@
 #include <mavros/setpoint_mixin.h>
 #include <eigen_conversions/eigen_msg.h>
 
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/TwistStamped.h>
-#include <std_msgs/Float64.h>
+#include <mavros_msgs/Float32Stamped.h>
 
 namespace mavros {
 namespace std_plugins {
@@ -37,6 +41,7 @@ public:
 	SetpointAttitudePlugin() : PluginBase(),
 		sp_nh("~setpoint_attitude"),
 		tf_rate(10.0),
+		use_attitude(true),
 		reverse_throttle(false)
 	{ }
 
@@ -47,6 +52,7 @@ public:
 		bool tf_listen;
 
 		// main params
+		sp_nh.param("use_attitude", use_attitude, true);
 		sp_nh.param("reverse_throttle", reverse_throttle, false);
 		// tf params
 		sp_nh.param("tf/listen", tf_listen, false);
@@ -56,16 +62,25 @@ public:
 
 		if (tf_listen) {
 			ROS_INFO_STREAM_NAMED("attitude",
-					"Listen to desired attitude transform "
-					<< tf_frame_id << " -> " << tf_child_frame_id);
+						"Listen to desired attitude transform "
+						<< tf_frame_id << " -> " << tf_child_frame_id);
 			tf2_start("AttitudeSpTF", &SetpointAttitudePlugin::transform_cb);
+
+			//TODO: implement tf2_ros::MessageFilter< M >
+		}
+		else if (use_attitude) {
+			//use message_filters to sync attitude and throttle msg coming from different topics
+			message_filters::Subscriber<geometry_msgs::PoseStamped> pose_sub(sp_nh, "attitude", 1);
+			message_filters::Subscriber<mavros_msgs::Float32Stamped> throttle_sub(sp_nh, "throttle", 1);
+
+			// matches messages, even if they have different time stamps, by using an adaptative algorithm <http://wiki.ros.org/message_filters/ApproximateTime>
+			typedef message_filters::sync_policies::ApproximateTime<geometry_msgs::PoseStamped, mavros_msgs::Float32Stamped> syncp;
+			message_filters::Synchronizer<syncp> sync(syncp(10), pose_sub, throttle_sub);
+			sync.registerCallback(boost::bind(&SetpointAttitudePlugin::attitude_pose_cb, this, _1, _2));
 		}
 		else {
 			twist_sub = sp_nh.subscribe("cmd_vel", 10, &SetpointAttitudePlugin::twist_cb, this);
-			pose_sub = sp_nh.subscribe("attitude", 10, &SetpointAttitudePlugin::pose_cb, this);
 		}
-
-		throttle_sub = sp_nh.subscribe("att_throttle", 10, &SetpointAttitudePlugin::throttle_cb, this);
 	}
 
 	Subscriptions get_subscriptions()
@@ -79,42 +94,37 @@ private:
 	ros::NodeHandle sp_nh;
 
 	ros::Subscriber twist_sub;
-	ros::Subscriber pose_sub;
-	ros::Subscriber throttle_sub;
 
 	std::string tf_frame_id;
 	std::string tf_child_frame_id;
 	double tf_rate;
+	bool use_attitude;
 	bool reverse_throttle;
 
 	/* -*- mid-level helpers -*- */
 
 	/**
-	 * @brief Send attitude setpoint to FCU attitude controller
-	 *
-	 * @note ENU frame.
+	 * @brief Send attitude setpoint and respective thrust to FCU attitude controller
 	 */
-	void send_attitude_target(const ros::Time &stamp, const Eigen::Affine3d &tr)
+	void send_attitude_target(const ros::Time &stamp, const Eigen::Affine3d &tr, const float throttle)
 	{
-		/* Thrust + RPY, also bits numbering started from 1 in docs
+		/* RPY, also bits numbering started from 1 in docs
 		 */
-		const uint8_t ignore_all_except_q = (1 << 6) | (7 << 0);
+		const uint8_t ignore_all_except_q_and_thrust = (7 << 0);
 
 		auto q = ftf::transform_orientation_enu_ned(
 					ftf::transform_orientation_baselink_aircraft(Eigen::Quaterniond(tr.rotation()))
 					);
 
 		set_attitude_target(stamp.toNSec() / 1000000,
-				ignore_all_except_q,
-				q,
-				Eigen::Vector3d::Zero(),
-				0.0);
+					ignore_all_except_q_and_thrust,
+					q,
+					Eigen::Vector3d::Zero(),
+					throttle);
 	}
 
 	/**
 	 * @brief Send angular velocity setpoint to FCU attitude controller
-	 *
-	 * @note ENU frame.
 	 */
 	void send_attitude_ang_velocity(const ros::Time &stamp, const Eigen::Vector3d &ang_vel)
 	{
@@ -125,41 +135,36 @@ private:
 		auto av = ftf::transform_frame_baselink_aircraft(ang_vel);
 
 		set_attitude_target(stamp.toNSec() / 1000000,
-				ignore_all_except_rpy,
-				Eigen::Quaterniond::Identity(),
-				av,
-				0.0);
-	}
-
-	/**
-	 * @brief Send throttle to FCU attitude controller
-	 */
-	void send_attitude_throttle(const float throttle)
-	{
-		// Q + RPY
-		const uint8_t ignore_all_except_throttle = (1 << 7) | (7 << 0);
-
-		set_attitude_target(ros::Time::now().toNSec() / 1000000,
-				ignore_all_except_throttle,
-				Eigen::Quaterniond::Identity(),
-				Eigen::Vector3d::Zero(),
-				throttle);
+					ignore_all_except_rpy,
+					Eigen::Quaterniond::Identity(),
+					av,
+					0.0);
 	}
 
 	/* -*- callbacks -*- */
 
-	void transform_cb(const geometry_msgs::TransformStamped &transform) {
+	void transform_cb(const geometry_msgs::TransformStamped &transform) {	//TODO: Replace this function with a sync one
 		Eigen::Affine3d tr;
 		tf::transformMsgToEigen(transform.transform, tr);
 
-		send_attitude_target(transform.header.stamp, tr);
+		send_attitude_target(transform.header.stamp, tr, 0.0);
 	}
 
-	void pose_cb(const geometry_msgs::PoseStamped::ConstPtr &req) {
+	void attitude_pose_cb(const geometry_msgs::PoseStamped::ConstPtr &pose_msg, const mavros_msgs::Float32Stamped::ConstPtr &throttle_msg) {
 		Eigen::Affine3d tr;
-		tf::poseMsgToEigen(req->pose, tr);
+		tf::poseMsgToEigen(pose_msg->pose, tr);
 
-		send_attitude_target(req->header.stamp, tr);
+		float throttle_normalized = throttle_msg->data;
+
+		/**
+		 * && are lazy, is_normalized() should be called only if reverse_throttle are true.
+		 */
+		if (reverse_throttle && !is_normalized(throttle_normalized, -1.0, 1.0))
+			return;
+		else if (!is_normalized(throttle_normalized, 0.0, 1.0))
+			return;
+
+		send_attitude_target(pose_msg->header.stamp, tr, throttle_normalized);
 	}
 
 	void twist_cb(const geometry_msgs::TwistStamped::ConstPtr &req) {
@@ -181,21 +186,6 @@ private:
 		}
 
 		return true;
-	}
-
-	void throttle_cb(const std_msgs::Float64::ConstPtr &req)
-	{
-		float throttle_normalized = req->data;
-
-		/**
-		 * && are lazy, is_normalized() should be called only if reverse_throttle are true.
-		 */
-		if (reverse_throttle && !is_normalized(throttle_normalized, -1.0, 1.0))
-			return;
-		else if (!is_normalized(throttle_normalized, 0.0, 1.0))
-			return;
-
-		send_attitude_throttle(throttle_normalized);
 	}
 };
 }	// namespace std_plugins
