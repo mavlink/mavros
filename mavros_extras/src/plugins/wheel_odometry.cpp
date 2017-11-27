c359391225d1fa459bfd41a56f15188ad9eb696d
Pavlo Kolomiiets <pkolomiets@gmail.com>
2017-11-27 14:04:19 +0200

    mavros_extras: Wheel odometry plugin fixes after CR.

--- mavros_extras/src/plugins/wheel_odometry.cpp
+++ mavros_extras/src/plugins/wheel_odometry.cpp
@@ -21,6 +21,7 @@
 #include <nav_msgs/Odometry.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2_eigen/tf2_eigen.h>
 
 namespace mavros {
 namespace extra_plugins {
@@ -37,10 +38,14 @@ public:
 		wo_nh("~wheel_odometry"),
 		count(0),
 		odom_mode(OM::NONE),
+		raw_send(false),
 		twist_send(false),
 		tf_send(false),
-		diagnostics(false),
-		yaw_initialized(false)
+		yaw_initialized(false),
+		rpose(Eigen::Vector3d::Zero()),
+		rtwist(Eigen::Vector3d::Zero()),
+		rpose_cov(Eigen::Vector3d::Zero()),
+		rtwist_cov(Eigen::Vector3d::Zero())
 	{ }
 
 	void initialize(UAS &uas_)
@@ -48,7 +53,7 @@ public:
 		PluginBase::initialize(uas_);
 
 		// General params
-		wo_nh.param("diagnostics", diagnostics, false);
+		wo_nh.param("send_raw", raw_send, false);
 		// Wheels configuration
 		wo_nh.param("count", count, 2);
 		count = std::max(1, count); // bound check
@@ -61,10 +66,10 @@ public:
 			odom_mode = OM::DIST;
 
 		// Odometry params
-		wo_nh.param("twist", twist_send, false);
+		wo_nh.param("send_twist", twist_send, false);
 		wo_nh.param<std::string>("frame_id", frame_id, "odom");
 		wo_nh.param<std::string>("child_frame_id", child_frame_id, "base_link");
-		wo_nh.param("vel_error", vel_cov, 0.1f);
+		wo_nh.param("vel_error", vel_cov, 0.1);
 		vel_cov = vel_cov*vel_cov; // std -> cov
 		// TF subsection
 		wo_nh.param("tf/send", tf_send, false);
@@ -74,64 +79,62 @@ public:
 		// Read parameters for each wheel.
 		{
 			int iwheel = 0;
-			bool has_param;
-			do {
+			while (true) {
 				// Build the string in the form "wheelX", where X is the wheel number.
 				// Check if we have "wheelX" parameter.
 				// Indices starts from 0 and should increase without gaps.
-				std::stringstream ss;
-				ss << "wheel" << iwheel++;
-				std::string wheel_name = ss.str();
+				auto name = utils::format("wheel%i", iwheel++);
+
 				// Check if we have "wheelX" parameter
-				has_param = wo_nh.hasParam(wheel_name);
+				if (!wo_nh.hasParam(name)) break;
 
-				if (has_param) {
-					Eigen::Vector2f offset;
-					float radius;
+				// Read
+				Eigen::Vector2d offset;
+				double radius;
 
-					wo_nh.param(wheel_name+"/x", offset[0], 0.f);
-					wo_nh.param(wheel_name+"/y", offset[1], 0.f);
-					wo_nh.param(wheel_name+"/radius", radius, 0.05f);
+				wo_nh.param(name+"/x", offset[0], 0.0);
+				wo_nh.param(name+"/y", offset[1], 0.0);
+				wo_nh.param(name+"/radius", radius, 0.05);
 
-					wheel_offset.push_back(offset);
-					wheel_radius.push_back(radius);
-				}
+				wheel_offset.push_back(offset);
+				wheel_radius.push_back(radius);
 			}
-			while (has_param);
 
 			// Check for all wheels specified
-			if (wheel_offset.size() < count) {
+			if (wheel_offset.size() >= count) {
+				// Duplicate 1st wheel if only one is available.
+				// This generalizes odometry computations for 1- and 2-wheels configurations.
+				if (wheel_radius.size() == 1) {
+					wheel_offset.resize(2);
+					wheel_radius.resize(2);
+					wheel_offset[1].x() = wheel_offset[0].x();
+					wheel_offset[1].y() = wheel_offset[0].y() + 1.0; // make separation non-zero to avoid div-by-zero
+					wheel_radius[1] = wheel_radius[0];
+				}
+
+				// Check for non-zero wheel separation (first two wheels)
+				double separation = std::abs(wheel_offset[1].y() - wheel_offset[0].y());
+				if (separation < 1.e-5) {
+					odom_mode = OM::NONE;
+					ROS_WARN_NAMED("wo", "WO: Separation between the first two wheels is too small (%f).", separation);
+				}
+
+				// Check for reasonable radiuses
+				for (int i = 0; i < wheel_radius.size(); i++) {
+					if (wheel_radius[i] <= 1.e-5) {
+						odom_mode = OM::NONE;
+						ROS_WARN_NAMED("wo", "WO: Wheel #%i has incorrect radius (%f).", i, wheel_radius[i]);
+					}
+				}
+			}
+			else {
 				odom_mode = OM::NONE;
 				ROS_WARN_NAMED("wo", "WO: Not all wheels have parameters specified (%lu/%i).", wheel_offset.size(), count);
-			}
-			// Duplicate 1st wheel if only one is available.
-			// This generalizes odometry computations for 1- and 2-wheels configurations.
-			else if (wheel_radius.size() == 1) {
-				wheel_offset.resize(2);
-				wheel_radius.resize(2);
-				wheel_offset[1].x() = wheel_offset[0].x();
-				wheel_offset[1].y() = wheel_offset[0].y() + 1.f; // make separation non-zero to avoid div-by-zero
-				wheel_radius[1] = wheel_radius[0];
-			}
-
-			// Check for non-zero wheel separation (first two wheels)
-			float separation = std::abs(wheel_offset[1].y() - wheel_offset[0].y());
-			if (separation < 1.e-5f) {
-				odom_mode = OM::NONE;
-				ROS_WARN_NAMED("wo", "WO: Separation between the first two wheels is too small (%f).", separation);
-			}
-
-			// Check for reasonable radiuses
-			for (int i = 0; i < wheel_radius.size(); i++) {
-				if (wheel_radius[i] <= 1.e-5f) {
-					odom_mode = OM::NONE;
-					ROS_WARN_NAMED("wo", "WO: Wheel #%i has incorrect radius (%f).", i, wheel_radius[i]);
-				}
 			}
 		}
 
 		// Advertise RPM-s and distance-s
-		if (diagnostics) {
+		if (raw_send) {
 			rpm_pub = wo_nh.advertise<mavros_msgs::WheelOdomStamped>("rpm", 10);
 			dist_pub = wo_nh.advertise<mavros_msgs::WheelOdomStamped>("distance", 10);
 		}
@@ -164,7 +167,6 @@ private:
 	ros::Publisher dist_pub;
 	ros::Publisher odom_pub;
 	ros::Publisher twist_pub;
-	tf2_ros::TransformBroadcaster tfBroadcaster;
 
 	/// @brief Odometry computation modes
 	enum class OM {
@@ -175,9 +177,9 @@ private:
 	OM odom_mode; //!< odometry computation mode
 
 	int count;		//!< requested number of wheels to compute odometry
-	bool diagnostics;	//!< publish wheel's RPM and cumulative distance
-	std::vector<Eigen::Vector2f> wheel_offset; //!< wheel x,y offsets (m,NED)
-	std::vector<float> wheel_radius; //!< wheel radiuses (m)
+	bool raw_send;		//!< send wheel's RPM and cumulative distance
+	std::vector<Eigen::Vector2d> wheel_offset; //!< wheel x,y offsets (m,NED)
+	std::vector<double> wheel_radius; //!< wheel radiuses (m)
 
 	bool twist_send;		//!< send geometry_msgs/TwistWithCovarianceStamped instead of nav_msgs/Odometry
 	bool tf_send;			//!< send TF
@@ -185,19 +187,19 @@ private:
 	std::string child_frame_id;	//!< body-fixed frame for topic headers
 	std::string tf_frame_id;	//!< origin for TF
 	std::string tf_child_frame_id;	//!< frame for TF and Pose
-	float vel_cov;			//!< wheel velocity measurement error 1-var (m/s)
+	double vel_cov;			//!< wheel velocity measurement error 1-var (m/s)
 
 	int count_meas;				//!< number of wheels in measurements
 	ros::Time time_prev;			//!< timestamp of previous measurement
-	std::vector<float> measurement_prev;	//!< previous measurement
+	std::vector<double> measurement_prev;	//!< previous measurement
 
 	bool yaw_initialized;			//!< initial yaw initialized (from IMU)
 
 	/// @brief Robot origin 2D-state (SI units)
-	Eigen::Vector3f rpose {0.f, 0.f, 0.f};		//!< pose (x, y, yaw)
-	Eigen::Vector3f rtwist {0.f, 0.f, 0.f};		//!< twist (vx, vy, vyaw)
-	Eigen::Vector3f rpose_cov {0.f, 0.f, 0.f};	//!< pose error 1-var (x_cov, y_cov, yaw_cov)
-	Eigen::Vector3f rtwist_cov {0.f, 0.f, 0.f};	//!< twist error 1-var (vx_cov, vy_cov, vyaw_cov)
+	Eigen::Vector3d rpose;		//!< pose (x, y, yaw)
+	Eigen::Vector3d rtwist;		//!< twist (vx, vy, vyaw)
+	Eigen::Vector3d rpose_cov;	//!< pose error 1-var (x_cov, y_cov, yaw_cov)
+	Eigen::Vector3d rtwist_cov;	//!< twist error 1-var (vx_cov, vy_cov, vyaw_cov)
 
 	/**
 	 * @brief Publish odometry.
@@ -211,35 +213,21 @@ private:
 		// Get initial yaw (from IMU)
 		// Check that IMU was already initialized
 		if (!yaw_initialized && m_uas->get_attitude_imu_enu()) {
-			auto quat = m_uas->get_attitude_orientation_enu();
-			tf2::Quaternion q(quat.x, quat.y, quat.z, quat.w);
-			double roll, pitch, yaw;
-			tf2::Matrix3x3 mat(q);
-			mat.getRPY(roll, pitch, yaw);
+			double yaw = ftf::quaternion_get_yaw(ftf::to_eigen(m_uas->get_attitude_orientation_enu()));
 
 			// Rotate current pose by initial yaw
-			float cosy = std::cos(yaw);
-			float siny = std::sin(yaw);
-			float x = rpose(0);
-			float y = rpose(1);
-			rpose(0) = x*cosy - y*siny; // x
-			rpose(1) = x*siny + y*cosy; // y
+			Eigen::Rotation2Dd rot(yaw);
+			rpose.head(2) = rot * rpose.head(2); // x,y
 			rpose(2) += yaw; // yaw
 
-			ROS_INFO_NAMED("wo", "WO: Initial yaw (deg) = %f\n", yaw/M_PI*180.0);
+			ROS_INFO_NAMED("wo", "WO: Initial yaw (deg): %f", yaw/M_PI*180.0);
 			yaw_initialized = true;
 		}
 
 		// Orientation (only if we have initial yaw)
 		geometry_msgs::Quaternion quat;
-		if (yaw_initialized) {
-			tf2::Quaternion q;
-			q.setRPY(0.0, 0.0, rpose(2));
-			quat.x = q.x();
-			quat.y = q.y();
-			quat.z = q.z();
-			quat.w = q.w();
-		}
+		if (yaw_initialized)
+			quat = tf2::toMsg(ftf::quaternion_from_rpy(0.0, 0.0, rpose(2)));
 
 		// Twist
 		geometry_msgs::TwistWithCovariance twist_cov;
@@ -281,7 +269,6 @@ private:
 			odom->pose.pose.position.z = 0.0;
 			odom->pose.pose.orientation = quat;
 			ftf::EigenMapCovariance6d pose_cov_map(odom->pose.covariance.data());
-			pose_cov_map.setZero();
 			pose_cov_map.block<3, 3>(0, 0).diagonal() << rpose_cov(0), rpose_cov(1), -1.0;
 			pose_cov_map.block<3, 3>(3, 3).diagonal() << -1.0, -1.0, rpose_cov(2);
 			// twist
@@ -309,80 +296,90 @@ private:
 	}
 
 	/**
+	 * @brief Update odometry for differential drive robot.
+	 * Odometry is computed for robot's origin (IMU).
+	 * The wheels are assumed to be parallel to the robot's x-direction (forward) and with the same x-offset.
+	 * No slip is assumed (Instantaneous Center of Curvature (ICC) along the axis connecting the wheels).
+	 * All computations are performed for ROS frame conventions.
+	 * @param distance	distance traveled by each wheel since last odometry update
+	 * @param dt		time elapse since last odometry update (s)
+	 */
+	void update_odometry_diffdrive(std::vector<double> distance, double dt)
+	{
+		double y0 = wheel_offset[0](1);
+		double y1 = wheel_offset[1](1);
+		double a = -wheel_offset[0](0);
+		double dy_inv = 1.0 / (y1 - y0);
+		double dt_inv = 1.0 / dt;
+
+		// Rotation angle
+		double theta = (distance[1] - distance[0]) * dy_inv;
+		// Distance traveled by the projection of origin onto the axis connecting the wheels (Op)
+		double L = (y1 * distance[1] - y0 * distance[0]) * dy_inv;
+
+		// Robot origin twist
+		rtwist(0) = L * dt_inv; // vx
+		rtwist(1) = a * theta * dt_inv; // vy
+		rtwist(2) = theta * dt_inv; // vyaw
+
+		// Compute displacement in robot origin frame
+		// dx = a*(cos(theta)-1) + R*sin(theta)
+		// dy = a*sin(theta) - R*(cos(theta)-1)
+		// where R - rotation radius of Op around ICC (R = L/theta).
+		double cos_theta = std::cos(theta);
+		double sin_theta = std::sin(theta);
+		double sin_theta_by_theta; // sin(theta)/theta
+		double cos_theta_1_by_theta; // (cos(theta)-1)/theta
+		// Limits for theta -> 0
+		if (std::abs(theta) < 1.e-5) {
+			sin_theta_by_theta = 1.0;
+			cos_theta_1_by_theta = 0.0;
+		}
+		else {
+			double theta_inv = 1.0 / theta;
+			sin_theta_by_theta = sin_theta * theta_inv;
+			cos_theta_1_by_theta = (cos_theta-1.0) * theta_inv;
+		}
+		double dx = a * (cos_theta-1.0) + L*sin_theta_by_theta;
+		double dy = a * sin_theta - L*cos_theta_1_by_theta;
+
+		// Pose in world frame
+		double cosy = std::cos(rpose(2));
+		double siny = std::sin(rpose(2));
+		rpose(0) += dx*cosy - dy*siny; // x
+		rpose(1) += dx*siny + dy*cosy; // y
+		rpose(2) += theta; // yaw
+
+
+		// Twist errors (constant in time)
+		if (rtwist_cov(0) == 0.0) {
+			rtwist_cov(0) = vel_cov * (y0*y0 + y1*y1) * dy_inv*dy_inv; // vx_cov
+			rtwist_cov(1) = vel_cov * a*a * std::sqrt(2.0) * dy_inv*dy_inv + 0.001; // vy_cov (add extra error, otherwise vy_cov= 0 if a=0)
+			rtwist_cov(2) = vel_cov * 2.0 * dy_inv*dy_inv; // vyaw_cov
+		}
+		// Pose errors (accumulated in time).
+		// Simple approximation is used not respecting kinematic equations.
+		// TODO: accurate odometry error propagation.
+		rpose_cov(0) = rpose_cov(0) + rtwist_cov(0) * dt*dt; // x_cov
+		rpose_cov(1) = rpose_cov(0); // y_cov
+		rpose_cov(2) = rpose_cov(2) + rtwist_cov(2) * dt*dt; // yaw_cov
+	}
+
+	/**
 	 * @brief Update odometry (currently, only 2-wheels differential configuration implemented).
 	 * Odometry is computed for robot's origin (IMU).
 	 * @param distance	distance traveled by each wheel since last odometry update
 	 * @param dt		time elapse since last odometry update (s)
 	 */
-	void update_odometry(std::vector<float> distance, float dt)
+	void update_odometry(std::vector<double> distance, double dt)
 	{
 		// Currently, only 2-wheels configuration implemented
-		int count = std::min(2, (int)distance.size());
-		switch (distance.size())
+		int nwheels = std::min(2, (int)distance.size());
+		switch (nwheels)
 		{
 		// Differential drive robot.
-		// The wheels are assumed to be parallel to the robot's x-direction (forward) and with the same x-offset.
-		// No slip is assumed (Instantaneous Center of Curvature (ICC) along the axis connecting the wheels).
-		// All computations are performed for ROS frame conventions.
 		case 2:
-			float y0 = wheel_offset[0](1);
-			float y1 = wheel_offset[1](1);
-			float a = -wheel_offset[0](0);
-			float dy_inv = 1.f / (y1 - y0);
-			float dt_inv = 1.f / dt;
-
-			// Rotation angle
-			float theta = (distance[1] - distance[0]) * dy_inv;
-			// Distance traveled by the projection of origin onto the axis connecting the wheels (Op)
-			float L = (y1 * distance[1] - y0 * distance[0]) * dy_inv;
-
-			// Robot origin twist
-			rtwist(0) = L * dt_inv; // vx
-			rtwist(1) = a * theta * dt_inv; // vy
-			rtwist(2) = theta * dt_inv; // vyaw
-
-			// Compute displacement in robot origin frame
-			// dx = a*(cos(theta)-1) + R*sin(theta)
-			// dy = a*sin(theta) - R*(cos(theta)-1)
-			// where R - rotation radius of Op around ICC (R = L/theta).
-			float cos_theta = std::cos(theta);
-			float sin_theta = std::sin(theta);
-			float sin_theta_by_theta; // sin(theta)/theta
-			float cos_theta_1_by_theta; // (cos(theta)-1)/theta
-			// Limits for theta -> 0
-			if (std::abs(theta) < 1.e-5f) {
-				sin_theta_by_theta = 1.f;
-				cos_theta_1_by_theta = 0.f;
-			}
-			else {
-				float theta_inv = 1.f / theta;
-				sin_theta_by_theta = sin_theta * theta_inv;
-				cos_theta_1_by_theta = (cos_theta-1.f) * theta_inv;
-			}
-			float dx = a * (cos_theta-1.f) + L*sin_theta_by_theta;
-			float dy = a * sin_theta - L*cos_theta_1_by_theta;
-
-			// Pose in world frame
-			float cosy = std::cos(rpose(2));
-			float siny = std::sin(rpose(2));
-			rpose(0) += dx*cosy - dy*siny; // x
-			rpose(1) += dx*siny + dy*cosy; // y
-			rpose(2) += theta; // yaw
-
-
-			// Twist errors (constant in time)
-			if (rtwist_cov(0) == 0.f) {
-				rtwist_cov(0) = vel_cov * (y0*y0 + y1*y1) * dy_inv*dy_inv; // vx_cov
-				rtwist_cov(1) = vel_cov * a*a * std::sqrt(2.f) * dy_inv*dy_inv + 0.001f; // vy_cov (add extra error, otherwise vy_cov= 0 if a=0)
-				rtwist_cov(2) = vel_cov * 2.f * dy_inv*dy_inv; // vyaw_cov
-			}
-			// Pose errors (accumulated in time).
-			// Simple approximation is used not respecting kinematic equations.
-			// TODO: accurate odometry error propagation.
-			rpose_cov(0) = rpose_cov(0) + rtwist_cov(0) * dt*dt; // x_cov
-			rpose_cov(1) = rpose_cov(0); // y_cov
-			rpose_cov(2) = rpose_cov(2) + rtwist_cov(2) * dt*dt; // yaw_cov
-
+			update_odometry_diffdrive(distance, dt);
 			break;
 		}
 	}
@@ -393,7 +390,7 @@ private:
 	 * @param rpm		whether measurement contains RPM-s or cumulative wheel distances
 	 * @param time		measurement's ROS time stamp
 	 */
-	void process_measurement(std::vector<float> measurement, bool rpm, ros::Time time)
+	void process_measurement(std::vector<double> measurement, bool rpm, ros::Time time)
 	{
 		// Initial measurement
 		if (time_prev == ros::Time(0)) {
@@ -412,16 +409,16 @@ private:
 		}
 		// Compute odometry
 		else {
-			float dt = (time - time_prev).toSec(); // Time since previous measurement (s)
+			double dt = (time - time_prev).toSec(); // Time since previous measurement (s)
 
 			// Distance traveled by each wheel since last measurement.
 			// Reserve for at least 2 wheels.
-			std::vector<float> distance(std::max(2, count));
+			std::vector<double> distance(std::max(2, count));
 			// Compute using RPM-s
 			if (rpm) {
 				for (int i = 0; i < count; i++) {
-					float RPM_2_SPEED = wheel_radius[i] * 2.f * (float)M_PI / 60.f; // RPM -> speed (m/s)
-					float rpm = 0.5f * (measurement[i] + measurement_prev[i]); // Mean RPM during last dt seconds
+					double RPM_2_SPEED = wheel_radius[i] * 2.0 * M_PI / 60.0; // RPM -> speed (m/s)
+					double rpm = 0.5 * (measurement[i] + measurement_prev[i]); // Mean RPM during last dt seconds
 					distance[i] = rpm * RPM_2_SPEED * dt;
 				}
 			}
@@ -462,11 +459,10 @@ private:
 		ros::Time timestamp =  ros::Time::now();
 
 		// Publish RPM-s
-		if (diagnostics) {
+		if (raw_send) {
 			auto rpm_msg = boost::make_shared<mavros_msgs::WheelOdomStamped>();
 
 			rpm_msg->header.stamp = timestamp;
-			rpm_msg->header.frame_id = "";
 			rpm_msg->data.resize(2);
 			rpm_msg->data[0] = rpm.rpm1;
 			rpm_msg->data[1] = rpm.rpm2;
@@ -476,9 +472,7 @@ private:
 
 		// Process measurement
 		if (odom_mode == OM::RPM) {
-			std::vector<float> measurement(2);
-			measurement[0] = rpm.rpm1;
-			measurement[1] = rpm.rpm2;
+			std::vector<double> measurement{rpm.rpm1, rpm.rpm2};
 			process_measurement(measurement, true, timestamp);
 		}
 	}
@@ -499,11 +493,10 @@ private:
 		ros::Time timestamp =  m_uas->synchronise_stamp(wheel_dist.time_usec);
 
 		// Publish distances
-		if (diagnostics) {
+		if (raw_send) {
 			auto wheel_dist_msg = boost::make_shared<mavros_msgs::WheelOdomStamped>();
 
 			wheel_dist_msg->header.stamp = timestamp;
-			wheel_dist_msg->header.frame_id = "";
 			wheel_dist_msg->data.resize(wheel_dist.count);
 			std::copy_n(wheel_dist.distance.begin(), wheel_dist.count, wheel_dist_msg->data.begin());
 
@@ -512,7 +505,7 @@ private:
 
 		// Process measurement
 		if (odom_mode == OM::DIST) {
-			std::vector<float> measurement(wheel_dist.count);
+			std::vector<double> measurement(wheel_dist.count);
 			std::copy_n(wheel_dist.distance.begin(), wheel_dist.count, measurement.begin());
 			process_measurement(measurement, false, timestamp);
 		}
