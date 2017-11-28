afa2ef94854c10e03c7f0959ac072cde25cdc52b
Pierre Kancir <pierre.kancir@azurdrones.com>
2017-11-28 22:37:07 +0100

    Setpoints: add params for initial frame

--- mavros/src/plugins/setpoint_position.cpp
+++ mavros/src/plugins/setpoint_position.cpp
@@ -59,6 +59,13 @@ public:
 			setpoint_sub = sp_nh.subscribe("local", 10, &SetpointPositionPlugin::setpoint_cb, this);
 		}
 		mav_frame_srv = sp_nh.advertiseService("mav_frame", &SetpointPositionPlugin::set_mav_frame_cb, this);
+
+		// mav_frame
+		if (!sp_nh.getParam("mav_frame", mav_frame_str)) {
+			mav_frame = MAV_FRAME::LOCAL_NED;
+		} else {
+			mav_frame = utils::mav_frame_from_str(mav_frame_str);
+		}
 	}
 
 	Subscriptions get_subscriptions()
@@ -80,7 +87,8 @@ private:
 	bool tf_listen;
 	double tf_rate;
 
-	uint8_t mav_frame = utils::enum_value(MAV_FRAME::LOCAL_NED);
+	std::string mav_frame_str;
+	MAV_FRAME mav_frame;
 
 	/* -*- mid-level helpers -*- */
 
@@ -100,7 +108,7 @@ private:
 		 */
 		const uint16_t ignore_all_except_xyz_y = (1 << 11) | (7 << 6) | (7 << 3);
 
-		auto p = [&] {
+		auto p = [&] () {
 			if (static_cast<MAV_FRAME>(mav_frame) == MAV_FRAME::BODY_NED || static_cast<MAV_FRAME>(mav_frame) == MAV_FRAME::BODY_OFFSET_NED) {
 				return ftf::transform_frame_baselink_aircraft(Eigen::Vector3d(tr.translation()));
 			} else {
@@ -108,8 +116,8 @@ private:
 			}
 		} ();
 
-		auto q = [&] {
-			if (static_cast<MAV_FRAME>(mav_frame) == MAV_FRAME::BODY_NED || static_cast<MAV_FRAME>(mav_frame) == MAV_FRAME::BODY_OFFSET_NED) {
+		auto q = [&] () {
+			if (mav_frame == MAV_FRAME::BODY_NED || mav_frame == MAV_FRAME::BODY_OFFSET_NED) {
 				return ftf::transform_orientation_baselink_aircraft(Eigen::Quaterniond(tr.rotation()));
 			} else {
 				return ftf::transform_orientation_enu_ned(
@@ -118,7 +126,7 @@ private:
 		} ();
 
 		set_position_target_local_ned(stamp.toNSec() / 1000000,
-					mav_frame,
+					utils::enum_value(mav_frame),
 					ignore_all_except_xyz_y,
 					p,
 					Eigen::Vector3d::Zero(),
@@ -147,7 +155,9 @@ private:
 
 	bool set_mav_frame_cb(mavros_msgs::SetMavFrame::Request &req, mavros_msgs::SetMavFrame::Response &res)
 	{
-		mav_frame = utils::enum_value(static_cast<MAV_FRAME>(req.mav_frame));
+		mav_frame = static_cast<MAV_FRAME>(req.mav_frame);
+		mav_frame_str = utils::to_string(mav_frame);
+		sp_nh.setParam("mav_frame", mav_frame_str);
 		res.success = true;
 		return true;
 	}
