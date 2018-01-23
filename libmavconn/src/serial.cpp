03f778d9c6596ce966b54b44071761d2f18a7f2f
Pierre Kancir <pierre.kancir@skeyetech.fr>
2018-01-23 11:33:44 +0100

    Libmavconn : Set the serial port on Raw mode to prevent EOF error

--- libmavconn/src/serial.cpp
+++ libmavconn/src/serial.cpp
@@ -54,6 +54,14 @@ MAVConnSerial::MAVConnSerial(uint8_t system_id, uint8_t component_id,
 		serial_dev.set_option(SPB::parity(SPB::parity::none));
 		serial_dev.set_option(SPB::stop_bits(SPB::stop_bits::one));
 		serial_dev.set_option(SPB::flow_control( (hwflow) ? SPB::flow_control::hardware : SPB::flow_control::none));
+#ifdef __linux__
+		// Set serial port to "raw" mode. Prevent the EOF exit
+		int fd = serial_dev.native_handle();
+		termios tio;
+		tcgetattr(fd, &tio);
+		cfmakeraw(&tio);
+		tcsetattr(fd, TCSANOW, &tio);
+#endif
 	}
 	catch (boost::system::system_error &err) {
 		throw DeviceError("serial", err);
