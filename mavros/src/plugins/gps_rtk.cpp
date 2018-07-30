5e69a597d8ff8580cda0318157691f4cf9091c97
Alexis Paques <alexis.paques@gmail.com>
2018-07-30 10:29:48 +0200

    Updating the gps_rtk plugin to fit mavros guidelines: - Updating max_frag_len to allow changes in size in MAVLink seamlessly - Using std::copy instead of memset - Zero fill with std::fill - Preapply the sequence flags - Use of std iterators - Add the maximal data size in the mavros_msgs

--- mavros/src/plugins/gps_rtk.cpp
+++ mavros/src/plugins/gps_rtk.cpp
@@ -48,33 +48,41 @@ private:
 
 	void rtk_cb(const mavros_msgs::RTCM::ConstPtr &msg)
 	{
-		const int maxMessageLength = 180;	// Should be replaced by MAVLINK_MSG_GPS_RTCM_DATA_FIELD_DATA_LEN
-		int seq = msg->header.seq;
+		// const int max_frag_len = mavlink::common::msg::GPS_RTCM_DATA::data::size
+		// ‘mavlink::common::msg::GPS_RTCM_DATA::data’ is not a class, namespace, or enumeration
+		mavlink::common::msg::GPS_RTCM_DATA rtcm_data;
+		const int max_frag_len = rtcm_data.data.size();
 
-		if (msg->data.size() > 4 * maxMessageLength) {
-			ROS_FATAL("gps_rtk: RTCM message received is bigger than the maximal possible size");
+		uint8_t seq_u5 = uint8_t(msg->header.seq & 0x1F) << 3;
+
+		if (msg->data.size() > 4 * max_frag_len) {
+			ROS_FATAL("gps_rtk: RTCM message received is bigger than the maximal possible size.");
 			return;
 		}
 
-		if (msg->data.size() < maxMessageLength) {
-			mavlink::common::msg::GPS_RTCM_DATA rtcm_data;
+		auto data_it = msg->data.begin();
+		auto end_it = msg->data.end();
+
+		if (msg->data.size() <= max_frag_len) {
 			rtcm_data.len = msg->data.size();
-			rtcm_data.flags = (seq & 0x1F) << 3;
-			memcpy(&rtcm_data.data, &msg->data, msg->data.size());
+			rtcm_data.flags = seq_u5;
+			std::copy(data_it, end_it, rtcm_data.data.begin());
+			std::fill(rtcm_data.data.begin() + rtcm_data.len, rtcm_data.data.end(), 0);
 			UAS_FCU(m_uas)->send_message(rtcm_data);
 		} else {
-			uint8_t fragmentId = 0;
-			int start = 0;
-			while (start < msg->data.size()) {
-				mavlink::common::msg::GPS_RTCM_DATA rtcm_data;
-				int length = std::min((int)msg->data.size() - start, maxMessageLength);
-				rtcm_data.flags = 1;				// LSB set indicates message is fragmented
-				rtcm_data.flags |= fragmentId++ << 1;		// Next 2 bits are fragment id
-				rtcm_data.flags |= (seq & 0x1F) << 3;		// Next 5 bits are sequence id
-				rtcm_data.len = length;
-				memcpy(&rtcm_data.data, &msg->data + start, length);
-				UAS_FCU(m_uas)->send_message(rtcm_data);
-				start += length;
+			for (uint8_t fragment_id = 0; fragment_id < 4 && data_it < end_it; fragment_id++) {
+				mavlink::common::msg::GPS_RTCM_DATA rtcm_data_frag;	// Can we send rtcm_data 4 times with different data safely?
+
+				uint8_t len = std::min((int)std::distance(data_it, end_it), max_frag_len);
+				rtcm_data_frag.flags = 1;				// LSB set indicates message is fragmented
+				rtcm_data_frag.flags |= fragment_id++ << 1;		// Next 2 bits are fragment id
+				rtcm_data_frag.flags |= seq_u5;		// Next 5 bits are sequence id
+				rtcm_data_frag.len = len;
+
+				std::copy(data_it, data_it + len, rtcm_data_frag.data.begin());
+				std::fill(rtcm_data_frag.data.begin() + len, rtcm_data_frag.data.end(), 0);
+				UAS_FCU(m_uas)->send_message(rtcm_data_frag);
+				std::advance(data_it, len);
 			}
 		}
 	}
