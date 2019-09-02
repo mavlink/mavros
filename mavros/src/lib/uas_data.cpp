cacf5dd7a1eb1994e592b2c17a68be65d258cb35
Saifullah3396 <Saifullah3396@gmail.com>
2019-09-02 10:01:03 +0500

    Added a static transform from fcu_frd frame to fcu frame.

--- mavros/src/lib/uas_data.cpp
+++ mavros/src/lib/uas_data.cpp
@@ -54,6 +54,9 @@ UAS::UAS() :
 	// send static transform from "local_origin" (ENU) to "local_origin_ned" (NED)
 	publish_static_transform("local_origin", "local_origin_ned", Eigen::Affine3d(ftf::quaternion_from_rpy(M_PI, 0, M_PI_2)));
 	publish_static_transform("fcu", "fcu_frd", Eigen::Affine3d(ftf::quaternion_from_rpy(M_PI, 0, 0)));
+
+	// Fix for local_origin_ned to fcu transform
+	publish_static_transform("fcu_frd", "fcu", Eigen::Affine3d(ftf::quaternion_from_rpy(-M_PI, 0, 0)));
 }
 
 /* -*- heartbeat handlers -*- */
