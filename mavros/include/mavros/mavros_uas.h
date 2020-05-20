8376c538ec254259b22c677d1e4c66b0b03957d6
Braedon <braedon@rizse.io>
2020-05-20 14:53:20 -0500

    Added alias for capabilities enum to UAS

--- mavros/include/mavros/mavros_uas.h
+++ mavros/include/mavros/mavros_uas.h
@@ -70,7 +70,7 @@ public:
 	using MAV_AUTOPILOT = mavlink::common::MAV_AUTOPILOT;
 	using MAV_MODE_FLAG = mavlink::common::MAV_MODE_FLAG;
 	using MAV_STATE = mavlink::common::MAV_STATE;
-	using MAV_CAP = mavlink::common::MAV_PROTOCOL_CAPABILITY
+	using MAV_CAP = mavlink::common::MAV_PROTOCOL_CAPABILITY;
 	using timesync_mode = utils::timesync_mode;
 
 	// other UAS aliases
