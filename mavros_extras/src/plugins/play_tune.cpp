327497e7b1d4dde18f4517de8773a763b6bc76bd
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2020-09-10 13:53:02 +0200

    play_tune: Uncrustify

--- mavros_extras/src/plugins/play_tune.cpp
+++ mavros_extras/src/plugins/play_tune.cpp
@@ -6,52 +6,52 @@ namespace mavros
 {
 namespace extra_plugins
 {
-
 class PlayTunePlugin : public plugin::PluginBase
 {
-  public:
-    PlayTunePlugin() : PluginBase(), nh("~") {}
+public:
+	PlayTunePlugin() : PluginBase(), nh("~") {}
 
-    void initialize(UAS& uas_) override
-    {
-        PluginBase::initialize(uas_);
-        sub = nh.subscribe("play_tune", 1, &PlayTunePlugin::callback, this);
-    }
+	void initialize(UAS& uas_) override
+	{
+		PluginBase::initialize(uas_);
+		sub = nh.subscribe("play_tune", 1, &PlayTunePlugin::callback, this);
+	}
 
-    Subscriptions get_subscriptions() override { return { /* No subscriptions */ }; }
+	Subscriptions get_subscriptions() override {
+		return { /* No subscriptions */ };
+	}
 
-  private:
-    ros::NodeHandle nh;
-    ros::Subscriber sub;
+private:
+	ros::NodeHandle nh;
+	ros::Subscriber sub;
 
-    void callback(const mavros_msgs::PlayTuneV2::ConstPtr& tune)
-    {
-        auto msg = mavlink::common::msg::PLAY_TUNE_V2{};
-        m_uas->msg_set_target(msg);
+	void callback(const mavros_msgs::PlayTuneV2::ConstPtr& tune)
+	{
+		auto msg = mavlink::common::msg::PLAY_TUNE_V2{};
+		m_uas->msg_set_target(msg);
 
-        switch (tune->format)
-        {
-            case mavros_msgs::PlayTuneV2::TUNE_FORMAT_QBASIC1_1:
-                msg.format = static_cast<uint32_t>(mavlink::common::TUNE_FORMAT::QBASIC1_1);
-                break;
+		switch (tune->format)
+		{
+		case mavros_msgs::PlayTuneV2::TUNE_FORMAT_QBASIC1_1:
+			msg.format = static_cast<uint32_t>(mavlink::common::TUNE_FORMAT::QBASIC1_1);
+			break;
 
-            case mavros_msgs::PlayTuneV2::TUNE_FORMAT_MML_MODERN:
-                msg.format = static_cast<uint32_t>(mavlink::common::TUNE_FORMAT::MML_MODERN);
-                break;
+		case mavros_msgs::PlayTuneV2::TUNE_FORMAT_MML_MODERN:
+			msg.format = static_cast<uint32_t>(mavlink::common::TUNE_FORMAT::MML_MODERN);
+			break;
 
-            default:
-                ROS_ERROR_NAMED("play_tune", "Invalid tune format: '%i'", tune->format);
-                return;
-        }
+		default:
+			ROS_ERROR_NAMED("play_tune", "Invalid tune format: '%i'", tune->format);
+			return;
+		}
 
-        mavlink::set_string_z(msg.tune, tune->tune);
+		mavlink::set_string_z(msg.tune, tune->tune);
 
-        UAS_FCU(m_uas)->send_message_ignore_drop(msg);
-    }
+		UAS_FCU(m_uas)->send_message_ignore_drop(msg);
+	}
 };
-
-}  // namespace extra_plugins
-}  // namespace mavros
+}	// namespace extra_plugins
+}	// namespace mavros
 
 #include <pluginlib/class_list_macros.h>
 PLUGINLIB_EXPORT_CLASS(mavros::extra_plugins::PlayTunePlugin, mavros::plugin::PluginBase)
