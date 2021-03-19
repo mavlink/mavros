1a72af925e9b0df0a403f50abb629e22e8d69a07
Vladimir Ermakov <vooon341@gmail.com>
2021-03-19 13:45:04 +0300

    mavros: begin porting...

--- mavros/setup.py
+++ mavros/setup.py
@@ -1,11 +1,38 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+from setuptools import setup
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = 'mavros'
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['mavros'],
-    package_dir={'': 'src'})
-
-setup(**setup_args)
+setup(
+    name=package_name,
+    version='1.6.0',
+    packages=[package_name],
+    package_dir={'': 'src'},
+    # data_files=[
+    #     ('share/ament_index/resource_index/packages',
+    #         ['resource/' + package_name]),
+    #     ('share/' + package_name, ['package.xml']),
+    # ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Vladimir Ermakov',
+    author_email='vooon341@gmail.com',
+    maintainer='Vladimir Ermakov',
+    maintainer_email='vooon341@gmail.com',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Helper scripts and module for MAVROS',
+    license='Triple licensed under GPLv3, LGPLv3 and BSD',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'mavros = mavros.cmd:main',
+        ],
+    },
+)
