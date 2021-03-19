2c6e379dbe38531ca7ad9548bdd6b598b5ff6448
Vladimir Ermakov <vooon341@gmail.com>
2021-03-20 00:17:37 +0300

    router: add some code docs, ament-uncrustify

--- mavros/include/mavros/mavros_router.hpp
+++ mavros/include/mavros/mavros_router.hpp
@@ -33,136 +33,194 @@
 #include <mavros_msgs/srv/endpoint_add.hpp>
 #include <mavros_msgs/srv/endpoint_del.hpp>
 
-namespace mavros {
-namespace router {
+namespace mavros
+{
+namespace router
+{
 
-    using id_t = uint32_t;
-    using addr_t = uint32_t;
+using id_t = uint32_t;
+using addr_t = uint32_t;
 
-    using mavconn::Framing;
-    using ::mavlink::mavlink_message_t;
-    using ::mavlink::msgid_t;
+using mavconn::Framing;
+using ::mavlink::mavlink_message_t;
+using ::mavlink::msgid_t;
 
-    using namespace std::placeholders;
+using namespace std::placeholders;
 
-    class Router;
+class Router;
 
-    class Endpoint : public std::enable_shared_from_this<Endpoint> {
-    public:
-        RCLCPP_SMART_PTR_DEFINITIONS(Endpoint)
+/**
+ * Endpoint base class
+ *
+ * Represents one network connection to the Router
+ *
+ * One endpoint could map to several remote devices,
+ * e.g. mesh radio for swarm.
+ */
+class Endpoint : public std::enable_shared_from_this<Endpoint>
+{
+public:
+  RCLCPP_SMART_PTR_DEFINITIONS(Endpoint)
 
-        enum class Type {
-            fcu = 0,
-            gcs = 1,
-            uas = 2,
-        };
+  enum class Type
+  {
+    fcu = 0,
+    gcs = 1,
+    uas = 2,
+  };
 
-        Endpoint()
-            : parent()
-            , id(0)
-            , link_type(Type::fcu)
-            , url {}
-            , remote_addrs {}
-        {
-            const addr_t broadcase_addr = 0;
+  Endpoint()
+  : parent(),
+    id(0),
+    link_type(Type::fcu),
+    url{},
+    remote_addrs{}
+  {
+    const addr_t broadcase_addr = 0;
 
-            // Accept broadcasts by default
-            remote_addrs.emplace(broadcase_addr);
-        }
+    // Accept broadcasts by default
+    remote_addrs.emplace(broadcase_addr);
+  }
 
-        std::weak_ptr<Router> parent;
+  std::weak_ptr<Router> parent;
 
-        uint32_t id; // id of the endpoint
-        Type link_type;
-        std::string url;               // url to open that endpoint
-        std::set<addr_t> remote_addrs; // remotes that we heard there
+  uint32_t id;                         // id of the endpoint
+  Type link_type;                      // class of the endpoint
+  std::string url;                     // url to open that endpoint
+  std::set<addr_t> remote_addrs;       // remotes that we heard there
 
-        virtual bool is_open() = 0;
-        virtual bool open() = 0;
-        virtual void close() = 0;
+  virtual bool is_open() = 0;
+  virtual bool open() = 0;
+  virtual void close() = 0;
 
-        virtual void send_message(const mavlink_message_t* msg, const Framing framing = Framing::ok) = 0;
-        virtual void recv_message(const mavlink_message_t* msg, const Framing framing = Framing::ok);
-    };
+  virtual void send_message(const mavlink_message_t * msg, const Framing framing = Framing::ok) = 0;
+  virtual void recv_message(const mavlink_message_t * msg, const Framing framing = Framing::ok);
+};
 
-    class Router : public rclcpp::Node {
-    public:
-        RCLCPP_SMART_PTR_DEFINITIONS(Router)
+/**
+ * Router class implements MAVROS Router node
+ *
+ * Router is essential part that connects several MAVLink devices together.
+ * In general there are three device classes that router uses:
+ * - a FCU endpoint - should be faced to autopilot firmware(s),
+ * - a GCS endpoint - that ep connects control software
+ * - a UAS endpoint - special type of endpoint that is used to connect MAVROS UAS node(s) to FCU(s)
+ *
+ * Some traffic rules:
+ * 1. FCU broadcast -> GCS, UAS, but not to any other FCU
+ * 2. FCU targeted system/component -> GCS/UAS endpoint that have matching address
+ * 3. FCU targeted system -> same as for p.2
+ * 4. GCS broadcast -> FCU, UAS
+ * 5. GCS targeted -> FCU/UAS maching addr
+ * 6. UAS broadcast -> FCU, GCS
+ * 7. UAS targeted -> FCU/GCS
+ */
+class Router : public rclcpp::Node
+{
+public:
+  RCLCPP_SMART_PTR_DEFINITIONS(Router)
 
-        Router(std::string node_name = "mavros_router")
-            : rclcpp::Node(node_name, rclcpp::NodeOptions().use_intra_process_comms(true))
-            , endpoints {}
-        {
-            RCLCPP_DEBUG(this->get_logger(), "Start mavros::router::Router initialization...");
+  Router(std::string node_name = "mavros_router")
+  : rclcpp::Node(node_name, rclcpp::NodeOptions().use_intra_process_comms(true)),
+    endpoints{}
+  {
+    RCLCPP_DEBUG(this->get_logger(), "Start mavros::router::Router initialization...");
 
-            add_service = this->create_service<mavros_msgs::srv::EndpointAdd>("~/add_endpoint", std::bind(&Router::add_endpoint, this, _1, _2));
-            del_service = this->create_service<mavros_msgs::srv::EndpointDel>("~/del_endpoint", std::bind(&Router::del_endpoint, this, _1, _2));
-        }
+    add_service =
+      this->create_service<mavros_msgs::srv::EndpointAdd>(
+      "~/add_endpoint",
+      std::bind(&Router::add_endpoint, this, _1, _2));
+    del_service =
+      this->create_service<mavros_msgs::srv::EndpointDel>(
+      "~/del_endpoint",
+      std::bind(&Router::del_endpoint, this, _1, _2));
+  }
 
-        void route_message(Endpoint::SharedPtr src, const mavlink_message_t* msg, const Framing framing);
+  void route_message(Endpoint::SharedPtr src, const mavlink_message_t * msg, const Framing framing);
 
-    private:
-        friend class Endpoint;
+private:
+  friend class Endpoint;
 
-        static std::atomic<id_t> id_counter;
+  static std::atomic<id_t> id_counter;
 
-        // map stores all routing endpoints
-        std::unordered_map<id_t, Endpoint::SharedPtr> endpoints;
+  // map stores all routing endpoints
+  std::unordered_map<id_t, Endpoint::SharedPtr> endpoints;
 
-        rclcpp::Service<mavros_msgs::srv::EndpointAdd>::SharedPtr add_service;
-        rclcpp::Service<mavros_msgs::srv::EndpointDel>::SharedPtr del_service;
+  rclcpp::Service<mavros_msgs::srv::EndpointAdd>::SharedPtr add_service;
+  rclcpp::Service<mavros_msgs::srv::EndpointDel>::SharedPtr del_service;
 
-        void add_endpoint(const mavros_msgs::srv::EndpointAdd::Request::SharedPtr request, mavros_msgs::srv::EndpointAdd::Response::SharedPtr response);
-        void del_endpoint(const mavros_msgs::srv::EndpointDel::Request::SharedPtr request, mavros_msgs::srv::EndpointDel::Response::SharedPtr response);
+  void add_endpoint(
+    const mavros_msgs::srv::EndpointAdd::Request::SharedPtr request,
+    mavros_msgs::srv::EndpointAdd::Response::SharedPtr response);
+  void del_endpoint(
+    const mavros_msgs::srv::EndpointDel::Request::SharedPtr request,
+    mavros_msgs::srv::EndpointDel::Response::SharedPtr response);
 
-        void periodic_reconnect_endpoints();
-    };
+  void periodic_reconnect_endpoints();
+};
 
-    class MAVConnEndpoint : public Endpoint {
-    public:
-        MAVConnEndpoint()
-            : Endpoint()
-        {
-        }
+/**
+ * MAVConnEndpoint implements Endpoint for FCU or GCS connection
+ * via good old libmavconn url's
+ *
+ * TODO(vooon): support multiple remotes on UDP,
+ *              choose right TCP client instead of simple broadcast
+ */
+class MAVConnEndpoint : public Endpoint
+{
+public:
+  MAVConnEndpoint()
+  : Endpoint()
+  {
+  }
 
-        ~MAVConnEndpoint()
-        {
-            close();
-        }
+  ~MAVConnEndpoint()
+  {
+    close();
+  }
 
-        mavconn::MAVConnInterface::Ptr link; // connection
+  mavconn::MAVConnInterface::Ptr link;       // connection
 
-        bool is_open() override;
-        bool open() override;
-        void close() override;
+  bool is_open() override;
+  bool open() override;
+  void close() override;
 
-        void send_message(const mavlink_message_t* msg, const Framing framing = Framing::ok) override;
-    };
+  void send_message(const mavlink_message_t * msg, const Framing framing = Framing::ok) override;
+};
 
-    class ROSEndpoint : public Endpoint {
-    public:
-        ROSEndpoint()
-            : Endpoint()
-        {
-        }
+/**
+ * ROSEndpoint implements Endpoint for UAS node
+ *
+ * That endpoint converts mavlink messages to ROS2 IDL
+ * and passes them trough DDL messaging or intra-process comms.
+ *
+ * Each drone would have separate UAS node
+ */
+class ROSEndpoint : public Endpoint
+{
+public:
+  ROSEndpoint()
+  : Endpoint()
+  {
+  }
 
-        ~ROSEndpoint()
-        {
-            close();
-        }
+  ~ROSEndpoint()
+  {
+    close();
+  }
 
-        rclcpp::Subscription<mavros_msgs::msg::Mavlink>::SharedPtr to; // UAS -> FCU
-        rclcpp::Publisher<mavros_msgs::msg::Mavlink>::SharedPtr from;  // FCU -> UAS
+  rclcpp::Subscription<mavros_msgs::msg::Mavlink>::SharedPtr to;       // UAS -> FCU
+  rclcpp::Publisher<mavros_msgs::msg::Mavlink>::SharedPtr from;        // FCU -> UAS
 
-        bool is_open() override;
-        bool open() override;
-        void close() override;
+  bool is_open() override;
+  bool open() override;
+  void close() override;
 
-        void send_message(const mavlink_message_t* msg, const Framing framing = Framing::ok) override;
+  void send_message(const mavlink_message_t * msg, const Framing framing = Framing::ok) override;
 
-    private:
-        void ros_recv_message(const mavros_msgs::msg::Mavlink::SharedPtr rmsg);
-    };
+private:
+  void ros_recv_message(const mavros_msgs::msg::Mavlink::SharedPtr rmsg);
+};
 
 } // namespace router
 } // namespace mavros
--- mavros/src/lib/mavros_router.cpp
+++ mavros/src/lib/mavros_router.cpp
@@ -17,128 +17,171 @@
 using namespace mavros::router;
 using rclcpp::QoS;
 
-static std::atomic<id_t> id_counter { 0 };
+static std::atomic<id_t> Router::id_counter {0};
 
-void Router::route_message(Endpoint::SharedPtr src, const mavlink_message_t* msg, const Framing framing)
+void Router::route_message(
+  Endpoint::SharedPtr src, const mavlink_message_t * msg,
+  const Framing framing)
 {
 }
 
-void Router::add_endpoint(const mavros_msgs::srv::EndpointAdd::Request::SharedPtr request, mavros_msgs::srv::EndpointAdd::Response::SharedPtr response)
+void Router::add_endpoint(
+  const mavros_msgs::srv::EndpointAdd::Request::SharedPtr request,
+  mavros_msgs::srv::EndpointAdd::Response::SharedPtr response)
+{
+  auto lg = get_logger();
+
+  RCLCPP_INFO(lg, "Requested to add endpoint: type %d, url: %s", request->type, request->url);
+
+  id_t id = id_counter.fetch_add(1);
+
+  Endpoint::SharedPtr ep;
+  if (request->type == mavros_msgs::srv::EndpointAdd::TYPE_UAS) {
+    ep = ROSEndpoint::make_shared();
+  } else {
+    ep = MAVConnEndpoint::make_shared();
+  }
+
+  ep.parent = shared_from_this();
+  ep.id = id;
+  ep.link_type = static_cast<Endpoint::Type>(request->type);
+  ep.url = request->url;
+
+  this->endpoints[id] = ep;
+
+  RCLCPP_INFO(lg, "Endpoint link[%d] created", id);
+
+  response.id = id;
+  response.success = ep.open();
+}
+
+void Router::del_endpoint(
+  const mavros_msgs::srv::EndpointDel::Request::SharedPtr request,
+  mavros_msgs::srv::EndpointDel::Response::SharedPtr response)
+{
+
+}
+
+void Router::periodic_reconnect_endpoints()
 {
 }
 
-void Router::del_endpoint(const mavros_msgs::srv::EndpointDel::Request::SharedPtr request, mavros_msgs::srv::EndpointDel::Response::SharedPtr response)
+void Endpoint::recv_message(const mavlink_message_t * msg, const Framing framing)
 {
-}
+  rcpputils::assert_true(msg, "msg not nullptr");
+  //rcpputils::assert_true(this->parent, "parent not nullptr");
 
-void Endpoint::recv_message(const mavlink_message_t* msg, const Framing framing)
-{
-    rcpputils::assert_true(msg, "msg not nullptr");
-    //rcpputils::assert_true(this->parent, "parent not nullptr");
+  const addr_t sysid_addr = msg->sysid << 8;
+  const addr_t sysid_compid_addr = (msg->sysid << 8) | msg->compid;
 
-    const addr_t sysid_addr = msg->sysid << 8;
-    const addr_t sysid_compid_addr = (msg->sysid << 8) | msg->compid;
+  auto sp = this->remote_addrs.emplace(sysid_addr);
+  auto scp = this->remote_addrs.emplace(sysid_compid_addr);
 
-    auto sp = this->remote_addrs.emplace(sysid_addr);
-    auto scp = this->remote_addrs.emplace(sysid_compid_addr);
-
-    if (auto nh = this->parent.lock()) {
-        if (sp.second || scp.second) {
-            RCLCPP_INFO(nh->get_logger(), "link[%d] detected remote address %d.%d", this->id, msg->sysid, msg->compid);
-        }
-
-        nh->route_message(shared_from_this(), msg, framing);
+  if (auto nh = this->parent.lock()) {
+    if (sp.second || scp.second) {
+      RCLCPP_INFO(
+        nh->get_logger(), "link[%d] detected remote address %d.%d", this->id, msg->sysid,
+        msg->compid);
     }
+
+    nh->route_message(shared_from_this(), msg, framing);
+  }
 }
 
 bool MAVConnEndpoint::is_open()
 {
-    if (!this->link) {
-        return false;
-    }
+  if (!this->link) {
+    return false;
+  }
 
-    return this->link->is_open();
+  return this->link->is_open();
 }
 
 bool MAVConnEndpoint::open()
 {
-    auto link = mavconn::MAVConnInterface::open_url(this->url); // XXX may trow DeviceError
-    link->message_received_cb = std::bind(&MAVConnEndpoint::recv_message, shared_from_this(), _1, _2);
+  auto link = mavconn::MAVConnInterface::open_url(this->url);   // XXX may trow DeviceError
+  link->message_received_cb = std::bind(&MAVConnEndpoint::recv_message, shared_from_this(), _1, _2);
 
-    this->link = link;
-    return true;
+  this->link = link;
+  return true;
 }
 
 void MAVConnEndpoint::close()
 {
-    if (!this->link) {
-        return;
-    }
+  if (!this->link) {
+    return;
+  }
 
-    this->link->close();
-    this->link.reset();
+  this->link->close();
+  this->link.reset();
 }
 
-void MAVConnEndpoint::send_message(const mavlink_message_t* msg, const Framing framing)
+void MAVConnEndpoint::send_message(const mavlink_message_t * msg, const Framing framing)
 {
-    if (!this->link) {
-        return;
-    }
+  if (!this->link) {
+    return;
+  }
 
-    this->link->send_message_ignore_drop(msg);
+  this->link->send_message_ignore_drop(msg);
 }
 
 bool ROSEndpoint::is_open()
 {
-    return this->from && this->to;
+  return this->from && this->to;
 }
 
 bool ROSEndpoint::open()
 {
-    auto nh = this->parent.lock();
-    if (!nh) {
-        return false;
-    }
+  auto nh = this->parent.lock();
+  if (!nh) {
+    return false;
+  }
 
-    this->from = nh->create_publisher<mavros_msgs::msg::Mavlink>(utils::format("%s/%s", this->url, "mavlink_from"), QoS(1000).best_effort());
-    this->to = nh->create_subscription<mavros_msgs::msg::Mavlink>(utils::format("%s/%s", this->url, "mavlink_to"), QoS(1000).best_effort(),
-            std::bind(&ROSEndpoint::ros_recv_message, this, _1));
+  this->from =
+    nh->create_publisher<mavros_msgs::msg::Mavlink>(
+    utils::format(
+      "%s/%s", this->url,
+      "mavlink_from"), QoS(
+      1000).best_effort());
+  this->to = nh->create_subscription<mavros_msgs::msg::Mavlink>(
+    utils::format("%s/%s", this->url, "mavlink_to"), QoS(1000).best_effort(),
+    std::bind(&ROSEndpoint::ros_recv_message, this, _1));
 
-    return true;
+  return true;
 }
 
 void ROSEndpoint::close()
 {
-    this->from.reset();
-    this->to.reset();
+  this->from.reset();
+  this->to.reset();
 }
 
-void ROSEndpoint::send_message(const mavlink_message_t* msg, const Framing framing)
+void ROSEndpoint::send_message(const mavlink_message_t * msg, const Framing framing)
 {
-    rcpputils::assert_true(msg, "msg not null");
+  rcpputils::assert_true(msg, "msg not null");
 
-    auto rmsg = mavros_msgs::msg::Mavlink();
-    auto success = mavros_msgs::mavlink::convert(*msg, rmsg, utils::enum_value(framing));
+  auto rmsg = mavros_msgs::msg::Mavlink();
+  auto success = mavros_msgs::mavlink::convert(*msg, rmsg, utils::enum_value(framing));
 
-    if (success) {
-        this->from->publish(rmsg);
-    } else if (auto nh = this->parent.lock()) {
-        RCLCPP_ERROR(nh->get_logger(), "message conversion error");
-    }
+  if (success) {
+    this->from->publish(rmsg);
+  } else if (auto nh = this->parent.lock()) {
+    RCLCPP_ERROR(nh->get_logger(), "message conversion error");
+  }
 }
 
 void ROSEndpoint::ros_recv_message(const mavros_msgs::msg::Mavlink::SharedPtr rmsg)
 {
-    rcpputils::assert_true(!!rmsg, "rmsg not nullptr");
+  rcpputils::assert_true(!!rmsg, "rmsg not nullptr");
 
-    mavlink::mavlink_message_t mmsg;
+  mavlink::mavlink_message_t mmsg;
 
-    auto success = mavros_msgs::mavlink::convert(*rmsg, mmsg);
-    auto framing = static_cast<Framing>(rmsg->framing_status);
+  auto success = mavros_msgs::mavlink::convert(*rmsg, mmsg);
+  auto framing = static_cast<Framing>(rmsg->framing_status);
 
-    if (success) {
-        recv_message(&mmsg, framing);
-    } else if (auto nh = this->parent.lock()) {
-        RCLCPP_ERROR(nh->get_logger(), "message conversion error");
-    }
+  if (success) {
+    recv_message(&mmsg, framing);
+  } else if (auto nh = this->parent.lock()) {
+    RCLCPP_ERROR(nh->get_logger(), "message conversion error");
+  }
 }
