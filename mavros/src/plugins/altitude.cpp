6d3755cffb24197bea058b3437dcf9b9ee87a66b
Vladimir Ermakov <vooon341@gmail.com>
2021-04-09 10:18:59 +0300

    plugin: altitude: port to ros2

--- mavros/src/plugins/altitude.cpp
+++ mavros/src/plugins/altitude.cpp
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2015 Andreas Antener <andreas@uaventure.com>.
+ * Copyright 2021 Vladimir Ermakov.
+ *
+ * This file is part of the mavros package and subject to the license terms
+ * in the top-level LICENSE file of the mavros repository.
+ * https://github.com/mavlink/mavros/tree/master/LICENSE.md
+ */
 /**
  * @brief Altitude plugin
  * @file altitude.cpp
@@ -6,42 +14,39 @@
  * @addtogroup plugin
  * @{
  */
-/*
- * Copyright 2015 Andreas Antener <andreas@uaventure.com>.
- *
- * This file is part of the mavros package and subject to the license terms
- * in the top-level LICENSE file of the mavros repository.
- * https://github.com/mavlink/mavros/tree/master/LICENSE.md
- */
 
-#include <mavros/mavros_plugin.h>
+#include <rcpputils/asserts.hpp>
+#include <mavros/mavros_uas.hpp>
+#include <mavros/plugin.hpp>
+#include <mavros/plugin_filter.hpp>
 
-#include <mavros_msgs/Altitude.h>
+#include <mavros_msgs/msg/altitude.hpp>
 
 namespace mavros
 {
 namespace std_plugins
 {
+
 /**
  * @brief Altitude plugin.
+ * @plugin altitude
  */
-class AltitudePlugin : public plugin::PluginBase
+class AltitudePlugin : public plugin::Plugin
 {
 public:
-  AltitudePlugin()
-  : PluginBase(),
-    nh("~")
-  {}
-
-  /**
-   * Plugin initializer. Constructor should not do this.
-   */
-  void initialize(UAS & uas_) override
+  explicit AltitudePlugin(plugin::UASPtr uas_)
+  : Plugin(uas_, "altitude")
   {
-    PluginBase::initialize(uas_);
+    enable_node_watch_parameters();
 
-    nh.param<std::string>("frame_id", frame_id, "map");
-    altitude_pub = nh.advertise<mavros_msgs::Altitude>("altitude", 10);
+    node_declate_and_watch_parameter(
+      "frame_id", "map", [&](const rclcpp::Parameter & p) {
+        frame_id = p.as_string();
+      });
+
+    auto sensor_qos = rclcpp::SensorDataQoS();
+
+    altitude_pub = node->create_publisher<mavros_msgs::msg::Altitude>("altitude", sensor_qos);
   }
 
   Subscriptions get_subscriptions() override
@@ -52,30 +57,29 @@ public:
   }
 
 private:
-  ros::NodeHandle nh;
   std::string frame_id;
 
-  ros::Publisher altitude_pub;
+  rclcpp::Publisher<mavros_msgs::msg::Altitude>::SharedPtr altitude_pub;
 
   void handle_altitude(
-    const mavlink::mavlink_message_t * msg,
-    mavlink::common::msg::ALTITUDE & altitude)
+    const mavlink::mavlink_message_t * msg [[maybe_unused]],
+    mavlink::common::msg::ALTITUDE & altitude, plugin::filter::SystemAndOk filter [[maybe_unused]])
   {
-    auto ros_msg = boost::make_shared<mavros_msgs::Altitude>();
-    ros_msg->header = m_uas->synchronized_header(frame_id, altitude.time_usec);
+    auto ros_msg = mavros_msgs::msg::Altitude();
+    ros_msg.header = uas->synchronized_header(frame_id, altitude.time_usec);
+    ros_msg.monotonic = altitude.altitude_monotonic;
+    ros_msg.amsl = altitude.altitude_amsl;
+    ros_msg.local = altitude.altitude_local;
+    ros_msg.relative = altitude.altitude_relative;
+    ros_msg.terrain = altitude.altitude_terrain;
+    ros_msg.bottom_clearance = altitude.bottom_clearance;
 
-    ros_msg->monotonic = altitude.altitude_monotonic;
-    ros_msg->amsl = altitude.altitude_amsl;
-    ros_msg->local = altitude.altitude_local;
-    ros_msg->relative = altitude.altitude_relative;
-    ros_msg->terrain = altitude.altitude_terrain;
-    ros_msg->bottom_clearance = altitude.bottom_clearance;
-
-    altitude_pub.publish(ros_msg);
+    altitude_pub->publish(ros_msg);
   }
 };
+
 }       // namespace std_plugins
 }       // namespace mavros
 
-#include <pluginlib/class_list_macros.h>
-PLUGINLIB_EXPORT_CLASS(mavros::std_plugins::AltitudePlugin, mavros::plugin::PluginBase)
+#include <mavros/mavros_plugin_register_macro.hpp>  // NOLINT
+MAVROS_PLUGIN_REGISTER(mavros::std_plugins::AltitudePlugin)
