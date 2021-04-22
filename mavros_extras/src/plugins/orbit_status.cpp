0d0984ded5e37dab99283b68adb0f235649791d1
tahsinkose <tahsincan.kose@aeolusbot.com>
2021-04-22 13:34:00 +0300

    Address review.

--- mavros_extras/src/plugins/orbit_status.cpp
+++ mavros_extras/src/plugins/orbit_status.cpp
@@ -32,12 +32,12 @@ private:
 	void handle_orbit_execution_status(const mavlink::mavlink_message_t *msg,
 		mavlink::common::msg::ORBIT_EXECUTION_STATUS &mav_msg) {
 		auto ros_msg = boost::make_shared<mavros_msgs::OrbitStatus>();
-		ros_msg->timestamp = mav_msg.time_usec;
+		ros_msg->header.stamp.fromNSec(mav_msg.time_usec * 1e3);
+		ros_msg->header.frame_id = mav_msg.frame;
 		ros_msg->radius = mav_msg.radius;
 		ros_msg->x = mav_msg.x;
 		ros_msg->y = mav_msg.y;
 		ros_msg->z = mav_msg.z;
-		ros_msg->frame = mav_msg.frame;
 		ros_msg->state = mav_msg.state;
 		orbit_execution_status.publish(ros_msg);
 	}
