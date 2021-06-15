dda176bf98f43cac585c47a31487768fda792b59
Vladimir Ermakov <vooon341@gmail.com>
2021-06-15 21:50:57 +0300

    lib: yet another fix of cmake module

--- libmavconn/cmake/Modules/CheckGeographicLibDatasets.cmake
+++ libmavconn/cmake/Modules/CheckGeographicLibDatasets.cmake
@@ -26,23 +26,31 @@ find_path(GEOGRAPHICLIB_MAGNETIC_PATH_
 )
 
 if(NOT GEOGRAPHICLIB_GEOID_PATH)
-  message(STATUS "No geoid model datasets found. This will result on a SIGINT! "
-    "Please execute the script install_geographiclib_dataset.sh in /mavros/scripts")
+  message(STATUS "\
+No geoid model datasets found. This will result on a SIGINT! \
+Please execute the script install_geographiclib_dataset.sh")
 else()
-  message(STATUS "Geoid model datasets found in: " ${GEOGRAPHICLIB_GEOID_PATH}/geoid)
+  message(STATUS "\
+Geoid model datasets found in: ${GEOGRAPHICLIB_GEOID_PATH}/geoid")
   set(GEOGRAPHICLIB_GEOID_PATH ${GEOGRAPHICLIB_GEOID_PATH}/geoid)
 endif()
 if(NOT GEOGRAPHICLIB_GRAVITY_PATH_)
-  message(STATUS "No gravity field model datasets found. "
-    "Please execute the script install_geographiclib_dataset.sh in /mavros/scripts")
+  message(STATUS "\
+No gravity field model datasets found.
+Please execute the script install_geographiclib_dataset.sh")
 else()
-  message(STATUS "Gravity Field model datasets found in: " ${GEOGRAPHICLIB_GRAVITY_PATH_}/gravity)
+  message(STATUS "\
+Gravity Field model datasets found in: \
+${GEOGRAPHICLIB_GRAVITY_PATH_}/gravity")
   set(GEOGRAPHICLIB_GRAVITY_PATH ${GEOGRAPHICLIB_GRAVITY_PATH_}/gravity)
 endif()
 if(NOT GEOGRAPHICLIB_MAGNETIC_PATH_)
-  message(STATUS "No magnetic field model datasets found. "
-    "Please execute the script install_geographiclib_dataset.sh in /mavros/scripts")
+  message(STATUS "\
+No magnetic field model datasets found. \
+Please execute the script install_geographiclib_dataset.sh")
 else()
-  message(STATUS "Magnetic Field model datasets found in: " ${GEOGRAPHICLIB_MAGNETIC_PATH_}/magnetic)
+  message(STATUS "\
+Magnetic Field model datasets found in: \
+${GEOGRAPHICLIB_MAGNETIC_PATH_}/magnetic")
   set(GEOGRAPHICLIB_MAGNETIC_PATH ${GEOGRAPHICLIB_MAGNETIC_PATH_}/magnetic)
 endif()
