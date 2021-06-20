5b8da5c572faa7b7e26c1440112975ad39bade78
Vladimir Ermakov <vooon341@gmail.com>
2021-06-20 15:06:22 +0300

    plugin: add ability to set node options

--- mavros/include/mavros/plugin.hpp
+++ mavros/include/mavros/plugin.hpp
@@ -78,11 +78,13 @@ public:
   : uas(uas_), node(std::dynamic_pointer_cast<rclcpp::Node>(uas_))
   {}
 
-  explicit Plugin(UASPtr uas_, const std::string & subnode)
+  explicit Plugin(
+    UASPtr uas_, const std::string & subnode,
+    const rclcpp::NodeOptions & options = rclcpp::NodeOptions())
   : uas(uas_),
     // node(std::dynamic_pointer_cast<rclcpp::Node>(uas_)->create_sub_node(subnode))  // https://github.com/ros2/rclcpp/issues/731
     node(rclcpp::Node::make_shared(subnode,
-      std::dynamic_pointer_cast<rclcpp::Node>(uas_)->get_fully_qualified_name()))
+      std::dynamic_pointer_cast<rclcpp::Node>(uas_)->get_fully_qualified_name(), options))
   {}
 
   virtual ~Plugin() = default;
