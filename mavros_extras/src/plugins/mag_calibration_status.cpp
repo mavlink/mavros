4a9ace864d231dd5c0a3958ac69476a1d48a04ad
André Filipe <andre.ferreira@beyond-vision.pt>
2021-07-21 15:35:31 +0100

    Move Compass calibration report to extras. Rewrite code based on instructions.

--- mavros_extras/src/plugins/mag_calibration_status.cpp
+++ mavros_extras/src/plugins/mag_calibration_status.cpp
@@ -13,7 +13,6 @@
 #include <mavros_msgs/MagnetometerReporter.h>
 namespace mavros {
 namespace std_plugins {
-
 /**
  * @brief MagCalStatus plugin.
  *
@@ -22,7 +21,7 @@ namespace std_plugins {
 class MagCalStatusPlugin : public plugin::PluginBase {
 public:
 	MagCalStatusPlugin() : PluginBase(),
-		mcs_nh("~MagCalibration")
+		mcs_nh("~mag_calibration")
 	{ }
 
 	/**
@@ -55,44 +54,50 @@ private:
 	ros::NodeHandle mcs_nh;
 	ros::Publisher mcs_pub;
 	ros::Publisher mcr_pub;
-	uint8_t _rgCompassCalProgress[3] = {0};
-	bool calibration[3]={true, true, true};
+	std::vector<bool> calibration_show;
 	//Send progress of magnetometer calibration
 	void handle_status(const mavlink::mavlink_message_t *, mavlink::ardupilotmega::msg::MAG_CAL_PROGRESS &mp) {
 		auto mcs = boost::make_shared<std_msgs::UInt8>();
 
 		// How many compasses are we calibrating?
-		int compassCalCount = 0;
-		for (int i=0; i<3; i++) {
-			if (mp.cal_mask & (1 << i)) {
-				compassCalCount++;
-			}
+		std::bitset<8> compass_calibrating = mp.cal_mask;
+
+//		int compassCalCount = 0;
+//		for (int i=0; i<3; i++) {
+//			if (mp.cal_mask & (1 << i)) {
+//				compassCalCount++;
+//			}
+//		}
+		std::vector<uint8_t> _rg_compass_cal_progress(3);
+
+		if(calibration_show.size() < 3){
+			calibration_show.resize(3);
+			std::fill(calibration_show.begin(), calibration_show.end(), true);
 		}
 
-		if (mp.compass_id < 3 && compassCalCount != 0) {
+		if (mp.compass_id < 3 && compass_calibrating.count() > 0) {
 			// Each compass gets a portion of the overall progress
-			_rgCompassCalProgress[mp.compass_id] = mp.completion_pct;
+			_rg_compass_cal_progress[mp.compass_id] = mp.completion_pct;
 		}
 
-		mcs->data = static_cast<uint8_t>((_rgCompassCalProgress[0] + _rgCompassCalProgress[1] + _rgCompassCalProgress[2])/compassCalCount);
+		mcs->data = static_cast<uint8_t>((_rg_compass_cal_progress[0] + _rg_compass_cal_progress[1] + _rg_compass_cal_progress[2])/compass_calibrating.count());
 
 		mcs_pub.publish(mcs);
 	}
 
 	//Send report after calibration is done
 	void handle_report(const mavlink::mavlink_message_t *, mavlink::common::msg::MAG_CAL_REPORT &mr) {
-		if(calibration[mr.compass_id]) {
+		if(calibration_show[mr.compass_id]) {
 			auto mcr = boost::make_shared<mavros_msgs::MagnetometerReporter>();
 			mcr->header.stamp = ros::Time::now();
 			mcr->header.frame_id = std::to_string(mr.compass_id);
 			mcr->report = mr.cal_status;
 			mcr->confidence = mr.orientation_confidence;
 			mcr_pub.publish(mcr);
-			calibration[mr.compass_id] = false;
+			calibration_show[mr.compass_id] = false;
 		}
 	}
 };
-
 }	// namespace std_plugins
 }	// namespace mavros
 
