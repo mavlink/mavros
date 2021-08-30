46a55a37311b077f6c6e20073ae70464c945b6c1
Ricardo Marques <marques.ricardo17@gmail.com>
2021-08-30 15:41:44 +0200

    Update esc_status plugin with datatype change on MAVLink.
    ESC_INFO MAVLink message was updated to have negative temperates and also at a different resolution. This commit updates those changes on this side.
    
    Signed-off-by: Ricardo Marques <marques.ricardo17@gmail.com>

--- mavros_extras/src/plugins/esc_status.cpp
+++ mavros_extras/src/plugins/esc_status.cpp
@@ -96,7 +96,7 @@ private:
 			_esc_info.esc_info[esc_index + i].header = _esc_info.header;
 			_esc_info.esc_info[esc_index + i].failure_flags = esc_info.failure_flags[i];
 			_esc_info.esc_info[esc_index + i].error_count = esc_info.error_count[i];
-			_esc_info.esc_info[esc_index + i].temperature = esc_info.temperature[i];
+			_esc_info.esc_info[esc_index + i].temperature = esc_info.temperature[i] * 1E2;	//!<  [degreesC]
 		}
 
 		_max_esc_info_index = std::max(_max_esc_info_index, esc_info.index);
