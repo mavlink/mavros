85ce640f7dcb762f5eab911f9d72dc92b5e2732a
Vladimir Ermakov <vooon341@gmail.com>
2021-11-05 14:09:37 +0300

    extras: port vibration

--- mavros_extras/src/plugins/vibration.cpp
+++ mavros_extras/src/plugins/vibration.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2015 Nuno Marques.
+ *
+ * This file is part of the mavros package and subject to the license terms
+ * in the top-level LICENSE file of the mavros repository.
+ * https://github.com/mavlink/mavros/tree/master/LICENSE.md
+ */
 /**
  * @brief Vibration plugin
  * @file vibration.cpp
@@ -6,43 +13,42 @@
  * @addtogroup plugin
  * @{
  */
-/*
- * Copyright 2015 Nuno Marques.
- *
- * This file is part of the mavros package and subject to the license terms
- * in the top-level LICENSE file of the mavros repository.
- * https://github.com/mavlink/mavros/tree/master/LICENSE.md
- */
 
-#include <mavros/mavros_plugin.h>
-#include <eigen_conversions/eigen_msg.h>
+#include <tf2_eigen/tf2_eigen.h>
 
-#include <mavros_msgs/Vibration.h>
+#include "rcpputils/asserts.hpp"
+#include "mavros/mavros_uas.hpp"
+#include "mavros/plugin.hpp"
+#include "mavros/plugin_filter.hpp"
+
+#include "mavros_msgs/msg/vibration.hpp"
 
 namespace mavros
 {
 namespace extra_plugins
 {
+using namespace std::placeholders;      // NOLINT
+
 /**
  * @brief Vibration plugin
+ * @plugin vibration
  *
  * This plugin is intended to publish MAV vibration levels and accelerometer clipping from FCU.
  */
-class VibrationPlugin : public plugin::PluginBase
+class VibrationPlugin : public plugin::Plugin
 {
 public:
-  VibrationPlugin()
-  : PluginBase(),
-    vibe_nh("~vibration")
-  {}
-
-  void initialize(UAS & uas_) override
+  explicit VibrationPlugin(plugin::UASPtr uas_)
+  : Plugin(uas_, "vibration")
   {
-    PluginBase::initialize(uas_);
+    enable_node_watch_parameters();
 
-    vibe_nh.param<std::string>("frame_id", frame_id, "base_link");
+    node_declate_and_watch_parameter(
+      "frame_id", "base_link", [&](const rclcpp::Parameter & p) {
+        frame_id = p.as_string();
+      });
 
-    vibration_pub = vibe_nh.advertise<mavros_msgs::Vibration>("raw/vibration", 10);
+    vibration_pub = node->create_publisher<mavros_msgs::msg::Vibration>("~/raw/vibration", 10);
   }
 
   Subscriptions get_subscriptions() override
@@ -53,32 +59,31 @@ public:
   }
 
 private:
-  ros::NodeHandle vibe_nh;
+  rclcpp::Publisher<mavros_msgs::msg::Vibration>::SharedPtr vibration_pub;
 
   std::string frame_id;
 
-  ros::Publisher vibration_pub;
-
   void handle_vibration(
-    const mavlink::mavlink_message_t * msg,
-    mavlink::common::msg::VIBRATION & vibration)
+    const mavlink::mavlink_message_t * msg [[maybe_unused]],
+    mavlink::common::msg::VIBRATION & vibration,
+    plugin::filter::SystemAndOk filter [[maybe_unused]])
   {
-    auto vibe_msg = boost::make_shared<mavros_msgs::Vibration>();
+    auto vibe_msg = mavros_msgs::msg::Vibration();
 
-    vibe_msg->header = m_uas->synchronized_header(frame_id, vibration.time_usec);
+    vibe_msg.header = uas->synchronized_header(frame_id, vibration.time_usec);
 
     Eigen::Vector3d vib_enu = {vibration.vibration_x, vibration.vibration_y, vibration.vibration_z};
-    tf::vectorEigenToMsg(ftf::transform_frame_ned_enu(vib_enu), vibe_msg->vibration);
+    tf2::toMsg(ftf::transform_frame_ned_enu(vib_enu), vibe_msg.vibration);
 
-    vibe_msg->clipping[0] = vibration.clipping_0;
-    vibe_msg->clipping[1] = vibration.clipping_1;
-    vibe_msg->clipping[2] = vibration.clipping_2;
+    vibe_msg.clipping[0] = vibration.clipping_0;
+    vibe_msg.clipping[1] = vibration.clipping_1;
+    vibe_msg.clipping[2] = vibration.clipping_2;
 
-    vibration_pub.publish(vibe_msg);
+    vibration_pub->publish(vibe_msg);
   }
 };
 }       // namespace extra_plugins
 }       // namespace mavros
 
-#include <pluginlib/class_list_macros.h>
-PLUGINLIB_EXPORT_CLASS(mavros::extra_plugins::VibrationPlugin, mavros::plugin::PluginBase)
+#include <mavros/mavros_plugin_register_macro.hpp>  // NOLINT
+MAVROS_PLUGIN_REGISTER(mavros::extra_plugins::VibrationPlugin)
