a8e9f6cfab6e318fd5b39da0f9e1dc2ec9986a26
Ryuji Yasukochi <r.yasukouchi@m2-labo.jp>
2022-05-17 15:51:38 +0900

    make simple launch file

---
+++ mavros/launch/node.launch.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# Authors: Ryuji Yasukochi
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import ThisLaunchFileDir
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+	config_dir = LaunchConfiguration('config_dir',
+		default=os.path.join(
+			get_package_share_directory('mavros'),
+			'param',
+			"ros2_mavros_params.yaml"
+			))
+
+	return LaunchDescription([
+        Node(
+            package='mavros',
+            executable='mavros_node',
+			parameters=[config_dir],
+			),
+    ])
