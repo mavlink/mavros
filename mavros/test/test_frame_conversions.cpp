3394c42f3ea7881fca669865e449c0d4e9308002
Vladimir Ermakov <vooon341@gmail.com>
2024-10-10 10:17:44 +0200

    apply ament_uncrustify --reformat (jazzy)

--- mavros/test/test_frame_conversions.cpp
+++ mavros/test/test_frame_conversions.cpp
@@ -168,8 +168,8 @@ TEST(FRAME_TF, transform_frame__covariance3x3)
   // Rotation quaternion of PI around x axis
   const auto q = Eigen::Quaterniond(
       Eigen::AngleAxisd(0.0, Eigen::Vector3d::UnitZ()) *
-			Eigen::AngleAxisd(0.0, Eigen::Vector3d::UnitY()) *
-			Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitX())
+                        Eigen::AngleAxisd(0.0, Eigen::Vector3d::UnitY()) *
+                        Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitX())
   );
 
   /* Calculated as:
@@ -183,9 +183,9 @@ TEST(FRAME_TF, transform_frame__covariance3x3)
    *                            | 0  0 -1 |
    */
   ftf::Covariance3d expected = {{
-     1.0, -2.0, -3.0,
-    -4.0,  5.0,  6.0,
-    -7.0,  8.0,  9.0
+    1.0, -2.0, -3.0,
+    -4.0, 5.0, 6.0,
+    -7.0, 8.0, 9.0
   }};
 
   auto out = ftf::detail::transform_frame(input, q);
@@ -210,8 +210,8 @@ TEST(FRAME_TF, transform_frame__covariance6x6)
   // Rotation quaternion of PI around x axis
   const auto q = Eigen::Quaterniond(
       Eigen::AngleAxisd(0.0, Eigen::Vector3d::UnitZ()) *
-			Eigen::AngleAxisd(0.0, Eigen::Vector3d::UnitY()) *
-			Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitX())
+                        Eigen::AngleAxisd(0.0, Eigen::Vector3d::UnitY()) *
+                        Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitX())
   );
 
   /* Calculated as:
@@ -228,12 +228,12 @@ TEST(FRAME_TF, transform_frame__covariance6x6)
    *                            | 0  0 -1 |
    */
   ftf::Covariance6d expected = {{
-      1.0,  -2.0,  -3.0,   4.0,  -5.0,  -6.0,
-     -7.0,   8.0,   9.0, -10.0,  11.0,  12.0,
-    -13.0,  14.0,  15.0, -16.0,  17.0,  18.0,
-     19.0, -20.0, -21.0,  22.0, -23.0, -24.0,
-    -25.0,  26.0,  27.0, -28.0,  29.0,  30.0,
-    -31.0,  32.0,  33.0, -34.0,  35.0,  36.0
+    1.0, -2.0, -3.0, 4.0, -5.0, -6.0,
+    -7.0, 8.0, 9.0, -10.0, 11.0, 12.0,
+    -13.0, 14.0, 15.0, -16.0, 17.0, 18.0,
+    19.0, -20.0, -21.0, 22.0, -23.0, -24.0,
+    -25.0, 26.0, 27.0, -28.0, 29.0, 30.0,
+    -31.0, 32.0, 33.0, -34.0, 35.0, 36.0
   }};
 
   auto out = ftf::detail::transform_frame(input, q);
