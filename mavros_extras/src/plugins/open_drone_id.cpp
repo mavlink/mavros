8bfeee36141d38034ee961a9b87f40a7fba1bdf0
Vladimir Ermakov <vooon341@gmail.com>
2025-05-05 12:02:02 +0200

    extras: fix indent

--- mavros_extras/src/plugins/open_drone_id.cpp
+++ mavros_extras/src/plugins/open_drone_id.cpp
@@ -20,137 +20,140 @@
 #include "mavros/plugin.hpp"
 #include "mavros/plugin_filter.hpp"
 
-#include "mavros_msgs/msg/basic_id.hpp"
-#include "mavros_msgs/msg/operator_id.hpp"
-#include "mavros_msgs/msg/self_id.hpp"
-#include "mavros_msgs/msg/system.hpp"
-#include "mavros_msgs/msg/system_update.hpp"
+#include "mavros_msgs/msg/open_drone_id_basic_id.hpp"
+#include "mavros_msgs/msg/open_drone_id_operator_id.hpp"
+#include "mavros_msgs/msg/open_drone_id_self_id.hpp"
+#include "mavros_msgs/msg/open_drone_id_system.hpp"
+#include "mavros_msgs/msg/open_drone_id_system_update.hpp"
 
-namespace mavros {
-namespace extra_plugins {
+namespace mavros
+{
+namespace extra_plugins
+{
 using namespace std::placeholders;      // NOLINT
 
 /**
  * @brief Open Drone ID plugin
+ * @plugin open_drone_id
  *
  * Sends Open Drone ID data to the FCU
  */
-class OpenDroneIDPlugin : public plugin::Plugin 
+class OpenDroneIDPlugin : public plugin::Plugin
 {
 public:
-	explicit OpenDroneIDPlugin(plugin::UASPtr uas_) 
-	: Plugin(uas_, "open_drone_id")
-	{ 
-		basic_id_sub = node->create_subscription<mavros_msgs::msg::BasicID>(
-			"~/basic_id", 1, std::bind(
-			&OpenDroneIDPlugin::basic_id_cb, this,
-			_1));
+  explicit OpenDroneIDPlugin(plugin::UASPtr uas_)
+  : Plugin(uas_, "open_drone_id")
+  {
+    basic_id_sub = node->create_subscription<mavros_msgs::msg::BasicID>(
+                        "~/basic_id", 1, std::bind(
+                        &OpenDroneIDPlugin::basic_id_cb, this,
+                        _1));
 
-		operator_id_sub = node->create_subscription<mavros_msgs::msg::OperatorID>(
-			"~/operator_id", 1, std::bind(
-			&OpenDroneIDPlugin::operator_id_cb, this,
-			_1));
+    operator_id_sub = node->create_subscription<mavros_msgs::msg::OperatorID>(
+                        "~/operator_id", 1, std::bind(
+                        &OpenDroneIDPlugin::operator_id_cb, this,
+                        _1));
 
-		self_id_sub = node->create_subscription<mavros_msgs::msg::SelfID>(
-			"~/self_id", 1, std::bind(
-			&OpenDroneIDPlugin::self_id_cb, this,
-			_1));
+    self_id_sub = node->create_subscription<mavros_msgs::msg::SelfID>(
+                        "~/self_id", 1, std::bind(
+                        &OpenDroneIDPlugin::self_id_cb, this,
+                        _1));
 
-		system_sub = node->create_subscription<mavros_msgs::msg::System>(
-			"~/system", 1, std::bind(
-			&OpenDroneIDPlugin::system_cb, this,
-			_1));
+    system_sub = node->create_subscription<mavros_msgs::msg::System>(
+                        "~/system", 1, std::bind(
+                        &OpenDroneIDPlugin::system_cb, this,
+                        _1));
 
-		system_update_sub = node->create_subscription<mavros_msgs::msg::SystemUpdate>(
-			"~/system_update", 1, std::bind(
-			&OpenDroneIDPlugin::system_update_cb, this,
-			_1));
-	}
+    system_update_sub = node->create_subscription<mavros_msgs::msg::SystemUpdate>(
+                        "~/system_update", 1, std::bind(
+                        &OpenDroneIDPlugin::system_update_cb, this,
+                        _1));
+  }
 
-	Subscriptions get_subscriptions()
-	{
-		return { };
-	}
+  Subscriptions get_subscriptions()
+  {
+    return {};
+  }
 
 private:
-	rclcpp::Subscription<mavros_msgs::msg::BasicID>::SharedPtr basic_id_sub;
-	rclcpp::Subscription<mavros_msgs::msg::OperatorID>::SharedPtr operator_id_sub;
-	rclcpp::Subscription<mavros_msgs::msg::SelfID>::SharedPtr self_id_sub;
-	rclcpp::Subscription<mavros_msgs::msg::System>::SharedPtr system_sub;
-	rclcpp::Subscription<mavros_msgs::msg::SystemUpdate>::SharedPtr system_update_sub;
+  rclcpp::Subscription<mavros_msgs::msg::BasicID>::SharedPtr basic_id_sub;
+  rclcpp::Subscription<mavros_msgs::msg::OperatorID>::SharedPtr operator_id_sub;
+  rclcpp::Subscription<mavros_msgs::msg::SelfID>::SharedPtr self_id_sub;
+  rclcpp::Subscription<mavros_msgs::msg::System>::SharedPtr system_sub;
+  rclcpp::Subscription<mavros_msgs::msg::SystemUpdate>::SharedPtr system_update_sub;
 
 
-	void basic_id_cb(const mavros_msgs::msg::BasicID::SharedPtr msg)
-	{
-		mavlink::common::msg::OPEN_DRONE_ID_BASIC_ID basic_id{};
+  void basic_id_cb(const mavros_msgs::msg::BasicID::SharedPtr msg)
+  {
+    mavlink::common::msg::OPEN_DRONE_ID_BASIC_ID basic_id{};
 
-		basic_id.id_type = msg->id_type;
-		basic_id.ua_type = msg->ua_type;
+    basic_id.id_type = msg->id_type;
+    basic_id.ua_type = msg->ua_type;
 
-		size_t length = std::min(basic_id.uas_id.size(), msg->uas_id.size());
-		std::memcpy(basic_id.uas_id.data(), msg->uas_id.data(), length);
+    size_t length = std::min(basic_id.uas_id.size(), msg->uas_id.size());
+    std::memcpy(basic_id.uas_id.data(), msg->uas_id.data(), length);
 
-		uas->send_message(basic_id);
-	}
+    uas->send_message(basic_id);
+  }
 
-	void operator_id_cb(const mavros_msgs::msg::OperatorID::SharedPtr msg)
-	{
-		mavlink::common::msg::OPEN_DRONE_ID_OPERATOR_ID operator_id{};
+  void operator_id_cb(const mavros_msgs::msg::OperatorID::SharedPtr msg)
+  {
+    mavlink::common::msg::OPEN_DRONE_ID_OPERATOR_ID operator_id{};
 
-		operator_id.operator_id_type = msg->operator_id_type;
+    operator_id.operator_id_type = msg->operator_id_type;
 
-		size_t length = std::min(operator_id.operator_id.size(), msg->operator_id.size());
-		std::memcpy(operator_id.operator_id.data(), msg->operator_id.data(), length);
+    size_t length = std::min(operator_id.operator_id.size(), msg->operator_id.size());
+    std::memcpy(operator_id.operator_id.data(), msg->operator_id.data(), length);
 
-		uas->send_message(operator_id);
-	}
+    uas->send_message(operator_id);
+  }
 
-	void self_id_cb(const mavros_msgs::msg::SelfID::SharedPtr msg) 
-	{
-		mavlink::common::msg::OPEN_DRONE_ID_SELF_ID self_id{};
-		self_id.description_type = msg->description_type;
+  void self_id_cb(const mavros_msgs::msg::SelfID::SharedPtr msg)
+  {
+    mavlink::common::msg::OPEN_DRONE_ID_SELF_ID self_id{};
+    self_id.description_type = msg->description_type;
 
-		size_t length = std::min(self_id.description.size(), msg->description.size());
-		std::memcpy(self_id.description.data(), msg->description.data(), length);
+    size_t length = std::min(self_id.description.size(), msg->description.size());
+    std::memcpy(self_id.description.data(), msg->description.data(), length);
 
-		uas->send_message(self_id);
-	}
+    uas->send_message(self_id);
+  }
 
-	void system_cb(const mavros_msgs::msg::System::SharedPtr msg) 
-	{
-		mavlink::common::msg::OPEN_DRONE_ID_SYSTEM system{};
+  void system_cb(const mavros_msgs::msg::System::SharedPtr msg)
+  {
+    mavlink::common::msg::OPEN_DRONE_ID_SYSTEM system{};
 
-		system.operator_location_type = msg->operator_location_type;
-		system.classification_type = msg->classification_type;
-		system.operator_latitude = msg->operator_latitude;
-		system.operator_longitude = msg->operator_longitude;
-		system.area_count = msg->area_count;
-		system.area_radius = msg->area_radius;
-		system.area_ceiling = msg->area_ceiling;
-		system.area_floor = msg->area_floor;
-		system.category_eu = msg->category_eu;
-		system.class_eu = msg->class_eu;
-		system.operator_altitude_geo = msg->operator_altitude_geo;
-		system.timestamp = msg->timestamp;
+    system.operator_location_type = msg->operator_location_type;
+    system.classification_type = msg->classification_type;
+    system.operator_latitude = msg->operator_latitude;
+    system.operator_longitude = msg->operator_longitude;
+    system.area_count = msg->area_count;
+    system.area_radius = msg->area_radius;
+    system.area_ceiling = msg->area_ceiling;
+    system.area_floor = msg->area_floor;
+    system.category_eu = msg->category_eu;
+    system.class_eu = msg->class_eu;
+    system.operator_altitude_geo = msg->operator_altitude_geo;
+    system.timestamp = msg->timestamp;
 
-		uas->send_message(system);
-	}
+    uas->send_message(system);
+  }
 
-	void system_update_cb(const mavros_msgs::msg::SystemUpdate::SharedPtr msg) 
-	{
-		mavlink::common::msg::OPEN_DRONE_ID_SYSTEM_UPDATE system_update{};
+  void system_update_cb(const mavros_msgs::msg::SystemUpdate::SharedPtr msg)
+  {
+    mavlink::common::msg::OPEN_DRONE_ID_SYSTEM_UPDATE system_update{};
 
-		system_update.operator_latitude = msg->operator_latitude;
-		system_update.operator_longitude = msg->operator_longitude;
-		system_update.operator_altitude_geo = msg->operator_altitude_geo;
-		system_update.timestamp = msg->timestamp;
+    system_update.operator_latitude = msg->operator_latitude;
+    system_update.operator_longitude = msg->operator_longitude;
+    system_update.operator_altitude_geo = msg->operator_altitude_geo;
+    system_update.timestamp = msg->timestamp;
 
-		uas->send_message(system_update);
-	}
+    uas->send_message(system_update);
+  }
 
 };
-}	// namespace extra_plugins
-}	// namespace mavros
+}       // namespace extra_plugins
+}       // namespace mavros
 
 #include <mavros/mavros_plugin_register_macro.hpp>  // NOLINT
 MAVROS_PLUGIN_REGISTER(mavros::extra_plugins::OpenDroneIDPlugin)
