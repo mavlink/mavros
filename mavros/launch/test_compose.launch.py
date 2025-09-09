mavlink/mavros

commit 09df29645e34b46c781e27dffe1fe613f81f50e0

Update RouterTest for Router parameters post-constructor.
- Added startup_delay_timer->cancel() to RouterTest::create_node()

- Created RouterTest::create_node_no_endpoints() for the two unit tests
  which call Router::set_parameters() to set endpoints.

- Converterd Router parameter declaration from an inline functional
  to a standalone function which can be called from
  RouterTest::Create_node_no_endpoints(), and from the timer in the
  constructor

mavros/include/mavros/mavros_router.hpp
@@ -183,16 +183,7 @@ public:
     //
     // Repeat the pattern from mavros_uas of using a delay timer
     startup_delay_timer = this->create_wall_timer(
-      10ms, [this]() {
-        RCLCPP_WARN(get_logger(), "In timer callback");
-        startup_delay_timer->cancel();
-        set_parameters_handle_ptr =
-        this->add_on_set_parameters_callback(std::bind(&Router::on_set_parameters_cb, this, _1));
-
-        this->declare_parameter<StrV>("fcu_urls", StrV());
-        this->declare_parameter<StrV>("gcs_urls", StrV());
-        this->declare_parameter<StrV>("uas_urls", StrV());
-      });
+      10ms, std::bind(&Router::initialize_parameters, this));
   }
 
   void route_message(Endpoint::SharedPtr src, const mavlink_message_t * msg, const Framing framing);
@@ -229,6 +220,18 @@ private:
 
   void periodic_reconnect_endpoints();
   void periodic_clear_stale_remote_addrs();
+
+  void initialize_parameters()
+  {
+    RCLCPP_WARN(get_logger(), "In timer callback");
+    startup_delay_timer->cancel();
+    set_parameters_handle_ptr =
+      this->add_on_set_parameters_callback(std::bind(&Router::on_set_parameters_cb, this, _1));
+
+    this->declare_parameter<StrV>("fcu_urls", StrV());
+    this->declare_parameter<StrV>("gcs_urls", StrV());
+    this->declare_parameter<StrV>("uas_urls", StrV());
+  }
 
   rcl_interfaces::msg::SetParametersResult on_set_parameters_cb(
     const std::vector<rclcpp::Parameter> & parameters);

mavros/test/test_router.cpp
@@ -90,6 +90,7 @@ public:
   Router::SharedPtr create_node()
   {
     auto router = std::make_shared<Router>("test_mavros_router");
+    router->startup_delay_timer->cancel();
 
     auto make_and_add_mock_endpoint =
       [router](id_t id, const std::string & url, LT type,
@@ -116,6 +117,17 @@ public:
     make_and_add_mock_endpoint(1003, "/uas2", LT::uas, {0x0000, 0x0200, 0x02BF});
     make_and_add_mock_endpoint(1004, "mock://gcs1", LT::gcs, {0x0000, 0xFF00, 0xFFBE});
     make_and_add_mock_endpoint(1005, "mock://gcs2", LT::gcs, {0x0000, 0xFF00, 0xFFBD});
+
+    return router;
+  }
+
+  Router::SharedPtr create_node_no_endpoints()
+  {
+    auto router = std::make_shared<Router>("test_mavros_router");
+    router->startup_delay_timer->cancel();
+
+    // Call this manually
+    router->initialize_parameters();
 
     return router;
   }
@@ -184,7 +196,7 @@ public:
 
 TEST_F(TestRouter, set_parameter)
 {
-  auto router = std::make_shared<Router>("test_mavros_router");
+  auto router = this->create_node_no_endpoints();
 
   router->set_parameters(
     std::vector<rclcpp::Parameter>{
@@ -388,7 +400,7 @@ TEST_F(TestRouter, route_targeted_system_component)
 
 TEST_F(TestRouter, endpoint_recv_message)
 {
-  auto router = std::make_shared<Router>("test_mavros_router");
+  auto router = create_node_no_endpoints();
 
   router->set_parameters(
     std::vector<rclcpp::Parameter>{

commit 1606c338bfadbb2e38021520ae8bfa842b7a8736

router: fix mavros_node fault introduced by #2053
Fix #2055

Signed-off-by: Vladimir Ermakov <vooon341@gmail.com>

mavros/include/mavros/mavros_router.hpp
@@ -151,6 +151,10 @@ public:
   {
     RCLCPP_DEBUG(this->get_logger(), "Start mavros::router::Router initialization...");
 
+    this->declare_parameter<StrV>("fcu_urls", StrV());
+    this->declare_parameter<StrV>("gcs_urls", StrV());
+    this->declare_parameter<StrV>("uas_urls", StrV());
+
     add_service = this->create_service<mavros_msgs::srv::EndpointAdd>(
       "~/add_endpoint",
       std::bind(&Router::add_endpoint, this, _1, _2));
@@ -179,11 +183,13 @@ public:
     RCLCPP_INFO(get_logger(), "Known MAVLink dialects:%s", ss.str().c_str());
     RCLCPP_INFO(get_logger(), "MAVROS Router started");
 
-    // This must be run _after_ the constructor has finished
-    //
-    // Repeat the pattern from mavros_uas of using a delay timer
-    startup_delay_timer = this->create_wall_timer(
-      10ms, std::bind(&Router::initialize_parameters, this));
+    // Delay parameter callback initialization because
+    // add/del endpoints calls have to use shared_from_this(),
+    // which cannot be used before we leave the constructor.
+    startup_delay_timer = this->create_wall_timer(1ms, [this]() {
+          this->startup_delay_timer->cancel();
+          this->param_init_once();
+    });
   }
 
   void route_message(Endpoint::SharedPtr src, const mavlink_message_t * msg, const Framing framing);
@@ -221,16 +227,19 @@ private:
   void periodic_reconnect_endpoints();
   void periodic_clear_stale_remote_addrs();
 
-  void initialize_parameters()
+  std::once_flag param_init_flag;
+  void param_init()
   {
-    RCLCPP_WARN(get_logger(), "In timer callback");
-    startup_delay_timer->cancel();
     set_parameters_handle_ptr =
       this->add_on_set_parameters_callback(std::bind(&Router::on_set_parameters_cb, this, _1));
 
-    this->declare_parameter<StrV>("fcu_urls", StrV());
-    this->declare_parameter<StrV>("gcs_urls", StrV());
-    this->declare_parameter<StrV>("uas_urls", StrV());
+    auto params = get_parameters({"fcu_urls", "gcs_urls", "fcu_urls"});
+    on_set_parameters_cb(params);
+  }
+
+  void param_init_once()
+  {
+    std::call_once(param_init_flag, std::bind(&Router::param_init, this));
   }
 
   rcl_interfaces::msg::SetParametersResult on_set_parameters_cb(

mavros/launch/test_compose.launch.py
@@ -1,0 +1,29 @@
+from launch import LaunchDescription
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+def generate_launch_description():
+    """Generate launch description for MAVROS composable node."""
+
+    container = ComposableNodeContainer(
+        name="mavros_container",
+        namespace="",
+        package="rclcpp_components",
+        executable="component_container_mt",
+        composable_node_descriptions=[
+            ComposableNode(
+                package="mavros",
+                plugin="mavros::router::Router",  # Or the specific MAVROS component you need
+                name="mavros_router",
+                parameters=[
+                    {"fcu_urls": ["tcp://localhost:6777"]},
+                    {"fcu_protocol": "v2.0"},
+                    {"uas_urls": ["/uas1"]},
+                ],
+                extra_arguments=[{"use_intra_process_comms": True}],
+            )
+        ],
+        output="screen",
+    )
+
+    return LaunchDescription([container])

mavros/test/test_router.cpp
@@ -1,4 +1,3 @@
-//
 // mavros
 // Copyright 2021 Vladimir Ermakov, All rights reserved.
 //
@@ -127,7 +126,7 @@ public:
     router->startup_delay_timer->cancel();
 
     // Call this manually
-    router->initialize_parameters();
+    router->param_init_once();
 
     return router;
   }