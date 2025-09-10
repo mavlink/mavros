1f1bd266e76befefb5df30a033f7936ac8e8a2a4
Vladimir Ermakov <vooon341@gmail.com>
2025-09-10 11:20:39 +0200

    router: fix copy-paste error in endpoint loading
    Signed-off-by: Vladimir Ermakov <vooon341@gmail.com>

--- mavros/launch/test_compose.launch.py
+++ mavros/launch/test_compose.launch.py
@@ -16,14 +16,29 @@ def generate_launch_description():
                 plugin="mavros::router::Router",  # Or the specific MAVROS component you need
                 name="mavros_router",
                 parameters=[
-                    {"fcu_urls": ["tcp://localhost:6777"]},
+                    # {"fcu_urls": ["tcp://127.0.0.1:5760"]},
+                    {"fcu_urls": ["udp://0.0.0.0:14540@"]},
+                    {"gcs_urls": ["udp://127.0.0.1:14555@"]},
+                    {"uas_urls": ["/uas1", "/uas2"]},
                     {"fcu_protocol": "v2.0"},
-                    {"uas_urls": ["/uas1"]},
                 ],
                 extra_arguments=[{"use_intra_process_comms": True}],
-            )
+            ),
+            ComposableNode(
+                package="mavros",
+                plugin="mavros::uas::UAS",  # Or the specific MAVROS component you need
+                name="UAS1",
+                parameters=[
+                    {"uas_url": "/uas1"},
+                    {"fcu_protocol": "v2.0"},
+                    {"plugin_allowlist": ["sys_*", ]},
+                    {"plugin_denylist": ["*"]},
+                ],
+                extra_arguments=[{"use_intra_process_comms": True}],
+            ),
         ],
         output="screen",
+         arguments=['--ros-args', '--log-level', 'DEBUG'],
     )
 
     return LaunchDescription([container])
