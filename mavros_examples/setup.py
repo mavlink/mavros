8f7e9aa2bccd2bf0e0e131ee7c850cbac0cf0a88
Vladimir Ermakov <vooon341@gmail.com>
2025-10-26 16:13:28 +0100

    examples: add sample setup.py

---
+++ mavros_examples/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+package_name = "mavros_examples"
+
+setup(
+    name=package_name,
+    version="2.12.0",
+    packages=[package_name],
+    install_requires=["setuptools"],
+    zip_safe=True,
+)
